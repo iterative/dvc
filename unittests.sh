0a68f3d7f9a019c08c1dbe0b1873777965130914
Dmitry Petrov <dmitry.petrov@nevesomo.com>
2017-03-22 02:28:12 -0700

    Remove the token from file and add "report" param

--- unittests.sh
+++ unittests.sh
@@ -1,2 +1,5 @@
 nosetests --cover-inclusive --cover-erase --cover-package=dvc --with-coverage
-CODECLIMATE_REPO_TOKEN=a668a3d02db00ab993ad82e1b3d5d2eb00c3c2e26e659153ae4cfd2f12aaad23 codeclimate-test-reporter
+
+if [ "$1" = "report" ]; then
+    codeclimate-test-reporter
+fi
