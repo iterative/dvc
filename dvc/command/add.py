aa396d1b42e1b415c26e82d8eb64dbf110c64644
Ruslan Kuprieiev <kupruser@gmail.com>
2018-01-07 13:33:26 +0200

    add/remove: catch and report errors
    Signed-off-by: Ruslan Kuprieiev <kupruser@gmail.com>

--- dvc/command/add.py
+++ dvc/command/add.py
@@ -1,8 +1,13 @@
+from dvc.exceptions import DvcException
 from dvc.command.common.base import CmdBase
 
 
 class CmdAdd(CmdBase):
     def run(self):
         for target in self.args.targets:
-            self.project.add(target)
+            try:
+                self.project.add(target)
+            except DvcException as ex:
+                self.project.logger.error('Failed to add {}: {}'.format(target, str(ex)))
+                return 1
         return 0
--- dvc/command/remove.py
+++ dvc/command/remove.py
@@ -1,8 +1,13 @@
+from dvc.exceptions import DvcException
 from dvc.command.common.base import CmdBase
 
 
 class CmdRemove(CmdBase):
     def run(self):
         for target in self.args.targets:
-            self.project.remove(target)
+            try:
+                self.project.remove(target)
+            except DvcException as ex:
+                self.project.logger.error('Failed to remove {}: {}'.format(target, str(ex)))
+                return 1
         return 0
