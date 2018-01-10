dba9d1f5eb8072a4fad1a83c073d9bbfad4d4ae1
Ruslan Kuprieiev <kupruser@gmail.com>
2018-01-10 12:46:47 +0200

    travis: build packages only for tags
    Signed-off-by: Ruslan Kuprieiev <kupruser@gmail.com>

--- scripts/ci/script.sh
+++ scripts/ci/script.sh
@@ -1,10 +1,12 @@
 ./unittests.sh
 ./functests.sh
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-	./scripts/build_macos.sh
-else
-	./scripts/build_linux.sh
-fi
+if [[ ! -z "$TRAVIS_TAG" ]]; then
+	if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+		./scripts/build_macos.sh
+	else
+		./scripts/build_linux.sh
+	fi
 
-./scripts/build_package.sh
+	./scripts/build_package.sh
+fi
