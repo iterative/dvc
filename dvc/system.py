d54f803eb6cb6ca44eff355741a99e024d20de68
Ruslan Kuprieiev <kupruser@gmail.com>
2018-02-14 18:22:19 +0200

    system: remove unused code
    Signed-off-by: Ruslan Kuprieiev <kupruser@gmail.com>

--- dvc/system.py
+++ dvc/system.py
@@ -5,7 +5,6 @@ import subprocess
 from builtins import str
 
 if os.name == 'nt':
-    from ctypes import create_unicode_buffer, windll
     import ntfsutils.hardlink as winlink
 
 
@@ -33,51 +32,3 @@ class System(object):
             return os.path.samefile(path1, path2)
 
         return winlink.samefile(path1, path2)
-
-    @staticmethod
-    def _get_symlink_string(path):
-        p = subprocess.Popen(["dir", path], shell=True)
-        output, _ = p.communicate()
-        if p.returncode != 0 or output == None:
-            return None
-
-        lines = output.split('\n')
-        for line in lines:
-            if System.SYMLINK_OUTPUT in line:
-                return line
-        return None
-
-    @staticmethod
-    def realpath(path):
-        # It is definitely not the best way to check a symlink.
-
-        if System.is_unix():
-            return os.path.realpath(path)
-
-        output = System._get_symlink_string(path)
-        if output is None:
-            return os.path.realpath(path)
-
-        groups = re.compile(r'\[\S+\]$').findall(output.strip())
-        if len(groups) < 1:
-            return os.path.realpath(path)
-
-        resolved_link = groups[0][1:-1]
-        return resolved_link
-
-    @staticmethod
-    def get_long_path(path):
-        """Convert short path to a full path. It is needed for Windows."""
-        if System.is_unix():
-            return path
-
-        buffer = ctypes.create_unicode_buffer(System.LONG_PATH_BUFFER_SIZE)
-        get_long_path_name = ctypes.windll.kernel32.GetLongPathNameW
-        result = get_long_path_name(u'%s' % str(path), buffer, System.LONG_PATH_BUFFER_SIZE)
-        if result == 0 or result > System.LONG_PATH_BUFFER_SIZE:
-            return path
-        return buffer.value
-
-    @staticmethod
-    def get_cwd():
-        return System.get_long_path(os.getcwd())
