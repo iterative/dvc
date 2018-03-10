7094ec01519f678cb2c9ffb7e33be81eff3206f5
Ruslan Kuprieiev <kupruser@gmail.com>
2018-03-10 11:22:32 +0200

    dvc: calculate 'effective inode' from getfileinfo() on windows
    Signed-off-by: Ruslan Kuprieiev <kupruser@gmail.com>

--- dvc/system.py
+++ dvc/system.py
@@ -6,6 +6,7 @@ from builtins import str
 
 if os.name == 'nt':
     import ntfsutils.hardlink as winlink
+    from ntfsutils.fs import *
 
 
 class System(object):
@@ -22,6 +23,29 @@ class System(object):
             return os.link(source, link_name)
 
         return winlink.create(source, link_name)
+
+    @staticmethod
+    def _getfileinfo(path):
+        # FIXME: current implementation from ntfsutils doesn't support dirs,
+        # and until https://github.com/sid0/ntfs/pull/10 is not merged and
+        # pip package not updated, we have to use our own version.
+        hfile = CreateFile(path, GENERIC_READ, FILE_SHARE_READ, None, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, None)
+        if hfile is None:
+            raise WinError()
+        info = BY_HANDLE_FILE_INFORMATION()
+        rv = GetFileInformationByHandle(hfile, info)
+        CloseHandle(hfile)
+        if rv == 0:
+            raise WinError()
+        return info
+
+    @staticmethod
+    def inode(path):
+        if System.is_unix():
+            return os.stat(path).st_ino
+
+        info = System._getfileinfo(path)
+        return hash((info.dwVolumeSerialNumber, info.nFileIndexHigh, info.nFileIndexLow))
 
     @staticmethod
     def samefile(path1, path2):
