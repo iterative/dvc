1f72274192127162939f4e318cfee15f98f4870d
Ruslan Kuprieiev <kupruser@gmail.com>
2018-04-18 09:48:10 +0300

    repro: print user-friendly error
    Fixes #659
    
    Signed-off-by: Ruslan Kuprieiev <kupruser@gmail.com>

--- dvc/command/repro.py
+++ dvc/command/repro.py
@@ -1,5 +1,8 @@
+import os
+
 from dvc.command.common.base import CmdBase
-from dvc.project import ReproductionError
+from dvc.project import ReproductionError, StageNotFoundError
+from dvc.stage import Stage
 
 
 class CmdRepro(CmdBase):
@@ -13,6 +16,16 @@ class CmdRepro(CmdBase):
             except ReproductionError as ex:
                 msg = 'Failed to reproduce \'{}\''.format(target)
                 self.project.logger.error(msg, ex)
+                return 1
+            except StageNotFoundError as ex:
+                if os.path.exists(target):
+                    msg = '\'{}\' is not a dvc file.'.format(target)
+                    dvcfile = target + Stage.STAGE_FILE_SUFFIX
+                    if os.path.exists(target) and os.path.isfile(dvcfile):
+                        msg += ' Maybe you meant \'{}\'?'.format(dvcfile)
+                else:
+                    msg = '\'{}\' does not exist.'.format(target)
+                self.project.logger.error(msg)
                 return 1
             except Exception as ex:
                 msg = 'Failed to reproduce \'{}\' - unexpected error'.format(target)
