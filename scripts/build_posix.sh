adaf3f749ff451619154fd7f4c0f403657a7c440
Ruslan Kuprieiev <kupruser@gmail.com>
2018-05-17 14:18:40 +0300

    scripts: merge build_linux and build_macos
    Signed-off-by: Ruslan Kuprieiev <kupruser@gmail.com>

--- scripts/build_posix.sh
+++ scripts/build_posix.sh
@@ -1,10 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+set -x
 
 BUILD_DIR=build
-INSTALL_DIR=usr/local
 BIN_DIR=$BUILD_DIR/$INSTALL_DIR/bin
+
+if [[ "$(uname)" == 'Linux' ]]; then
+	INSTALL_DIR=usr
+	FPM_FLAGS=
+else
+	INSTALL_DIR=usr/local
+	FPM_FLAGS='--osxpkg-identifier-prefix com.dataversioncontrol'
+fi
 
 print_error()
 {
@@ -32,7 +40,7 @@ fpm_build()
 {
 	print_info "Building $1..."
 	VERSION=$(python -c "import dvc; from dvc import VERSION; print(str(VERSION))")
-	fpm -s dir -f -t $1 --osxpkg-identifier-prefix com.dataversioncontrol -n dvc -v $VERSION -C $BUILD_DIR $INSTALL_DIR
+	fpm -s dir -f -t $1 $FPM_FLAGS -n dvc -v $VERSION -C $BUILD_DIR $INSTALL_DIR
 }
 
 cleanup()
@@ -43,11 +51,25 @@ cleanup()
 
 install_dependencies()
 {
+	print_info "Installing fpm..."
+	if command_exists dnf; then
+		sudo dnf install ruby-devel gcc make rpm-build
+	elif command_exists yum; then
+		sudo yum install ruby-devel gcc make rpm-build
+	elif command_exists apt-get; then
+		sudo apt-get update -y
+		sudo apt-get install ruby-dev build-essential rpm python-pip python-dev
+	elif command_exists brew; then
+		brew update
+		brew install ruby
+	else
+		echo "Unable to install fpm dependencies" && exit 1
+	fi
+
+	gem install --no-ri --no-rdoc fpm
+
 	print_info "Installing requirements..."
 	pip install -r requirements.txt
-
-	print_info "Installing fpm..."
-	gem install --no-ri --no-rdoc fpm
 
 	print_info "Installing pyinstaller..."
 	pip install pyinstaller
@@ -57,12 +79,21 @@ build_dvc()
 {
 	print_info "Building dvc binary..."
 	pyinstaller --onefile --additional-hooks-dir $(pwd)/scripts/hooks dvc/__main__.py --name dvc --distpath $BIN_DIR --specpath $BUILD_DIR
+
+	$BIN_DIR/dvc --help
 }
 
 cleanup
 install_dependencies
 build_dvc
-fpm_build osxpkg
+
+if [[ "$(uname)" == 'Linux' ]]; then
+	fpm_build rpm
+	fpm_build deb
+else
+	fpm_build osxpkg
+fi
+
 cleanup
 
-print_info "Successfully built dvc osx package"
+print_info "Successfully built dvc packages"
