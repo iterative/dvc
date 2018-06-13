78e5b166fee89adf80be75926be2f79a928f5dff
Ruslan Kuprieiev <kupruser@gmail.com>
2018-06-13 13:53:06 +0300

    dvc: replace StageNotFoundError with NotDvcFileError
    Fixes #760
    
    Signed-off-by: Ruslan Kuprieiev <kupruser@gmail.com>

--- dvc/command/repro.py
+++ dvc/command/repro.py
@@ -1,7 +1,7 @@
 import os
 
 from dvc.command.common.base import CmdBase
-from dvc.project import ReproductionError, StageNotFoundError
+from dvc.project import ReproductionError
 from dvc.stage import Stage
 
 
@@ -21,17 +21,6 @@ class CmdRepro(CmdBase):
             except ReproductionError as ex:
                 msg = 'Failed to reproduce \'{}\''.format(target)
                 self.project.logger.error(msg, ex)
-                ret = 1
-                break
-            except StageNotFoundError as ex:
-                if os.path.exists(target):
-                    msg = '\'{}\' is not a dvc file.'.format(target)
-                    dvcfile = target + Stage.STAGE_FILE_SUFFIX
-                    if os.path.exists(target) and os.path.isfile(dvcfile):
-                        msg += ' Maybe you meant \'{}\'?'.format(dvcfile)
-                else:
-                    msg = '\'{}\' does not exist.'.format(target)
-                self.project.logger.error(msg)
                 ret = 1
                 break
             except Exception as ex:
