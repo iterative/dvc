d5c015ff632c127b1c744f165ecce5ca716b7983
Ruslan Kuprieiev <kupruser@gmail.com>
2018-07-06 04:56:30 +0300

    dvc: bump to 0.10.0

--- dvc/__init__.py
+++ dvc/__init__.py
@@ -5,7 +5,7 @@ Make your data science projects reproducible and shareable.
 """
 import os
 
-VERSION_BASE = '0.9.7'
+VERSION_BASE = '0.10.0'
 __version__ = VERSION_BASE
 
 PACKAGEPATH = os.path.abspath(os.path.dirname(__file__))
