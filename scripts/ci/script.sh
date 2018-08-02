bd4bbf1438f6b1496061b1506f2742186f6d4027
Ruslan Kuprieiev <ruslan@iterative.ai>
2018-08-02 14:46:38 +0300

    build: disable binary build on Python 3.7
    Fixes #962
    
    Signed-off-by: Ruslan Kuprieiev <ruslan@iterative.ai>

--- scripts/ci/script.sh
+++ scripts/ci/script.sh
@@ -12,5 +12,12 @@ fi
 
 if [[ ! -z "$TRAVIS_TAG" ]]; then
 	./scripts/build_package.sh
-	./scripts/build_posix.sh
+
+        PY_VER=$(python -c 'import sys; print(sys.version_info[0:2])')
+        if [[ $PY_VER  == '(3, 7)' ]]; then
+            ./scripts/build_posix.sh
+        else
+            echo "Not building dvc binary package on Python 3.7, since it is "\
+                 "not supported by PyInstaller yet."
+        fi
 fi
