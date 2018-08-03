f02bf73587f9f684ec76dcf14b124f61748d9c17
Ruslan Kuprieiev <ruslan@iterative.ai>
2018-08-03 20:55:23 +0300

    reqs: only requre futures pkg on python 2.7
    Signed-off-by: Ruslan Kuprieiev <ruslan@iterative.ai>

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requires = [
     "jsonpath-rw==1.4.0",
     "reflink==0.2.0",
     "requests>=2.18.4",
-    "futures>=3.2.0",
+    "futures>=3.2.0; python_version == \"2.7\"",
 ]
 
 # Extra dependencies for remote integrations
