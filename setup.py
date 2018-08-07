c34555246044f07cd1a1968cff7ee8c2f307c993
Ruslan Kuprieiev <ruslan@iterative.ai>
2018-08-07 04:34:18 +0300

    setup: use proper env marker for futures dependency
    Fixes #968
    
    Signed-off-by: Ruslan Kuprieiev <ruslan@iterative.ai>

--- setup.py
+++ setup.py
@@ -23,10 +23,8 @@ install_requires = [
     "jsonpath-rw==1.4.0",
     "reflink==0.2.0",
     "requests>=2.18.4",
+    'futures; python_version == "2.7"',
 ]
-
-if sys.version_info[0] == 2:
-    install_requires.append("futures>=3.2.0")
 
 # Extra dependencies for remote integrations
 gs = [
