5071b5026d3dfe4973f52d9803b637d55a245cc8
Ruslan Kuprieiev <kupruser@gmail.com>
2018-09-23 09:56:19 +0300

    setup: exclude tests

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
     ],
-    packages=find_packages(include=['dvc']),
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
     url='http://dataversioncontrol.com',
     entry_points={
