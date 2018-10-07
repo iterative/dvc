881f1880165d15abbeb74c1a6daa57dd2c86dd00
Ruslan Kuprieiev <kupruser@gmail.com>
2018-10-07 03:15:56 +0300

    tests: run in parallel mode

--- tests/__main__.py
+++ tests/__main__.py
@@ -7,9 +7,5 @@ os.chdir(REPO_ROOT)
 os.putenv('PATH', '{}:{}'.format(os.path.join(REPO_ROOT, 'bin'), os.getenv('PATH')))
 os.putenv('DVC_HOME', REPO_ROOT)
 
-cmd = 'nosetests -v --process-timeout=200 --cover-inclusive --cover-erase --cover-package=dvc --with-coverage'
-# NOTE: run on CRON in single process mode to avoid problems in coverage
-# module for nose tests, where it sometimes doesn't parse .coveragerc.
-if not (os.getenv('TRAVIS') == 'true' and os.getenv('TRAVIS_EVENT_TYPE') == 'cron'):
-    cmd += ' --processes=-1'
+cmd = 'nosetests -v --processes=-1 --process-timeout=200 --cover-inclusive --cover-erase --cover-package=dvc --with-coverage'
 check_call(cmd, shell=True)
