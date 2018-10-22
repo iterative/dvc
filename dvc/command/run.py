0e0ba5fce173e93c7802bccbd19095162e754dd2
dixi3 <23087210+dixi3@users.noreply.github.com>
2018-10-23 02:21:27 +0300

    Fixed duplicated output

--- dvc/command/run.py
+++ dvc/command/run.py
@@ -8,6 +8,13 @@ class CmdRun(CmdBase):
             if self.args.yes:
                 self.project.prompt.default = True
 
+            if len(self.args.outs) != len(set(self.args.outs)):
+                s = set([x for x in self.args.outs if self.args.outs.count(x) > 1])
+                for i in s:
+                    print("Warning: Output \'" + i +
+                          "\' was specified more than one time")
+                self.args.outs = set(self.args.outs)
+
             self.project.run(cmd=' '.join(self.args.command),
                              outs=self.args.outs,
                              outs_no_cache=self.args.outs_no_cache,
