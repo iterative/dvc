53cd67f18d8fb6926b7afc156c32fe6e3ef5d078
Mr. Outis <mroutis@protonmail.com>
2018-10-26 21:01:50 -0500

    remove: Add confirmation when trying to purge

--- dvc/command/remove.py
+++ dvc/command/remove.py
@@ -4,8 +4,17 @@ from dvc.logger import Logger
 
 
 class CmdRemove(CmdBase):
+    def _is_purge_confirmed(self):
+        msg = (
+            'Are you sure you want to remove the following files'
+            ' with their outputs? {}'
+            .format(self.args.targets)
+        )
+
+        return self.args.purge and self.project.prompt.prompt(msg)
+
     def run(self):
-        outs_only = not self.args.purge
+        outs_only = not self._is_purge_confirmed()
         for target in self.args.targets:
             try:
                 self.project.remove(target, outs_only=outs_only)
