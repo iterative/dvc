5fe58187b8ddfe8d00913c792467b62fb0c301ea
prd <pawelredzynski@gmail.com>
2018-11-03 19:09:13 +0100

    remove Logger pseudo-integration test, add direct test

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -11,6 +11,11 @@ from dvc.main import main
 from tests.basic_env import TestDvc
 
 
+class StringContaining(str):
+    def __eq__(self, other):
+        return self in other
+
+
 class TestLogger(TestDvc):
     def test_config(self):
         Logger('debug')
@@ -37,6 +42,24 @@ class TestLogger(TestDvc):
             # This is not a tty, so it should not colorize anything
             self.assertEqual(msg, name)
 
+    @patch('sys.stderr.write')
+    @patch('sys.stdout.write')
+    def test_stderr(self, mock_stdout, mock_stderr):
+        error_message = 'error msg'
+        Logger.init()
+        Logger.error(error_message)
+        mock_stdout.assert_not_called()
+        mock_stderr.assert_any_call(StringContaining(error_message))
+
+    @patch('sys.stderr.write')
+    @patch('sys.stdout.write')
+    def test_stdout(self, mock_stdout, mock_stderr):
+        non_error_message = 'non-error message'
+        Logger.init()
+        Logger.info(non_error_message)
+        mock_stderr.assert_not_called()
+        mock_stdout.assert_any_call(StringContaining(non_error_message))
+
 
 class TestLoggerCLI(TestDvc):
     def test(self):
@@ -52,20 +75,3 @@ class TestLoggerCLI(TestDvc):
         args.verbose = True
         CmdBase._set_loglevel(args)
         self.assertEqual(Logger.logger().getEffectiveLevel(), logging.DEBUG)
-
-    @patch('sys.stderr.write')
-    @patch('sys.stdout.write')
-    def test_stderr(self, mock_stdout, mock_stderr):
-        ret = main(['config', 'non_existing_section.field'])
-        self.assertEqual(ret, 1)
-        mock_stdout.assert_not_called()
-        mock_stderr.assert_called()
-
-    @patch('sys.stderr.write')
-    @patch('sys.stdout.write')
-    def test_stdout(self, mock_stdout, mock_stderr):
-        main(['remote', 'add', 'test_repo', 'repo_url'])
-        ret = main(['remote', 'list'])
-        self.assertEqual(ret, 0)
-        mock_stdout.assert_called()
-        mock_stderr.assert_not_called()
