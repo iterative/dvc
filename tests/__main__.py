43c775fc6f4a58c144d4b644d492408593e4c4d8
Ruslan Kuprieiev <kupruser@gmail.com>
2018-11-17 03:16:36 +0200

    tests: define DVC_TEST env variable

--- tests/__main__.py
+++ tests/__main__.py
@@ -6,6 +6,7 @@ os.chdir(REPO_ROOT)
 
 os.putenv('PATH', '{}:{}'.format(os.path.join(REPO_ROOT, 'bin'), os.getenv('PATH')))
 os.putenv('DVC_HOME', REPO_ROOT)
+os.putenv('DVC_TEST', 'true')
 
 cmd = 'nosetests -v --processes=-1 --process-timeout=200 --cover-inclusive --cover-erase --cover-package=dvc --with-coverage'
 check_call(cmd, shell=True)
