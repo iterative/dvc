d617dcf20af70294cf6c0ba819ba364b2c6ae48c
pared <pawelredzynski@gmail.com>
2019-01-13 00:38:40 +0100

    test: state: add mtime_and_size check for directory

--- tests/unit/state.py
+++ tests/unit/state.py
@@ -4,8 +4,10 @@ from tests.basic_env import TestDvc
 
 class TestMtimeAndSizeReturnType(TestDvc):
     def test(self):
-        self.create(self.DATA, self.DATA_CONTENTS)
-        time, size = State.mtime_and_size(self.DATA)
+        file_time, file_size = State.mtime_and_size(self.DATA)
+        dir_time, dir_size = State.mtime_and_size(self.DATA_DIR)
 
-        self.assertIs(type(time), str)
-        self.assertIs(type(time), str)
+        self.assertIs(type(file_time), str)
+        self.assertIs(type(file_size), str)
+        self.assertIs(type(dir_time), str)
+        self.assertIs(type(dir_size), str)
