a2fb7da23d60dc2835fe4528a28be71fe584a212
Ruslan Kuprieiev <ruslan@iterative.ai>
2019-02-04 03:09:21 +0200

    test: set maximum RLIMIT_NPROC
    Signed-off-by: Ruslan Kuprieiev <ruslan@iterative.ai>

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,3 +9,6 @@ else:
     import resource
 
     resource.setrlimit(resource.RLIMIT_NOFILE, (2048, 2048))
+
+    nproc_soft, nproc_hard = resource.getrlimit(resource.RLIMIT_NPROC)
+    resource.setrlimit(resource.RLIMIT_NPROC, (nproc_hard, nproc_hard))
