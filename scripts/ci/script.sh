1eee7e32264057b90377f94a4779a0f40a86b3a4
Ruslan Kuprieiev <ruslan@iterative.ai>
2019-02-09 23:52:15 +0200

    travis: enable cron builds
    Signed-off-by: Ruslan Kuprieiev <ruslan@iterative.ai>

--- scripts/ci/script.sh
+++ scripts/ci/script.sh
@@ -11,22 +11,24 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && \
 	codecov
 fi
 
-if [[ ! -z "$TRAVIS_TAG" ]]; then
+if [[ -n "$TRAVIS_TAG" || "$TRAVIS_EVENT_TYPE" == "cron" ]]; then
 	./scripts/build_package.sh
 
-        # Test version
-        pip uninstall -y dvc
-        if [ -x "$(command -v dvc)" ]; then
-            echo "ERROR: dvc command already exists!!!"
-            exit 1
-        fi
+        if [[ -n "$TRAVIS_TAG" ]]; then
+            # Test version
+            pip uninstall -y dvc
+            if [ -x "$(command -v dvc)" ]; then
+                echo "ERROR: dvc command already exists!!!"
+                exit 1
+            fi
 
-        pip install dist/dvc-*.whl
-        if [[ "$(dvc --version)" != "$TRAVIS_TAG" ]]; then
-            echo "ERROR: 'dvc --version'$(dvc -V) doesn't match '$TRAVIS_TAG'"
-            exit 1
+            pip install dist/dvc-*.whl
+            if [[ "$(dvc --version)" != "$TRAVIS_TAG" ]]; then
+                echo "ERROR: 'dvc --version'$(dvc -V) doesn't match '$TRAVIS_TAG'"
+                exit 1
+            fi
+            pip uninstall -y dvc
         fi
-        pip uninstall -y dvc
 
         PY_VER=$(python -c 'import sys; print(sys.version_info[0:2])')
         if [[ "$PY_VER" != '(3, 7)' ]]; then
