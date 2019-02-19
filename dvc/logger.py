56254f59735d08448963112aa84bf2809bb1cb60
Dmitry <dmitry.petrov@nevesomo.com>
2019-02-18 17:25:07 -0800

    Redirect logs to file and mock log handler change

--- dvc/logger.py
+++ dvc/logger.py
@@ -303,7 +303,24 @@ def _visual_center(line, width):
 
 def setup_handlers():
     if not logger.handlers:
-        logger.handlers = [logging.StreamHandler(), logging.StreamHandler()]
+        temp_log_file = 'tmp.log'
+        logger.handlers = [logging.FileHandler(temp_log_file), logging.FileHandler(temp_log_file)]
+
+
+class MockHandlers(object):
+    def __init__(self, l, num=2):
+        self._logger = l
+        self._handlers = l.handlers
+        self._num = num
+
+    def __enter__(self):
+        self._logger.handlers = [logging.FileHandler('tmp{}.log'.format(i)) for i in range(self._num)]
+        return self
+
+    def __exit__(self, exc_type=None, exc_val=None, exc_tb=None):
+        for h in self._logger.handlers:
+            h.close()
+        self._logger.handlers = self._handlers
 
 
 logger = logging.getLogger("dvc")  # pylint: disable=invalid-name
