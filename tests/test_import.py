ff65c048dbee7d14c6bd212f84ef7dd4b3c89bcf
Dmitry <dmitry.petrov@nevesomo.com>
2019-02-18 17:25:07 -0800

    Redirect logs to file and mock log handler change

--- tests/test_import.py
+++ tests/test_import.py
@@ -53,23 +53,22 @@ class TestFailedImportMessage(TestDvc):
 
     @patch("dvc.command.imp.urlparse")
     def _test(self, imp_urlparse_patch):
-        logger.setup_handlers()
+        with logger.MockHandlers(logger.logger):
+            logger.logger.handlers[1].stream = StringIO()
+            page_address = "http://somesite.com/file_name"
 
-        logger.logger.handlers[1].stream = StringIO()
-        page_address = "http://somesite.com/file_name"
+            def dvc_exception(*args, **kwargs):
+                raise DvcException("message")
 
-        def dvc_exception(*args, **kwargs):
-            raise DvcException("message")
-
-        imp_urlparse_patch.side_effect = dvc_exception
-        main(["import", page_address])
-        self.assertIn(
-            "Error: failed to import "
-            "http://somesite.com/file_name. You could also try "
-            "downloading it manually and adding it with `dvc add` "
-            "command.",
-            logger.logger.handlers[1].stream.getvalue(),
-        )
+            imp_urlparse_patch.side_effect = dvc_exception
+            main(["import", page_address])
+            self.assertIn(
+                "Error: failed to import "
+                "http://somesite.com/file_name. You could also try "
+                "downloading it manually and adding it with `dvc add` "
+                "command.",
+                logger.logger.handlers[1].stream.getvalue(),
+            )
 
 
 class TestInterruptedDownload(TestDvc):
