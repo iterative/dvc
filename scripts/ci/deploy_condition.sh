fc646882adafcc039ad68cfea00a0a86a92388d9
Ruslan Kuprieiev <kupruser@gmail.com>
2019-03-17 14:38:51 +0200

    deploy: use xcode 8.3 for mac

--- scripts/ci/deploy_condition.sh
+++ scripts/ci/deploy_condition.sh
@@ -9,7 +9,7 @@ if [[ $python_ver != "2" ]]; then
     exit 0
 fi
 
-if [[ $TRAVIS_OS_NAME == "osx" && $TRAVIS_OSX_IMAGE != "xcode10.1" ]]; then
+if [[ $TRAVIS_OS_NAME == "osx" && $TRAVIS_OSX_IMAGE != "xcode8.3" ]]; then
     echo false
     exit 0
 fi
