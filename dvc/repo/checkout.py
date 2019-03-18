a93a3efb8f67876484e443b34be7d93ff31c5fc1
pared <pawelredzynski@gmail.com>
2019-03-18 17:24:11 +0100

    multiple exception handling

--- dvc/repo/checkout.py
+++ dvc/repo/checkout.py
@@ -21,7 +21,7 @@ def get_progress_callback(stages):
     try:
         total_files_num = get_all_files_numbers(stages)
         return ProgressCallback(total_files_num)
-    except CacheNotFoundException or NotImplementedError:
+    except (CacheNotFoundException, NotImplementedError):
         return None
 
 
