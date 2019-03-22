402eb2da0122932c0a8cd04cd1a9b88f6a0bc432
Ruslan Kuprieiev <kupruser@gmail.com>
2019-03-22 15:50:30 +0200

    travis: debug codecov failure

--- scripts/ci/script.sh
+++ scripts/ci/script.sh
@@ -8,6 +8,7 @@ python -mtests
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && \
       "$TRAVIS_SECURE_ENV_VARS" == "true" && \
       "$(python -c 'import sys; print(sys.version_info[0])')" == '2' ]]; then
+	pip install codecov
 	codecov
 fi
 
