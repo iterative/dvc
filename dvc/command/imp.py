ef4cc805e4d7ee98a238d83cf33d968e9eb95845
Lee Yi Jie Joel <joel@Lees-MacBook-Pro-2.local>
2019-03-21 19:09:49 -0700

    dvc: add documentation link for imp command

--- dvc/command/imp.py
+++ dvc/command/imp.py
@@ -28,7 +28,8 @@ class CmdImport(CmdBase):
 
 
 def add_parser(subparsers, parent_parser):
-    IMPORT_HELP = "Import files from URL."
+    IMPORT_HELP = "Import files from URL.\n \
+documentation: https://dvc.org/doc/commands-reference/import"
     import_parser = subparsers.add_parser(
         "import",
         parents=[parent_parser],
