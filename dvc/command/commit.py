c6de389f2307ba98e6f1a55b3310b9a80bd22d3e
Lee Yi Jie Joel <joel@Lees-MacBook-Pro-2.local>
2019-03-21 18:42:27 -0700

    dvc: add documentation link for commit command

--- dvc/command/commit.py
+++ dvc/command/commit.py
@@ -29,7 +29,8 @@ class CmdCommit(CmdBase):
 
 
 def add_parser(subparsers, parent_parser):
-    COMMIT_HELP = "Record changes to the repository."
+    COMMIT_HELP = "Record changes to the repository.\n \
+https://dvc.org/doc/commands-reference/commit"
     commit_parser = subparsers.add_parser(
         "commit",
         parents=[parent_parser],
