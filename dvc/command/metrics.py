d994bba7806738bbd61e6d0991702ede1f3f0369
Lee Yi Jie Joel <joel@Lees-MacBook-Pro-2.local>
2019-03-22 10:34:41 -0700

    dvc: fix minor formatting errors in modify_help command

--- dvc/command/metrics.py
+++ dvc/command/metrics.py
@@ -158,8 +158,10 @@ def add_parser(subparsers, parent_parser):
     metrics_add_parser.add_argument("path", help="Path to a metric file.")
     metrics_add_parser.set_defaults(func=CmdMetricsAdd)
 
-    METRICS_MODIFY_HELP = "Modify metric file options. \n\
-                          documentation: https://man.dvc.org/metrics-modify "
+    METRICS_MODIFY_HELP = (
+        "Modify metric file options. \n"
+        "documentation: https://man.dvc.org/metrics-modify "
+    )
     metrics_modify_parser = metrics_subparsers.add_parser(
         "modify",
         parents=[parent_parser],
