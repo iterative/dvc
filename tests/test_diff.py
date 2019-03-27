f0a9255ddbc0857c7ea4ce5eb9b95daf6bf8a92f
Zhanibek <janibek.kaimuldenov@gmail.com>
2019-03-27 12:59:42 +0600

    tests: fix test_diff with new logic

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -22,17 +22,18 @@ class TestDiff(TestDvc):
         self.git.index.commit("adds new_file")
 
     def test(self):
-        out = self.dvc.scm.diff(self.new_file, self.a_ref)
-        self.assertTrue(out["deleted_file"])
-        self.assertEqual("new_file.dvc", out["file_path"])
+        out = self.dvc.scm.get_diff_trees(self.a_ref)
+        self.assertFalse(out["equal"])
+        self.assertEqual(str(self.a_ref), out["b_ref"])
+        self.assertEqual(str(self.git.head.commit), out["a_ref"])
 
 
 class TestDiffRepo(TestDiff):
     def test(self):
         new_ref = self.git.head.commit
         msg = self.dvc.diff(self.new_file, a_ref=self.a_ref)
-        test_msg = "dvc diff for '{}' from ".format(self.new_file)
-        test_msg += "{} to {}\n\n".format(new_ref, self.a_ref)
+        test_msg = "dvc diff from {} to {}\n\n".format(new_ref, self.a_ref)
+        test_msg += "diff for {}\n".format(self.new_file)
         test_msg += "-new_file with md5 25fa8325e4e0eb8180445e42558e60bd\n"
         test_msg += "deleted file with size -8 Bytes"
         self.assertEqual(test_msg, msg)
@@ -59,26 +60,27 @@ class TestDiffDir(TestDvc):
         self.git.index.commit(message="adds data_dir with new_file")
 
     def test(self):
-        out = self.dvc.scm.diff(self.DATA_DIR, self.a_ref)
-        self.assertFalse(out["deleted_file"], "new file added checker failed")
-        self.assertEqual(
-            self.DATA_DIR + ".dvc",
-            out["file_path"],
-            "path to file/directory is incorrect",
-        )
+        out = self.dvc.scm.get_diff_trees(self.a_ref)
+        self.assertFalse(out["equal"])
+        self.assertEqual(str(self.a_ref), out["b_ref"])
+        self.assertEqual(str(self.git.head.commit), out["a_ref"])
 
 
 class TestDiffDirRepo(TestDiffDir):
     def test(self):
         new_ref = self.git.head.commit
         msg = self.dvc.diff(self.DATA_DIR, a_ref=self.a_ref)
-        test_msg = "dvc diff for '{}' from ".format(self.DATA_DIR)
+        test_msg = "dvc diff from ".format(self.DATA_DIR)
         test_msg += "{} to {}\n\n".format(new_ref, self.a_ref)
-        test_msg += "-data_dir with md5 d5782f3072167ad3a53ee80b92b30718\n"
-        test_msg += "+data_dir with md5 bff5a787d16460f32e9f2e62b183b1cc\n\n"
-        test_msg += "2 files didn't change, 0 files modified, 0 files added, "
+        test_msg += "-data_dir with md5 d5782f3072167ad3a53ee80b92b30718.dir\n"
+        test_msg += (
+            "+data_dir with md5 bff5a787d16460f32e9f2e62b183b1cc.dir\n\n"
+        )
+        test_msg += "2 files not changed, 0 files modified, 0 files added, "
         test_msg += "1 file deleted, size was decreased by 30 Bytes"
-        self.assertEqual(msg, test_msg)
+        self.assertEqual(
+            msg, test_msg, msg="msg is %s test_msg is %s" % (msg, test_msg)
+        )
 
 
 class TestDiffFileNotFound(TestDiffDir):
@@ -86,12 +88,6 @@ class TestDiffFileNotFound(TestDiffDir):
         super(TestDiffFileNotFound, self).setUp()
         self.unknown_file = "unknown_file_" + str(id(self))
 
-    def test(self):
-        with self.assertRaises(FileNotInCommitError):
-            self.dvc.scm.diff(self.unknown_file, self.a_ref)
-
-
-class TestDiffFileNotFoundRepo(TestDiffFileNotFound):
     def test(self):
         with self.assertRaises(FileNotInCommitError):
             self.dvc.diff(self.unknown_file, a_ref=self.a_ref)
