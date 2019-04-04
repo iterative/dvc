2699a47f3ea8070b4d0887963160b14e442ceda8
Ruslan Kuprieiev <ruslan@iterative.ai>
2019-04-04 20:33:39 +0300

    travis: build binary packages only for specific python and os versions
    Signed-off-by: Ruslan Kuprieiev <ruslan@iterative.ai>

--- scripts/ci/script.sh
+++ scripts/ci/script.sh
@@ -12,30 +12,38 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && \
 	codecov
 fi
 
-if [[ -n "$TRAVIS_TAG" || "$TRAVIS_EVENT_TYPE" == "cron" ]]; then
-	./scripts/build_package.sh
-
-        if [[ -n "$TRAVIS_TAG" ]]; then
-            # Test version
-            pip uninstall -y dvc
-            if [ -x "$(command -v dvc)" ]; then
-                echo "ERROR: dvc command already exists!!!"
-                exit 1
-            fi
-
-            pip install dist/dvc-*.whl
-            if [[ "$(dvc --version)" != "$TRAVIS_TAG" ]]; then
-                echo "ERROR: 'dvc --version'$(dvc -V) doesn't match '$TRAVIS_TAG'"
-                exit 1
-            fi
-            pip uninstall -y dvc
-        fi
-
-        PY_VER=$(python -c 'import sys; print(sys.version_info[0:2])')
-        if [[ "$PY_VER" != '(3, 7)' ]]; then
-            ./scripts/build_posix.sh
-        else
-            echo "Not building dvc binary package on Python 3.7, since it is "\
-                 "not supported by PyInstaller yet."
-        fi
+if [[ -z "$TRAVIS_TAG" && "$TRAVIS_EVENT_TYPE" != "cron" ]]; then
+    echo "Skipping building package."
+    exit 0
 fi
+
+./scripts/build_package.sh
+
+if [[ -n "$TRAVIS_TAG" ]]; then
+    # Test version
+    pip uninstall -y dvc
+    if [ -x "$(command -v dvc)" ]; then
+        echo "ERROR: dvc command already exists!!!"
+        exit 1
+    fi
+
+    pip install dist/dvc-*.whl
+    if [[ "$(dvc --version)" != "$TRAVIS_TAG" ]]; then
+        echo "ERROR: 'dvc --version'$(dvc -V) doesn't match '$TRAVIS_TAG'"
+        exit 1
+    fi
+    pip uninstall -y dvc
+fi
+
+PY_VER=$(python -c 'import sys; print(sys.version_info[0:2])')
+if [[ "$PY_VER" != '(2, 7)' ]]; then
+    echo "Skipping building binary package because of a wrong python version."
+    exit 0
+fi
+
+if [[ "$TRAVIS_OS_NAME" == "osx" && "$TRAVIS_OSX_IMAGE" != "xcode8.3" ]]; then
+    echo "Skipping building binary package because of a wrong xcode version."
+    exit 0
+fi
+
+./scripts/build_posix.sh
