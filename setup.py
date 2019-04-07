354712e4a4c3ae80540475f205590a222cfd3719
Hugo <hugovk@users.noreply.github.com>
2019-04-07 17:05:18 +0300

    Add python_requires to help pip

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ setup(
         ':python_version=="2.7"': ["futures"],
     },
     keywords="data science, data version control, machine learning",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 2",
