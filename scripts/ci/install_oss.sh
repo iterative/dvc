47400bba6f9de3345f1086fcd85948a071c32957
Ruslan Kuprieiev <ruslan@iterative.ai>
2019-05-09 15:14:56 +0300

    travis: use iterative's fork of oss-emulator
    Just to keep things in-house.
    
    Signed-off-by: Ruslan Kuprieiev <ruslan@iterative.ai>

--- scripts/ci/install_oss.sh
+++ scripts/ci/install_oss.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-git clone https://github.com/nanaya-tachibana/oss-emulator.git
+git clone https://github.com/iterative/oss-emulator.git
 sudo docker image build -t oss:1.0 oss-emulator
 sudo docker run --detach --restart always -p 8880:8880 --name oss-emulator oss:1.0
 echo "export OSS_ENDPOINT='localhost:8880'" >> env.sh
