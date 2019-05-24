ee6a0b28750de0f4e787fc7ba92fdd1ec599193e
Tadas Audinis <ta15668@my.bristol.ac.uk>
2019-05-23 17:12:36 -0700

    tests: resolve deprecation warnings (#2028)
    * tests: resolve deprecation warnings
    
    Fixes #2021
    
    * tests: resolve deprecation warnings
    
    Fixes #2021

--- tests/func/test_checkout.py
+++ tests/func/test_checkout.py
@@ -428,10 +428,10 @@ class TestCheckoutShouldHaveSelfClearingProgressBar(TestDvc):
         finish_bar = progress_bars[-1]
 
         self.assertEqual(4, len(update_bars))
-        self.assertRegexpMatches(progress_bars[0], ".*\\[#{7} {23}\\] 25%.*")
-        self.assertRegexpMatches(progress_bars[1], ".*\\[#{15} {15}\\] 50%.*")
-        self.assertRegexpMatches(progress_bars[2], ".*\\[#{22} {8}\\] 75%.*")
-        self.assertRegexpMatches(progress_bars[3], ".*\\[#{30}\\] 100%.*")
+        assert re.search(".*\\[#{7} {23}\\] 25%.*", progress_bars[0])
+        assert re.search(".*\\[#{15} {15}\\] 50%.*", progress_bars[1])
+        assert re.search(".*\\[#{22} {8}\\] 75%.*", progress_bars[2])
+        assert re.search(".*\\[#{30}\\] 100%.*", progress_bars[3])
 
         self.assertCaretReturnFollowsEach(update_bars)
         self.assertNewLineFollows(finish_bar)
