f004a84e7cfcb23fa267f406c482602c891d3fda
Jorge Orpinel <jorge@orpinel.com>
2019-06-22 00:23:25 -0500

    tests: fix failing test_add (due to extra 's' in word)
    To close #2112

--- tests/func/test_add.py
+++ tests/func/test_add.py
@@ -96,7 +96,7 @@ class TestAddCmdDirectoryRecursive(TestDvc):
         warning = (
             "You are adding a large directory 'large-dir' recursively,"
             " consider tracking it as a whole instead.\n"
-            "{purple}HINT:{nc} Remove the generated DVC-files and then"
+            "{purple}HINT:{nc} Remove the generated DVC-file and then"
             " run {cyan}dvc add large-dir{nc}".format(
                 purple=colorama.Fore.MAGENTA,
                 cyan=colorama.Fore.CYAN,
