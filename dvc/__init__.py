12c7689a74f3faa126ebf4c14369a5c173661bd4
Ruslan Kuprieiev <ruslan@iterative.ai>
2019-06-24 00:23:19 +0300

    dvc: update paramiko requirements
    Fixes #2155
    
    No longer need to explicitly ignore paramiko warnings.
    
    Big thanks to @cowlinator for the heads-up!
    
    Signed-off-by: Ruslan Kuprieiev <ruslan@iterative.ai>

--- dvc/__init__.py
+++ dvc/__init__.py
@@ -19,18 +19,3 @@ dvc.logger.setup()
 # we can simply ignore them so that they don't show up when you are using dvc.
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
-
-# Ignore paramiko's warning: https://github.com/paramiko/paramiko/issues/1386.
-# This only affects paramiko 2.4.2 and should be fixed in the next version.
-# Cryptography developers decided that it is a brilliant idea not to inherit
-# from DeprecationWarning [1] because it is invisible by default, and decided
-# to spam everyone instead. So it is their fault and not paramiko's.
-#
-# [1] https://github.com/pyca/cryptography/blob/2.6.1/src/cryptography/
-#     utils.py#L14
-try:
-    from cryptography.utils import CryptographyDeprecationWarning
-
-    warnings.simplefilter("ignore", CryptographyDeprecationWarning)
-except ImportError:
-    pass
