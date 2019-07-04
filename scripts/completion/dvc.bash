0b394ec841c68357cec6eb1cfa2cb20b636d3739
Naba7 <dashnabanita@gmail.com>
2019-07-04 19:12:08 +0530

    autocompletion of commands and files

--- scripts/completion/dvc.bash
+++ scripts/completion/dvc.bash
@@ -58,23 +58,118 @@ _dvc_version=''
 #       x="hello"
 #       ${!x} ->  ${hello} ->  "world"
 #
-_dvc () {
-  local word='${COMP_WORDS[COMP_CWORD]}'
+_dvc ()
+{
+    local cur prev opts
+            COMPREPLY=()
+            cur="${COMP_WORDS[COMP_CWORD]}"
+            prev="${COMP_WORDS[COMP_CWORD-1]}"
+            commands="add cache checkout commit config destroy diff fetch get-url get gc
+                 import-url import init install lock metrics move pipeline pull push
+                 remote remove repro root run status unlock unprotect version"
+            global_opts="-h --help -q --quiet -v --verbose"
+            opts="-h --help -V --version"
+                 case "${COMP_CWORD}" in
+                      1)
+                         COMPREPLY=($(compgen -W "${commands} ${opts}" -- ${cur}))
+                         ;;
+                      2)
+                        case "${prev}" in
+                            add)
+                               COMPREPLY=($(compgen -W "-R --recursive -f --file --no-commit ${global_opts} $(ls ./)" -- ${cur}))
+                               ;;
+                            cache)
+                               COMPREPLY=($(compgen -W "${global_opts}" -- ${cur}))
+                               ;;
+                            checkout)
+                               COMPREPLY=($(compgen -W "-f --force -d --with-deps -R --recursive ${global_opts} $(ls ./*.dvc)" -- ${cur}))
+                               ;;
+                            commit)
+                               COMPREPLY=($(compgen -W "-f --force -d --with-deps -R --recursive ${global_opts} $(ls ./*.dvc)" -- ${cur}))
+                               ;;
+                            config)
+                               COMPREPLY=($(compgen -W "-u --unset --local --system --global ${global_opts}" -- ${cur}))
+                               ;;
+                            destroy)
+                               COMPREPLY=($(compgen -W "-f --force ${global_opts}" -- ${cur}))
+                               ;;
+                            diff)
+                               COMPREPLY=($(compgen -W "-t --target ${global_opts}" -- ${cur}))
+                               ;;
+                            fetch)
+                               COMPREPLY=($(compgen -W "--show-checksums -j --jobs -r --remote -a --all-branches -T --all-tags -d --with-deps -R --recursive ${global_opts} $(ls ./*.dvc)" -- ${cur}))
+                               ;;
+                            get-url)
+                               COMPREPLY=($(compgen -W "${global_opts}" -- ${cur}))
+                               ;;
+                            get)
+                               COMPREPLY=($(compgen -W "-o --out --rev ${global_opts}" -- ${cur}))
+                               ;;
+                            gc)
+                               COMPREPLY=($(compgen -W "-a --all-branches -T --all-tags -c --cloud -r --remote -f --force -p --projects -j --jobs ${global_opts}" -- ${cur}))
+                               ;;
+                            import)
+                               COMPREPLY=($(compgen -W "-o --out --rev ${global_opts}" -- ${cur}))
+                               ;;
+                            import-url)
+                               COMPREPLY=($(compgen -W "--resume -f --file ${global_opts}" -- ${cur}))
+                               ;;
+                            init)
+                               COMPREPLY=($(compgen -W "--no-scm -f --force ${global_opts}" -- ${cur}))
+                               ;;
+                            install)
+                               COMPREPLY=($(compgen -W "${global_opts}" -- ${cur}))
+                               ;;
+                            lock)
+                               COMPREPLY=($(compgen -W "${global_opts} $(ls ./*.dvc)" -- ${cur}))
+                               ;;
+                            metrics)
+                               COMPREPLY=($(compgen -W "${global_opts}" -- ${cur}))
+                               ;;
+                            move)
+                               COMPREPLY=($(compgen -W "${global_opts} $(ls ./)" -- ${cur}))
+                               ;;
+                            pipeline)
+                               COMPREPLY=($(compgen -W " ${global_opts}" -- ${cur}))
+                               ;;
+                            pull)
+                               COMPREPLY=($(compgen -W "--show-checksums -j --jobs -r --remote -a --all-branches -T --all-tags -f --force -d --with-deps -R --recursive ${global_opts} $(ls ./*.dvc)" -- ${cur}))
+                               ;;
+                            push)
+                               COMPREPLY=($(compgen -W "--show-checksums -j --jobs -r --remote -a --all-branches -T --all-tags -d --with-deps -R --recursive ${global_opts} $(ls ./*.dvc)" -- ${cur}))
+                               ;;
+                            remove)
+                               COMPREPLY=($(compgen -W "-o --outs -p --purge -f --force ${global_opts} $(ls ./*.dvc)" -- ${cur}))
+                               ;;
+                            remote)
+                               COMPREPLY=($(compgen -W "${global_opts}" -- ${cur}))
+                               ;;
+                            repro)
+                               COMPREPLY=($(compgen -W "-f --force -s --single-item -c --cwd -m --metrics --dry -i --interactive -p --pipeline -P --all-pipelines --ignore-build-cache --no-commit -R --recursive --downstream ${global_opts} $(ls ./*.dvc)" -- ${cur}))
+                               ;;
+                            root)
+                               COMPREPLY=($(compgen -W "${global_opts}" -- ${cur}))
+                               ;;
+                            run)
+                               COMPREPLY=($(compgen -W "--no-exec -f --file -c --cwd -d --deps -o --outs -O --outs-no-cache --outs-persist --outs-persist-no-cache -m --metrics -M --metrics-no-cache -y --yes --overwrite-dvcfile --ignore-build-cache --remove-outs --no-commit -w --wdir ${global_opts}" -- ${cur}))
+                               ;;
+                            status)
+                               COMPREPLY=($(compgen -W "--show-checksums -j --jobs -r --remote -a --all-branches -T --all-tags -d --with-deps -c --cloud ${global_opts} $(ls ./*.dvc)" -- ${cur}))
+                               ;;
+                            unlock)
+                               COMPREPLY=($(compgen -W "${global_opts} $(ls ./*.dvc)" -- ${cur}))
+                               ;;
+                            unprotect)
+                               COMPREPLY=($(compgen -W "${global_opts} $(ls ./)" -- ${cur}))
+                               ;;
+                            version)
+                               COMPREPLY=($(compgen -W "${global_opts}" -- ${cur}))
+                               ;;
 
-  COMPREPLY=()
-
-  if [ '${COMP_CWORD}' -eq 1 ]; then
-    case '$word' in
-      -*) COMPREPLY=($(compgen -W '$_dvc_options' -- '$word')) ;;
-      *)  COMPREPLY=($(compgen -W '$_dvc_commands' -- '$word')) ;;
-    esac
-  elif [ '${COMP_CWORD}' -eq 2 ]; then
-    local options_list='_dvc_${COMP_WORDS[1]}'
-
-    COMPREPLY=($(compgen -W '$_dvc_global_options ${!options_list}' -- '$word'))
-  fi
-
-  return 0
+                        esac
+                        ;;
+                  esac
+                  ;;
 }
 
 complete -F _dvc dvc
