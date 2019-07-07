145879664680553be47dfecff9cdc4df77c6adce
Naba7 <dashnabanita@gmail.com>
2019-07-07 23:25:12 +0530

    changed

--- scripts/completion/dvc.bash
+++ scripts/completion/dvc.bash
@@ -17,6 +17,7 @@ _dvc_global_options='-h --help -q --quiet -v --verbose'
 
 _dvc_add='-R --recursive -f --file --no-commit $(compgen -G *)'
 _dvc_cache=''
+_dvc_cache_dir=' --global --system --local -u --unset'
 _dvc_checkout='-f --force -d --with-deps -R --recursive $(compgen -G *.dvc)'
 _dvc_commit='-f --force -d --with-deps -R --recursive $(compgen -G *.dvc)'
 _dvc_config='-u --unset --local --system --global'
@@ -32,11 +33,22 @@ _dvc_init='--no-scm -f --force'
 _dvc_install=''
 _dvc_lock='$(compgen -G *.dvc)'
 _dvc_metrics=''
+_dvc_metrics_add='-t --type -x --xpath $(compgen -G *)'
+_dvc_metrics_modify='-t --type -x --xpath $(compgen -G *)'
+_dvc_metrics_remove='$(compgen -G *)'
+_dvc_metrics_show='$(compgen -t --type -x --xpath -a --all-branches -T --all-tags -R --recursive $(compgen -G *)'
 _dvc_move='$(compgen -G *)'
 _dvc_pipeline=''
+_dvc_pipeline_list=''
+_dvc_pipeline_show='-c --commands -o --outs --ascii --dot --tree -l --locked $(compgen -G *.dvc)'
 _dvc_pull='--show-checksums -j --jobs -r --remote -a --all-branches -T --all-tags -f --force -d --with-deps -R --recursive $(compgen -G *.dvc)'
 _dvc_push='--show-checksums -j --jobs -r --remote -a --all-branches -T --all-tags -d --with-deps -R --recursive $(compgen -G *.dvc)'
 _dvc_remote=''
+_dvc_remote_add='--global --system --local -d --default -f --force'
+_dvc_remote_default='--global --system --local -u --unset'
+_dvc_remote_list='--global --system --local'
+_dvc_remote_modify='--global --system --local -u --unset'
+_dvc_remote_remove='--global --system --local'
 _dvc_remove='-o --outs -p --purge -f --force $(compgen -G *.dvc)'
 _dvc_repro='-f --force -s --single-item -c --cwd -m --metrics --dry -i --interactive -p --pipeline -P --all-pipelines --ignore-build-cache --no-commit -R --recursive --downstream $(compgen -G *.dvc)'
 _dvc_root=''
@@ -59,19 +71,23 @@ _dvc_version=''
 #       ${!x} ->  ${hello} ->  "world"
 #
 _dvc () {
-  local word='${COMP_WORDS[COMP_CWORD]}'
+  local word="${COMP_WORDS[COMP_CWORD]}"
 
   COMPREPLY=()
 
-  if [ '${COMP_CWORD}' -eq 1 ]; then
-    case '$word' in
-      -*) COMPREPLY=($(compgen -W '$_dvc_options' -- '$word')) ;;
-      *)  COMPREPLY=($(compgen -W '$_dvc_commands' -- '$word')) ;;
+  if [ "${COMP_CWORD}" -eq 1 ]; then
+    case "$word" in
+      -*) COMPREPLY=($(compgen -W "$_dvc_options" -- "$word")) ;;
+      *)  COMPREPLY=($(compgen -W "$_dvc_commands" -- "$word")) ;;
     esac
-  elif [ '${COMP_CWORD}' -eq 2 ]; then
-    local options_list='_dvc_${COMP_WORDS[1]}'
+  elif [ "${COMP_CWORD}" -eq 2 ]; then
+    local options_list="_dvc_${COMP_WORDS[1]}"
 
-    COMPREPLY=($(compgen -W '$_dvc_global_options ${!options_list}' -- '$word'))
+    COMPREPLY=($(compgen -W "$_dvc_global_options ${!options_list}" -- "$word")) ;;
+  elif [ "${COMP_CWORD}" -eq 3 ]; then
+    local options_list="_dvc_${COMP_WORDS[1]}_${COMP_WORDS[2]}"
+
+    COMPREPLY=($(compgen -W "$_dvc_global_options ${!options_list} $(ls ./)" --"$word")) ;;
   fi
 
   return 0
