fa89e84baac21b232c8fc182cd68b08f8e8619f2
Ruslan Kuprieiev <kupruser@gmail.com>
2019-07-10 12:12:11 +0300

    travis: use stages (#2249)
    Signed-off-by: Ruslan Kuprieiev <kupruser@gmail.com>

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -3,9 +3,24 @@
 set -e
 
 if [ ! -d "dvc" ]; then
-	echo "Please run this script from repository root"
-	exit 1
+    echo "Please run this script from repository root"
+    exit 1
 fi
 
 python setup.py sdist
 python setup.py bdist_wheel --universal
+
+# Make sure we have a correct version
+if [[ -n "$TRAVIS_TAG" ]]; then
+    pip uninstall -y dvc
+    if ! [ -x "$(command -v dvc)" ]; then
+        echo "ERROR: dvc command still exists! Unable to verify dvc version."
+	exit 1
+    fi
+    pip install dist/dvc-*.whl
+    if [[ "$(dvc --version)" != "$TRAVIS_TAG" ]]; then
+        echo "ERROR: 'dvc --version'$(dvc -V) doesn't match '$TRAVIS_TAG'"
+        exit 1
+    fi
+    pip uninstall -y dvc
+fi
