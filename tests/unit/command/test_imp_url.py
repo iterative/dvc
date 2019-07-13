13335a7beb3d6950da30b79f9822351113f3b197
Ruslan Kuprieiev <ruslan@iterative.ai>
2019-07-14 00:55:22 +0300

    tests: import-url: rewrite and move error msg test
    This one was failing randomly.
    
    Signed-off-by: Ruslan Kuprieiev <ruslan@iterative.ai>

--- tests/unit/command/test_imp_url.py
+++ tests/unit/command/test_imp_url.py
@@ -1,4 +1,7 @@
+import logging
+
 from dvc.cli import parse_args
+from dvc.exceptions import DvcException
 from dvc.command.imp_url import CmdImportUrl
 
 
@@ -14,3 +17,21 @@ def test_import_url(mocker, dvc_repo):
     assert cmd.run() == 0
 
     m.assert_called_once_with("src", out="out", resume=True, fname="file")
+
+
+def test_failed_import_url(mocker, caplog, dvc_repo):
+    cli_args = parse_args(["import-url", "http://somesite.com/file_name"])
+    assert cli_args.func == CmdImportUrl
+
+    cmd = cli_args.func(cli_args)
+    with mocker.patch.object(
+        cmd.repo, "imp_url", side_effect=DvcException("error")
+    ):
+        with caplog.at_level(logging.ERROR, logger="dvc"):
+            assert cmd.run() == 1
+            expected_error = (
+                "failed to import http://somesite.com/file_name. "
+                "You could also try downloading it manually and "
+                "adding it with `dvc add` command."
+            )
+            assert expected_error in caplog.text
