ef013c8472c6329cc183f33d6677a60e58feef35
Ivan Shcheklein <shcheklein@gmail.com>
2019-07-20 15:51:11 -0700

    url info: minor fix typo in a comment

--- dvc/path_info.py
+++ dvc/path_info.py
@@ -186,7 +186,7 @@ class URLInfo(object):
     def __getattr__(self, name):
         # When deepcopy is called, it creates and object without __init__,
         # self.parsed is not initialized and it causes infinite recursion.
-        # More on this special ca   sing here:
+        # More on this special casing here:
         # https://stackoverflow.com/a/47300262/298182
         if name.startswith("__"):
             raise AttributeError(name)
