cbc5e1b52d3cddc14e9b80550fbf05e2b9933405
joshua postel <jpostel1@ford.com>
2019-08-21 14:10:53 -0400

    setup.py: add gssapi requirement
    Fixes #2327

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ gs = ["google-cloud-storage==1.13.0"]
 s3 = ["boto3==1.9.115"]
 azure = ["azure-storage-blob==2.1.0"]
 oss = ["oss2==2.6.1"]
-ssh = ["paramiko>=2.5.0"]
+ssh = ["paramiko[gssapi]>=2.5.0"]
 hdfs = ["pyarrow==0.14.0"]
 all_remotes = gs + s3 + azure + ssh + oss
 
