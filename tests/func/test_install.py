b3e4ff7cbae3206f87863a9c0db51163f9bb597b
Ruslan Kuprieiev <kupruser@gmail.com>
2019-09-09 05:10:30 +0300

    test: install: don't forget to adjust hook tests

--- tests/func/test_install.py
+++ tests/func/test_install.py
@@ -39,7 +39,7 @@ class TestInstall(object):
         expected_script = (
             "#!/bin/sh\n"
             "echo hello\n"
-            '[ "$3" == "0" ]'
+            '[ "$3" = "0" ]'
             ' || [ -z "$(git ls-files .dvc)" ]'
             " || exec dvc checkout\n"
         )
@@ -53,7 +53,7 @@ class TestInstall(object):
 
         expected_script = (
             "#!/bin/sh\n"
-            '[ "$3" == "0" ]'
+            '[ "$3" = "0" ]'
             ' || [ -z "$(git ls-files .dvc)" ]'
             " || exec dvc checkout\n"
         )
