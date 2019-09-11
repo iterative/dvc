5700b0c5eebcec1646abf3c04c555e4ea87a4dfd
pawel <pawelredzynski@gmail.com>
2019-09-11 15:48:23 +0200

    working wtih g-r-m build with -e

--- scripts/hooks/hook-google.resumable_media.py
+++ scripts/hooks/hook-google.resumable_media.py
@@ -1,4 +1,5 @@
 from PyInstaller.utils.hooks import copy_metadata
 
-datas = copy_metadata("google-resumable-media")
+datas = copy_metadata("six")
 datas += copy_metadata("requests")
+datas += copy_metadata("google-resumable-media")
