617a0e731826c6d0780adc936c97963810a3e16a
Paweł Redzyński <pawelredzynski@gmail.com>
2019-09-12 09:37:43 +0200

    Update setup.py
    Co-Authored-By: Ruslan Kuprieiev <kupruser@gmail.com>

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ else:
 
 # Extra dependencies for remote integrations
 
-gs = ["google-resumable-media>=0.4.0", "google-cloud-storage>=1.19.0"]
+gs = ["google-cloud-storage==1.19.0"]
 s3 = ["boto3==1.9.115"]
 azure = ["azure-storage-blob==2.1.0"]
 oss = ["oss2==2.6.1"]
