a865cfc24344620997e4d848af03d9365c440a05
Mr. Outis <mroutis@protonmail.com>
2019-09-16 18:47:44 -0500

    test pull and fetch

--- tests/func/test_import.py
+++ tests/func/test_import.py
@@ -3,6 +3,8 @@ import filecmp
 
 from tests.utils import trees_equal
 
+from dvc.stage import Stage
+
 
 def test_import(repo_dir, git, dvc_repo, erepo):
     src = erepo.FOO
@@ -39,3 +41,37 @@ def test_import_rev(repo_dir, git, dvc_repo, erepo):
     with open(dst, "r+") as fobj:
         assert fobj.read() == "branch"
     assert git.git.check_ignore(dst)
+
+
+def test_fetching_imported_stage(dvc_repo, erepo):
+    src = erepo.FOO
+    dst = erepo.FOO + "_imported"
+
+    dvc_repo.imp(erepo.root_dir, src, dst)
+
+    dst_stage = Stage.load(dvc_repo, 'foo_imported.dvc')
+    dst_cache = dst_stage.outs[0].cache_path
+
+    os.remove(dst_cache)
+
+    dvc_repo.fetch(['foo_imported.dvc'])
+
+    assert os.path.isfile(dst_cache)
+
+
+def test_pulling_imported_stage(dvc_repo, erepo):
+    src = erepo.FOO
+    dst = erepo.FOO + "_imported"
+
+    dvc_repo.imp(erepo.root_dir, src, dst)
+
+    dst_stage = Stage.load(dvc_repo, 'foo_imported.dvc')
+    dst_cache = dst_stage.outs[0].cache_path
+
+    os.remove(dst)
+    os.remove(dst_cache)
+
+    dvc_repo.pull(['foo_imported.dvc'])
+
+    assert os.path.isfile(dst)
+    assert os.path.isfile(dst_cache)
