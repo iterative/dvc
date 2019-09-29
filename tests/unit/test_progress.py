7fefafe9d4096c2169aad53415d631c8f49540c3
Casper da Costa-Luis <casper.dcl@physics.org>
2019-09-29 16:37:29 +0100

    fix tests

--- tests/unit/test_progress.py
+++ tests/unit/test_progress.py
@@ -1,5 +1,6 @@
 import logging
 from dvc.progress import Tqdm
+import sys
 
 
 def test_quiet(caplog, capsys):
@@ -10,6 +11,14 @@ def test_quiet(caplog, capsys):
         assert out_err.out == ""
         assert out_err.err == ""
     with caplog.at_level(logging.INFO, logger="dvc"):
+        for _ in Tqdm(range(10)):
+            pass
+        out_err = capsys.readouterr()
+        assert out_err.out == ""
+        assert out_err.err == ""
+
+        # simulate interactive terminal
+        sys.stderr.isatty = lambda: True
         for _ in Tqdm(range(10)):
             pass
         out_err = capsys.readouterr()
