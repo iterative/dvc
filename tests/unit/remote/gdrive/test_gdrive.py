917f5c0d4ad12d9629d9848d25107bb26eb9c536
Max Risuhin <risuhin.max@gmail.com>
2019-10-23 12:17:56 -0700

    Fix tests

--- tests/unit/remote/gdrive/test_gdrive.py
+++ tests/unit/remote/gdrive/test_gdrive.py
@@ -2,14 +2,14 @@ import mock
 from dvc.remote.gdrive import RemoteGDrive
 
 
-@mock.patch("dvc.remote.gdrive.RemoteGDrive.drive")
+@mock.patch("dvc.remote.gdrive.RemoteGDrive.init_drive")
 def test_init_drive(repo):
     url = "gdrive://root/data"
     gdrive = RemoteGDrive(repo, {"url": url})
     assert str(gdrive.path_info) == url
 
 
-@mock.patch("dvc.remote.gdrive.RemoteGDrive.drive")
+@mock.patch("dvc.remote.gdrive.RemoteGDrive.init_drive")
 def test_init_folder_id(repo):
     url = "gdrive://folder_id/data"
     gdrive = RemoteGDrive(repo, {"url": url})
