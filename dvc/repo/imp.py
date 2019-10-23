bfc0a3596f9ea633dc7037de07be958b7f322525
Srividya Krishnakumar <31186638+SrividyaKK@users.noreply.github.com>
2019-10-23 06:24:10 +0530

    Added path.lstrip for / in imports (#2651)

--- dvc/repo/imp.py
+++ dvc/repo/imp.py
@@ -2,5 +2,6 @@ def imp(self, url, path, out=None, rev=None):
     erepo = {"url": url}
     if rev is not None:
         erepo["rev"] = rev
+        path = path.lstrip("/")
 
     return self.imp_url(path, out=out, erepo=erepo, locked=True)
