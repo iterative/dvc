4efd048796564e366b1dfa7a4302aa5d67932481
Ruslan Kuprieiev <ruslan@iterative.ai>
2019-11-04 00:05:44 +0200

    test: try removing get_url test

--- tests/func/test_get_url.py
+++ tests/func/test_get_url.py
@@ -1,14 +1,10 @@
 from __future__ import unicode_literals
 
 import os
-import boto3
 import filecmp
 
 import pytest
 
-from moto import mock_s3
-
-from dvc.remote import RemoteS3
 from dvc.repo import Repo
 from dvc.utils import makedirs
 
@@ -36,29 +32,3 @@ def test_get_url_to_dir(dname, repo_dir):
 
     assert os.path.isdir(dname)
     assert filecmp.cmp(repo_dir.DATA, dst, shallow=False)
-
-
-@pytest.mark.parametrize("dst", [".", "./from"])
-def test_get_url_from_non_local_path_to_dir_and_file(repo_dir, dst):
-    with mock_s3():
-        file_name = "from"
-        file_content = "data"
-        base_info = RemoteS3.path_cls("s3://bucket/path")
-        from_info = base_info / file_name
-
-        s3 = boto3.client("s3")
-        s3.create_bucket(Bucket=from_info.bucket)
-        s3.put_object(
-            Bucket=from_info.bucket, Key=from_info.path, Body=file_content
-        )
-
-        Repo.get_url(from_info.url, dst)
-
-        result_path = (
-            os.path.join(dst, file_name) if os.path.isdir(dst) else dst
-        )
-
-        assert os.path.exists(result_path)
-        assert os.path.isfile(result_path)
-        with open(result_path, "r") as fd:
-            assert fd.read() == file_content
