49c0b84a6e4f0a034f5ed4f4acbd0ebf8f1366e3
Ruslan Kuprieiev <kupruser@gmail.com>
2019-11-04 20:21:14 +0200

    [TEST] DO NOT MERGE

--- dvc/__main__.py
+++ dvc/__main__.py
@@ -4,7 +4,31 @@ from __future__ import unicode_literals
 
 import sys
 
+
+
+
+
+
+
+
+
+
+
 from dvc.main import main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
