3ba4b8bd4d952546a1bb5e847750cdac33efb797
n3hrox <n3hrox@gmail.com>
2019-11-10 22:10:19 +0100

    remove unnecessary pip check

--- dvc/utils/pkg.py
+++ dvc/utils/pkg.py
@@ -1,17 +1,7 @@
-from dvc.utils import is_binary
-
-
-def check_build_patch():
+def get_package_manager():
     try:
         from .build import PKG  # file created during dvc build
 
         return PKG
     except ImportError:
         return None
-
-
-def get_package_manager():
-    if not is_binary():
-        return "pip"
-
-    return check_build_patch()
