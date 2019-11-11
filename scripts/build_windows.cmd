4bde281b4e89df1147c23965d37daab0e8783689
Ruslan Kuprieiev <kupruser@gmail.com>
2019-11-11 03:31:48 +0200

    build: windows: remove unneeded quotes
    Per https://github.com/iterative/dvc/pull/2773#discussion_r344529924

--- scripts/build_windows.cmd
+++ scripts/build_windows.cmd
@@ -21,7 +21,7 @@ where iscc
 if %errorlevel% neq 0 (echo Error: Couldn't find Inno Setup compiler. && goto :error)
 
 echo ====== Installing requirements... ======
-echo 'PKG = "exe"' > dvc\utils\build.py
+echo PKG = "exe" > dvc\utils\build.py
 call pip install .[all] || goto :error
 call pip install psutil || goto :error
 call dvc pull || goto :error
