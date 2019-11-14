9fa18a43d8d6cf281d8489e0c2bfdb8a5e89ea23
Max Risuhin <risuhin.max@gmail.com>
2019-11-14 03:26:36 -0800

    Support GDrive as remote

---
+++ tests/unit/remote/gdrive/test_gdrive.py
@@ -0,0 +1,9 @@
+import mock
+from dvc.remote.gdrive import RemoteGDrive
+
+
+@mock.patch("dvc.remote.gdrive.RemoteGDrive.init_drive")
+def test_init_drive(repo):
+    url = "gdrive://root/data"
+    gdrive = RemoteGDrive(repo, {"url": url})
+    assert str(gdrive.path_info) == url
