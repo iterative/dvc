1c752a9548b922f3207fdb4991130acb190a81f0
Ivan Shcheklein <shcheklein@gmail.com>
2019-11-19 00:32:09 -0800

    tests: makedirs permissions test simplify with tmpdir fixture

--- tests/func/test_utils.py
+++ tests/func/test_utils.py
@@ -76,16 +76,15 @@ class TestUtils(TestDvc):
 
 
 @pytest.mark.skipif(os.name == "nt", reason="Not supported for Windows.")
-def test_makedirs_permissions():
+def test_makedirs_permissions(tmpdir):
     dir_mode = 0o755
+    os.chdir(str(tmpdir))
     intermediate_dir = "тестовая-директория"
     test_dir = os.path.join(intermediate_dir, "data")
 
     assert not os.path.exists(intermediate_dir)
 
-    try:
-        utils.makedirs(test_dir, mode=dir_mode)
-        assert stat.S_IMODE(os.stat(test_dir).st_mode) == dir_mode
-        assert stat.S_IMODE(os.stat(intermediate_dir).st_mode) == dir_mode
-    finally:
-        shutil.rmtree(intermediate_dir)
+    utils.makedirs(test_dir, mode=dir_mode)
+
+    assert stat.S_IMODE(os.stat(test_dir).st_mode) == dir_mode
+    assert stat.S_IMODE(os.stat(intermediate_dir).st_mode) == dir_mode
