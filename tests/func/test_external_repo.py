751bdb2197afac42f7d2af93c43593c51ae516b2
Saugat Pachhai <suagatchhetri@outlook.com>
2019-11-21 22:08:33 +0545

    dvc: refactor startswith usage with path_isin util

--- tests/func/test_external_repo.py
+++ tests/func/test_external_repo.py
@@ -4,6 +4,7 @@ from mock import patch
 
 from dvc.external_repo import external_repo
 from dvc.scm.git import Git
+from dvc.utils.fs import path_isin
 
 
 def test_external_repo(erepo):
@@ -22,8 +23,6 @@ def test_external_repo(erepo):
 
         # Check cache_dir is unset
         with external_repo(url) as repo:
-            assert repo.cache.local.cache_dir.startswith(
-                repo.root_dir + os.sep
-            )
+            assert path_isin(repo.cache.local.cache_dir, repo.root_dir)
 
         assert mock.call_count == 1
