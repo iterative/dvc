285620d96e16bb8be5a359383d630a4a770c3059
Mr. Outis <mroutis@protonmail.com>
2019-12-03 09:41:23 -0600

    tests: mock analytics.collect while testing daemon

--- tests/func/test_analytics.py
+++ tests/func/test_analytics.py
@@ -3,7 +3,9 @@ import mock
 from dvc.main import main
 
 
+@mock.patch("dvc.analytics.collect")
 @mock.patch("dvc.analytics.send")
-def test_daemon_analytics(mock_send, tmp_path):
+def test_daemon_analytics(mock_collect, mock_send, tmp_path):
     assert 0 == main(["daemon", "analytics", None, 0])
+    assert mock_collect.called
     assert mock_send.called
