f829338e1eecb94735bbb13f97661e07457428cb
Dani Hodovic <dani.hodovic@gmail.com>
2019-12-03 14:06:49 +0100

    if instead of try / catch

--- dvc/repo/get.py
+++ dvc/repo/get.py
@@ -21,11 +21,9 @@ logger = logging.getLogger(__name__)
 
 
 def copy_git_file(repo, src, dst):
-    try:
-        # Assert that the file does indeed exist in git before attempting to
-        # copy it.
-        repo.scm.repo.head.commit.tree[src]  # noqa
-    except KeyError:
+    # Assert that the file does indeed exist in git before attempting to
+    # copy it.
+    if src not in repo.scm.repo.head.commit.tree:
         raise FileOutsideRepoError(src)
 
     src_full_path = os.path.join(repo.root_dir, src)
