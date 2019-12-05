57b0596bfedd65d391e635fcdab861df266e9b00
Ruslan Kuprieiev <kupruser@gmail.com>
2019-12-05 15:07:59 +0200

    state: quote % in path
    Fixes #2885

--- dvc/state.py
+++ dvc/state.py
@@ -496,8 +496,14 @@ def _connect_sqlite(filename, options):
 
 
 def _build_sqlite_uri(filename, options):
+    # In the doc mentioned below we only need to replace ? -> %3f and
+    # # -> %23, but, if present, we also need to replace % -> %25 first
+    # (happens when we are on a weird FS that shows urlencoded filenames
+    # instead of proper ones) to not confuse sqlite.
+    uri_path = filename.replace("%", "%25")
+
     # Convert filename to uri according to https://www.sqlite.org/uri.html, 3.1
-    uri_path = filename.replace("?", "%3f").replace("#", "%23")
+    uri_path = uri_path.replace("?", "%3f").replace("#", "%23")
     if os.name == "nt":
         uri_path = uri_path.replace("\\", "/")
         uri_path = re.sub(r"^([a-z]:)", "/\\1", uri_path, flags=re.I)
