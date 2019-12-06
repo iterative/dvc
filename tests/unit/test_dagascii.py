6cfc831879d3fea5c4a64b483929281a8aced726
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2019-12-06 21:54:40 +0100

    Use mocker fixture in test_dagascii.py

--- tests/unit/test_dagascii.py
+++ tests/unit/test_dagascii.py
@@ -5,24 +5,24 @@ from dvc import dagascii
 from dvc.env import DVC_PAGER
 
 
-def test_less_pager_returned_when_less_found():
-    with mock.patch.object(os, "system") as m:
-        m.return_value = 0
-        pager = dagascii.find_pager()
+def test_less_pager_returned_when_less_found(mocker):
+    mocker.patch.object(os, "system", return_value=0)
+
+    pager = dagascii.find_pager()
 
     assert pager.cmd == dagascii.DEFAULT_PAGER_FORMATTED
 
 
-def test_plainpager_returned_when_less_missing():
-    with mock.patch.object(os, "system") as m:
-        m.return_value = 1  # any non-zero value
-        pager = dagascii.find_pager()
+def test_plainpager_returned_when_less_missing(mocker):
+    mocker.patch.object(os, "system", return_value=1)
+
+    pager = dagascii.find_pager()
 
     assert pager.__name__ == "plainpager"
 
 
-def test_tempfilepager_returned_when_var_defined():
-    os.environ[DVC_PAGER] = dagascii.DEFAULT_PAGER
+def test_tempfilepager_returned_when_var_defined(mocker):
+    mocker.patch.dict(os.environ, {"DVC_PAGER": dagascii.DEFAULT_PAGER})
     pager = dagascii.find_pager()
 
     assert pager.cmd == dagascii.DEFAULT_PAGER
