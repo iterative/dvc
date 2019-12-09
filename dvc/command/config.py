3610961144c4f258e0340bda847e3abc0db9ffd4
Alexander Schepanovski <suor.web@gmail.com>
2019-12-09 13:46:10 +0300

    dvc: warn to run `dvc checkout --relink` in cache.type change

--- dvc/command/config.py
+++ dvc/command/config.py
@@ -29,6 +29,15 @@ class CmdConfig(CmdBaseNoRepo):
                 section, opt, self.args.value, level=self.args.level
             )
 
+        is_write = self.args.unset or self.args.value is not None
+        if is_write and self.args.name == "cache.type":
+            logger.warning(
+                "You have changed cache.type, this doesn't update link types "
+                "of any previously checked out files automatically. "
+                "That can be done with:\n"
+                "             dvc checkout --relink"
+            )
+
         return 0
 
 
