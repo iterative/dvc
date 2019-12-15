4d4a3fc716436f10360908e516c609f948a7c05c
Max Risuhin <risuhin.max@gmail.com>
2019-12-15 05:31:52 -0800

    Standardize gdrive tests with others remotes

--- tests/unit/remote/gdrive/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from dvc.remote.gdrive import RemoteGDrive
-
-
-@pytest.fixture
-def gdrive(repo):
-    ret = RemoteGDrive(None, {"url": "gdrive://root/data"})
-    return ret
--- tests/unit/remote/test_gdrive.py
+++ tests/unit/remote/test_gdrive.py
@@ -3,7 +3,7 @@ from dvc.remote.gdrive import RemoteGDrive
 
 
 @mock.patch("dvc.remote.gdrive.RemoteGDrive.init_drive")
-def test_init_drive(repo):
+def test_init(repo):
     url = "gdrive://root/data"
     gdrive = RemoteGDrive(repo, {"url": url})
     assert str(gdrive.path_info) == url
