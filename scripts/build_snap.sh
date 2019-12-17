d020c3d8b025204bf9b81a82331df31de00f1c2d
Casper da Costa-Luis <casper.dcl@physics.org>
2019-12-17 19:10:26 +0000

    test snaps even where tag is undefined

--- scripts/build_snap.sh
+++ scripts/build_snap.sh
@@ -9,19 +9,20 @@ fi
 
 sudo snapcraft --use-lxd
 
-# Make sure we have a correct version
+pip uninstall -y dvc
+if which dvc; then
+  echo "ERROR: dvc command still exists! Unable to verify dvc version." >&2
+  exit 1
+fi
+sudo snap install --dangerous --classic dvc_*.snap
 if [[ -n "$TRAVIS_TAG" ]]; then
-  pip uninstall -y dvc
-  if which dvc; then
-    echo "ERROR: dvc command still exists! Unable to verify dvc version." >&2
-    exit 1
-  fi
-  sudo snap install --dangerous --classic dvc_*.snap
+  # Make sure we have a correct version
   if [[ "$(dvc --version)" != "$TRAVIS_TAG" ]]; then
       echo "ERROR: 'dvc --version'$(dvc -V) doesn't match '$TRAVIS_TAG'" >&2
       exit 1
   fi
-  dvc version
-  dvc get https://github.com/iterative/dvc scripts/innosetup/dvc.ico
-  sudo snap remove dvc
 fi
+# ensure basic commands can run
+dvc version
+dvc get https://github.com/iterative/dvc scripts/innosetup/dvc.ico
+sudo snap remove dvc
