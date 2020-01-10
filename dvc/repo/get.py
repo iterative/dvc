8db43b96da9e7cc4ea30ddb1be1ad22639ec2cff
Fábio Santos <fabiosantosart@gmail.com>
2020-01-10 22:22:28 +0000

    get: leverage external_repo() context manager for DVC repositories

--- dvc/repo/get.py
+++ dvc/repo/get.py
@@ -9,7 +9,7 @@ from dvc.exceptions import (
     OutputNotFoundError,
     PathMissingError,
 )
-from dvc.external_repo import cached_clone
+from dvc.external_repo import external_repo, cached_clone
 from dvc.path_info import PathInfo
 from dvc.stage import Stage
 from dvc.utils import resolve_output
@@ -29,8 +29,6 @@ class GetDVCFileError(DvcException):
 
 @staticmethod
 def get(url, path, out=None, rev=None):
-    from dvc.repo import Repo
-
     out = resolve_output(path, out)
 
     if Stage.is_valid_filename(out):
@@ -43,41 +41,50 @@ def get(url, path, out=None, rev=None):
     # and won't work with reflink/hardlink.
     dpath = os.path.dirname(os.path.abspath(out))
     tmp_dir = os.path.join(dpath, "." + str(shortuuid.uuid()))
+    raw_git_dir = None
     try:
-        cached_clone(url, rev=rev, clone_path=tmp_dir)
         try:
-            repo = Repo(tmp_dir)
+            with external_repo(cache_dir=tmp_dir, url=url, rev=rev) as repo:
+                # Try any links possible to avoid data duplication.
+                #
+                # Not using symlink, because we need to remove cache after we
+                # are done, and to make that work we would have to copy data
+                # over anyway before removing the cache, so we might just copy
+                # it right away.
+                #
+                # Also, we can't use theoretical "move" link type here, because
+                # the same cache file might be used a few times in a directory.
+                repo.cache.local.cache_types = ["reflink", "hardlink", "copy"]
 
-            # Try any links possible to avoid data duplication.
-            #
-            # Not using symlink, because we need to remove cache after we are
-            # done, and to make that work we would have to copy data over
-            # anyway before removing the cache, so we might just copy it
-            # right away.
-            #
-            # Also, we can't use theoretical "move" link type here, because
-            # the same cache file might be used a few times in a directory.
-            repo.cache.local.cache_types = ["reflink", "hardlink", "copy"]
+                try:
+                    output = repo.find_out_by_relpath(path)
+                except OutputNotFoundError:
+                    output = None
 
-            output = repo.find_out_by_relpath(path)
-            if output.use_cache:
-                _get_cached(repo, output, out)
+                if output and output.use_cache:
+                    _get_cached(repo, output, out)
+                    return
+
+                # Either an uncached out with absolute path or a user error
+
+                if os.path.isabs(path):
+                    raise FileNotFoundError
+
+                fs_copy(os.path.join(repo.root_dir, path), out)
                 return
 
-        except (NotDvcRepoError, OutputNotFoundError):
+        except NotDvcRepoError:
+            # Not a DVC repository, continue below and copy from git
             pass
 
-        # It's an uncached out with absolute path, a non-DVC repo, or a
-        # user error
-        if os.path.isabs(path):
-            raise FileNotFoundError
-
-        fs_copy(os.path.join(tmp_dir, path), out)
-
+        raw_git_dir = cached_clone(url, rev=rev)
+        fs_copy(os.path.join(raw_git_dir, path), out)
     except (OutputNotFoundError, FileNotFoundError):
         raise PathMissingError(path, url)
     finally:
         remove(tmp_dir)
+        if raw_git_dir:
+            remove(raw_git_dir)
 
 
 def _get_cached(repo, output, out):
