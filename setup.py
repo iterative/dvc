8fca139246cc15e7ca0ce772d9e76ca8862b8e56
rxxg <robbiexgibson@yahoo.com>
2020-01-15 12:50:43 +0100

    Use released version of speedcopy library

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ install_requires = [
     "win-unicode-console>=0.5; sys_platform == 'win32'",
     "pywin32>=225; sys_platform == 'win32'",
     "networkx>=2.1,<2.4",
-    "speedcopy @ git+https://github.com/rxxg/speedcopy@5",
+    "speedcopy>=2.0.1",
     "pyfastcopy>=1.0.3",
 ]
 
