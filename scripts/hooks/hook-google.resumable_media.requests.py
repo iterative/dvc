4a619e3db854bdba76b43ac8415f02b85d4ec18d
Ruslan Kuprieiev <kupruser@gmail.com>
2020-01-17 06:17:19 +0200

    build: update pyinstaller version
    https://github.com/iterative/dvc/network/alert/scripts/build-requirements.txt/PyInstaller/open

--- scripts/hooks/hook-google.resumable_media.requests.py
+++ scripts/hooks/hook-google.resumable_media.requests.py
@@ -1,9 +1,7 @@
 from PyInstaller.utils.hooks import copy_metadata
 
-datas = copy_metadata("google-cloud-storage")
-
 # NOTE: including "requests" metadata because of google-resumable-media
 # bug. See [1] for more info.
 #
 # [1] https://github.com/googleapis/google-resumable-media-python/issues/59
-datas += copy_metadata("requests")
+datas = copy_metadata("requests")
