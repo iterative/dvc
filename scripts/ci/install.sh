8d8a1c0f0cefe2846fb6c982803292e0951bc1dc
Ruslan Kuprieiev <kupruser@gmail.com>
2020-01-21 14:14:54 +0200

    travis: don't upgrade pip

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -8,7 +8,6 @@ scriptdir="$(dirname $0)"
 # NOTE: it is not uncommon for pip to hang on travis for what seems to be
 # networking issues. Thus, let's retry a few times to see if it will eventually
 # work or not.
-$scriptdir/retry.sh pip install --upgrade pip setuptools wheel
 $scriptdir/retry.sh pip install .[all,tests]
 
 git config --global user.email "dvctester@example.com"
