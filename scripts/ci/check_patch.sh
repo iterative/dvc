b0b69ce81972bbc23d1081289e4e409bc13bdd9a
Casper da Costa-Luis <casper.dcl@physics.org>
2020-01-31 19:14:51 +0000

    lint/pre-commit/ci: replace shfmt => beautysh

--- scripts/ci/check_patch.sh
+++ scripts/ci/check_patch.sh
@@ -9,7 +9,4 @@ pip install Pygments collective.checkdocs pre-commit
 python setup.py checkdocs
 
 # stop the build if there are any formatting errors
-err=0
-pre-commit run --all-files black || err=1
-pre-commit run --all-files flake8 || err=1
-exit $err
+pre-commit run --all-files
--- scripts/ci/check_scripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -x
-set -e
-
-GO111MODULE=on go get mvdan.cc/sh/v3/cmd/shfmt
-
-# stop the build if there are any formatting errors
-shfmt -l -d -i 2 -ci -w .
