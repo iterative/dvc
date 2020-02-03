294633aa226619d1e01ab0c91383a506eaabbdab
Jorge Orpinel <jorge@orpinel.com>
2020-02-03 13:10:36 -0600

    get/import: update path arg desc as well (See previous commit)

--- dvc/command/imp.py
+++ dvc/command/imp.py
@@ -45,8 +45,7 @@ def add_parser(subparsers, parent_parser):
         "url", help="Location of DVC or Git repository to download from"
     )
     import_parser.add_argument(
-        "path",
-        help="Path to a file or directory within the project or repository",
+        "path", help="Path to a file or directory within the repository"
     )
     import_parser.add_argument(
         "-o", "--out", nargs="?", help="Destination path to download files to"
