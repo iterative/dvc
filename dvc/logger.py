9b105f066038c0e9b50e37b917e344aa2c88bc47
Restyled.io <commits@restyled.io>
2020-02-17 12:33:53 +0000

    Restyled by black

--- dvc/logger.py
+++ dvc/logger.py
@@ -200,6 +200,6 @@ def setup(level=logging.INFO):
                     ],
                 },
             },
-            "disable_existing_loggers": False
+            "disable_existing_loggers": False,
         }
     )
