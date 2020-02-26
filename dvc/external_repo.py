b136eb5151fa032fb0f2ca137947a76b6c438ba7
Bruno Deferrari <utizoc@gmail.com>
2020-02-26 17:37:29 -0300

    erepo: fix relative url on default remote (#2756) (#3378)
    * erepo: fix relative urls for remotes (#2756)
    
    * erepo: add test case for relative path remotes (#2756)
    
    * Remove unused argument
    
    * erepo: move handling of relative remotes to a separate method (#2756)
    
    * erepo: don't manually build paths on test (#2756)
    
    * erepo: refactor handling of missing or relative upstream (#2756)
    
    * erepo: move handling of source repo resource to `_fix_upstream` (#2756)
    
    * erepo: improve relative remote tests (#2756)
    
    - use `tmp_dir` fixture
    - commit config file changes
    
    * erepo: Remove comment (#2756)
    
    * erepo: use scm_add to commit too, no need for separate scm.commit call (#2756)
    
    * erepo: minor refactoring in `_fix_upstream` (#2756)
    
    * erepo: add comment explaining that old relative paths are restored (#2756)

--- dvc/external_repo.py
+++ dvc/external_repo.py
@@ -66,7 +66,7 @@ class ExternalRepo(Repo):
         super().__init__(root_dir)
         self.url = url
         self._set_cache_dir()
-        self._set_upstream()
+        self._fix_upstream()
 
     def pull_to(self, path, to_info):
         """
@@ -123,21 +123,35 @@ class ExternalRepo(Repo):
 
         self.cache.local.cache_dir = cache_dir
 
-    def _set_upstream(self):
-        # check if the URL is local and no default remote is present
-        # add default remote pointing to the original repo's cache location
-        if os.path.isdir(self.url):
-            if not self.config["core"].get("remote"):
-                src_repo = Repo(self.url)
-                try:
-                    cache_dir = src_repo.cache.local.cache_dir
-                finally:
-                    src_repo.close()
+    def _fix_upstream(self):
+        if not os.path.isdir(self.url):
+            return
 
-                self.config["remote"]["auto-generated-upstream"] = {
-                    "url": cache_dir
-                }
-                self.config["core"]["remote"] = "auto-generated-upstream"
+        remote_name = self.config["core"].get("remote")
+        src_repo = Repo(self.url)
+        try:
+            if remote_name:
+                self._fix_local_remote(src_repo, remote_name)
+            else:
+                self._add_upstream(src_repo)
+        finally:
+            src_repo.close()
+
+    def _fix_local_remote(self, src_repo, remote_name):
+        # If a remote URL is relative to the source repo,
+        # it will have changed upon config load and made
+        # relative to this new repo. Restore the old one here.
+        new_remote = self.config["remote"][remote_name]
+        old_remote = src_repo.config["remote"][remote_name]
+        if new_remote["url"] != old_remote["url"]:
+            new_remote["url"] = old_remote["url"]
+
+    def _add_upstream(self, src_repo):
+        # Fill the empty upstream entry with a new remote pointing to the
+        # original repo's cache location.
+        cache_dir = src_repo.cache.local.cache_dir
+        self.config["remote"]["auto-generated-upstream"] = {"url": cache_dir}
+        self.config["core"]["remote"] = "auto-generated-upstream"
 
 
 class ExternalGitRepo:
