84e41d51bf73809574957b923ac43f7186dc8ff7
Casper da Costa-Luis <casper.dcl@physics.org>
2020-03-11 16:08:16 +0000

    Revert "import-url: allow queries in URL"
    This reverts commit 48495d027963073fe260f596d85c3c179fee037f.

--- dvc/path_info.py
+++ dvc/path_info.py
@@ -112,7 +112,7 @@ class URLInfo(_BasePath):
 
     def __init__(self, url):
         p = urlparse(url)
-        assert not p.params and not p.fragment
+        assert not p.query and not p.params and not p.fragment
         assert p.password is None
 
         self.fill_parts(p.scheme, p.hostname, p.username, p.port, p.path)
