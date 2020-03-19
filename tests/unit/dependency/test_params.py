3f9db9c33df092b4f79ce9ea9dec71e420cd6cab
Ruslan Kuprieiev <ruslan@iterative.ai>
2020-03-20 00:56:31 +0200

    fix formatting

--- tests/unit/dependency/test_params.py
+++ tests/unit/dependency/test_params.py
@@ -1,5 +1,3 @@
-import mock
-
 from dvc.dependency import DependencyPARAMS
 from dvc.stage import Stage
 from tests.basic_env import TestDvc
@@ -8,8 +6,9 @@ from tests.basic_env import TestDvc
 class TestDependencyPARAM(TestDvc):
     def test_from_list(self):
         stage = Stage(self.dvc)
-        deps = DependencyPARAMS.from_list(stage, ['foo', 'bar,baz',
-                                                  'a_file:qux'])
+        deps = DependencyPARAMS.from_list(
+            stage, ["foo", "bar,baz", "a_file:qux"]
+        )
         assert len(deps) == 2
         assert deps[0].def_path == "a_file"
         assert deps[0].param_names == ["qux"]
