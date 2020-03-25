d4750cbd2fdb48e9173bf3c2be26926e64d402a0
Saugat Pachhai <suagatchhetri@outlook.com>
2020-03-25 15:55:49 +0545

    tests: unittest makedirs for s3 and gs

--- tests/unit/remote/test_remote.py
+++ tests/unit/remote/test_remote.py
@@ -1,4 +1,6 @@
-from dvc.remote import Remote
+import pytest
+
+from dvc.remote import Remote, RemoteS3, RemoteGS
 
 
 def test_remote_with_checksum_jobs(dvc):
@@ -25,3 +27,14 @@ def test_remote_without_checksum_jobs_default(dvc):
 
     remote = Remote(dvc, name="without_checksum_jobs")
     assert remote.checksum_jobs == remote.CHECKSUM_JOBS
+
+
+@pytest.mark.parametrize("remote_cls", [RemoteGS, RemoteS3])
+def test_makedirs_not_create_for_top_level_path(remote_cls, mocker):
+    url = "{.scheme}://bucket/".format(remote_cls)
+    remote = remote_cls(None, {"url": url})
+    exc = Exception("should not try to create dir on top-level path")
+    client = mocker.PropertyMock(side_effect=exc)
+    # we use remote clients with same name as scheme to interact with remote
+    setattr(remote_cls, remote_cls.scheme, client)
+    remote.makedirs(remote.path_info)
