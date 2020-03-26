974f81791f714c31658688354c9878ce2f508b06
Ruslan Kuprieiev <kupruser@gmail.com>
2020-03-26 03:33:48 +0200

    protect: ignore EROFS (#3527)
    * protect: fix bug in mode verification
    
    * protect: ignore EROFS
    
    Fixes #3510

--- dvc/remote/local.py
+++ dvc/remote/local.py
@@ -448,13 +448,17 @@ class RemoteLOCAL(RemoteBASE):
         try:
             os.chmod(path, mode)
         except OSError as exc:
-            # In share cache scenario, we might not own the cache file, so we
+            # There is nothing we need to do in case of a read-only file system
+            if exc.errno == errno.EROFS:
+                return
+
+            # In shared cache scenario, we might not own the cache file, so we
             # need to check if cache file is already protected.
             if exc.errno not in [errno.EPERM, errno.EACCES]:
                 raise
 
-            actual = os.stat(path).st_mode
-            if actual & mode != mode:
+            actual = stat.S_IMODE(os.stat(path).st_mode)
+            if actual != mode:
                 raise
 
     def _get_unpacked_dir_path_info(self, checksum):
--- tests/unit/remote/test_local.py
+++ tests/unit/remote/test_local.py
@@ -1,4 +1,5 @@
 import os
+import errno
 
 import pytest
 
@@ -58,3 +59,30 @@ def test_is_protected(tmp_dir, link_name):
         assert remote.is_protected(foo)
     else:
         assert not remote.is_protected(foo)
+
+
+@pytest.mark.parametrize("err", [errno.EPERM, errno.EACCES])
+def test_protect_ignore_errors(tmp_dir, mocker, err):
+    tmp_dir.gen("foo", "foo")
+    foo = PathInfo("foo")
+    remote = RemoteLOCAL(None, {})
+
+    remote.protect(foo)
+
+    mock_chmod = mocker.patch(
+        "os.chmod", side_effect=OSError(err, "something")
+    )
+    remote.protect(foo)
+    assert mock_chmod.called
+
+
+def test_protect_ignore_erofs(tmp_dir, mocker):
+    tmp_dir.gen("foo", "foo")
+    foo = PathInfo("foo")
+    remote = RemoteLOCAL(None, {})
+
+    mock_chmod = mocker.patch(
+        "os.chmod", side_effect=OSError(errno.EROFS, "read-only fs")
+    )
+    remote.protect(foo)
+    assert mock_chmod.called
