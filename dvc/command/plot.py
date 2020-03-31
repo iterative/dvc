7a65199a4c5a86035a912d0e41ea1f905cf9c184
pawel <pawelredzynski@gmail.com>
2020-03-31 19:29:38 +0200

    revision support

--- dvc/command/plot.py
+++ dvc/command/plot.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 class CmdPlot(CmdBase):
     def run(self):
-        self.repo.plot(self.args.targets)
+        self.repo.plot(self.args.targets, self.args.a_rev, self.args.b_rev)
 
 
 def add_parser(subparsers, parent_parser):
@@ -20,6 +20,16 @@ def add_parser(subparsers, parent_parser):
         description=append_doc_link(PLOT_HELP, "plot"),
         help=PLOT_HELP,
         formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    plot_parser.add_argument(
+        "--a_rev",
+        help="Old Git commit to compare (defaults to HEAD)",
+        nargs="?",
+    )
+    plot_parser.add_argument(
+        "--b_rev",
+        help=("New Git commit to compare (defaults to the current workspace)"),
+        nargs="?",
     )
     plot_parser.add_argument(
         "targets", nargs="+", help="Metrics file to visualize."
--- dvc/repo/plot.py
+++ dvc/repo/plot.py
@@ -1,20 +1,75 @@
+import json
 import logging
-import os
 
 from dvc.plot import DefaultTemplate
 
 logger = logging.getLogger(__name__)
 
 
-def create_data_dict(target, typ):
-    result = {}
-    if typ == "json":
-        import json
+class PageTemplate:
+    HTML = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Embedding Vega-Lite</title>
+        <script src="https://cdn.jsdelivr.net/npm/vega@5.10.0"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vega-lite@4.8.1"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vega-embed@6.5.1"></script>
+    </head>
+    <body>
+        {divs}
+    </body>
+    </html>"""
 
-        with open(target, "r+") as fd:
-            data = json.load(fd)
-            for d in data:
-                d["revision"] = "HEAD"
+    @staticmethod
+    def save(divs, path):
+        page = PageTemplate.HTML.format(divs="\n".join(divs))
+        with open(path, "w") as fd:
+            fd.write(page)
+
+
+class DivTemplate:
+    HTML = """
+        <div id = "{id}"></div>
+        <script type = "text/javascript">
+          var spec = {vega_dict};
+          vegaEmbed('#{id}', spec);
+        </script>"""
+
+    @staticmethod
+    def prepare_div(vega_dict):
+        from shortuuid import uuid
+
+        id = uuid()
+        return DivTemplate.HTML.format(
+            id=str(id),
+            vega_dict=json.dumps(vega_dict, indent=4, separators=(",", ": ")),
+        )
+
+
+def _load(tree, target, revision="curren workspace"):
+    with tree.open(target, "r") as fd:
+        data = json.load(fd)
+        for d in data:
+            d["revision"] = revision
+    return data
+
+
+def create_data_dict(repo, target, typ, a_rev=None, b_rev=None):
+    result = {}
+    data = []
+    if typ == "json":
+
+        if a_rev and b_rev:
+            logger.error("diff")
+            a_tree = repo.scm.get_tree(a_rev)
+            b_tree = repo.scm.get_tree(b_rev)
+            logger.error((a_tree, b_tree))
+            data.extend(_load(a_tree, target, a_rev))
+            data.extend(_load(b_tree, target, b_rev))
+        else:
+            logger.error(str(repo.tree.tree))
+            data.extend(_load(repo.tree, target))
 
     result["data"] = {}
     result["data"]["values"] = data
@@ -22,14 +77,15 @@ def create_data_dict(target, typ):
     return result
 
 
-def plot(repo, targets, typ="json"):
+def plot(repo, targets, a_rev=None, b_rev=None, typ="json"):
     # TODO how to handle multiple targets
-    target = targets[0]
-    vega_data_dict = create_data_dict(target, typ)
+    logger.error((a_rev, b_rev))
+    divs = []
+    for target in targets:
+        vega_data_dict = create_data_dict(repo, target, typ, a_rev, b_rev)
 
-    # TODO need to pass title, probably need a way to pass additional config
-    # from json file
+        # TODO need to pass title, probably need a way to pass additional conf
 
-    DefaultTemplate(repo.dvc_dir).save(
-        vega_data_dict, os.path.join(repo.root_dir, "viz.html")
-    )
+        vega_dict = DefaultTemplate(repo.dvc_dir).fill(vega_data_dict)
+        divs.append(DivTemplate.prepare_div(vega_dict))
+    PageTemplate.save(divs, "viz.html")
