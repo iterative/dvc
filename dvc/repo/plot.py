b51c9a1f2ba92d4e97d3ae1046c919d7e55d8ac6
pawel <pawelredzynski@gmail.com>
2020-04-01 15:51:59 +0200

    efiop review

--- dvc/repo/plot.py
+++ dvc/repo/plot.py
@@ -3,13 +3,12 @@ import logging
 import os
 
 from dvc.plot import DefaultTemplate
+from dvc.repo import locked
 from dvc.utils import format_link
 
 logger = logging.getLogger(__name__)
 
-
-class PageTemplate:
-    HTML = """<html>
+PAGE_HTML = """<html>
 <head>
     <title>dvc plot</title>
     <script src="https://cdn.jsdelivr.net/npm/vega@5.10.0"></script>
@@ -21,47 +20,51 @@ class PageTemplate:
 </body>
 </html>"""
 
-    @staticmethod
-    def save(divs, path):
-        page = PageTemplate.HTML.format(divs="\n".join(divs))
-        with open(path, "w") as fd:
-            fd.write(page)
+DIV_HTML = """<div id = "{id}"></div>
+<script type = "text/javascript">
+    var spec = {vega_json};
+    vegaEmbed('#{id}', spec);
+</script>"""
 
 
-class DivTemplate:
-    HTML = """
-        <div id = "{id}"></div>
-        <script type = "text/javascript">
-          var spec = {vega_json};
-          vegaEmbed('#{id}', spec);
-        </script>"""
+def _save_plot_html(divs, path):
+    page = PAGE_HTML.format(divs="\n".join(divs))
+    with open(path, "w") as fobj:
+        fobj.write(page)
 
-    @staticmethod
-    def prepare_div(vega_dict):
-        from shortuuid import uuid
 
-        id = uuid()
-        return DivTemplate.HTML.format(
-            id=str(id),
-            vega_json=json.dumps(vega_dict, indent=4, separators=(",", ": ")),
-        )
+def _prepare_div(vega_dict):
+    from shortuuid import uuid
+
+    id = uuid()
+    return DIV_HTML.format(
+        id=str(id),
+        vega_json=json.dumps(vega_dict, indent=4, separators=(",", ": ")),
+    )
 
 
 def _load(tree, target, revision="current workspace"):
-    with tree.open(target, "r") as fd:
-        data = json.load(fd)
+    with tree.open(target, "r") as fobj:
+        data = json.load(fobj)
         for d in data:
             d["revision"] = revision
     return data
 
 
-def plot(repo, targets, plot_path="plot.html", typ="json"):
+@locked
+def plot(repo, targets, plot_path=None, typ="json"):
+
+    if not plot_path:
+        plot_path = "plot.html"
+
     divs = []
     for target in targets:
         data = _load(repo.tree, target)
         vega_plot_json = DefaultTemplate(repo.dvc_dir).fill(data, target)
-        divs.append(DivTemplate.prepare_div(vega_plot_json))
-    PageTemplate.save(divs, plot_path)
+        divs.append(_prepare_div(vega_plot_json))
+
+    _save_plot_html(divs, plot_path)
+
     logger.info(
         "Your can see your plot by opening {} in your "
         "browser!".format(
