224f3399f2fb238cba024d1012550ef18fe4a1c1
pawel <pawelredzynski@gmail.com>
2020-04-02 17:12:57 +0200

    refactor tests

--- tests/func/test_plot.py
+++ tests/func/test_plot.py
@@ -1,74 +1,98 @@
 import json
 import os
-from copy import copy
 
 from bs4 import BeautifulSoup
 from funcy import first
 
-from dvc.plot import Template
+
+def _run_with_metric(tmp_dir, dvc, metric, metric_filename, commit=None):
+    tmp_dir.gen({metric_filename: json.dumps(metric)})
+    dvc.run(metrics_no_cache=[metric_filename])
+    if hasattr(dvc, "scm"):
+        dvc.scm.add([metric_filename, metric_filename + ".dvc"])
+        if commit:
+            dvc.scm.commit(commit)
 
 
-def _add_revision(data, rev="current workspace"):
-    new_data = copy(data)
-    for e in new_data:
-        e["revision"] = rev
-
-    return new_data
-
-
-def test_plot_linear(tmp_dir, dvc):
+def test_plot_linear(tmp_dir, scm, dvc):
     metric = [{"x": 1, "y": 2}, {"x": 2, "y": 3}]
-    tmp_dir.dvc_gen({"metric.json": json.dumps(metric)})
-    dvc.metrics.add("metric.json")
+    _run_with_metric(tmp_dir, dvc, metric, "metric.json", "first run")
 
     dvc.plot(["metric.json"], "result.html")
 
     page = tmp_dir / "result.html"
-
     assert page.exists()
     page_content = BeautifulSoup(page.read_text())
 
-    with_revision = _add_revision(metric)
-    expected_script_content = json.dumps(
-        Template.fill(
-            os.path.join(dvc.plot_templates.templates_dir, "default.json"),
-            with_revision,
-            "metric.json",
-        ),
+    expected_vega_json = json.dumps(
+        {
+            "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+            "data": {
+                "values": [
+                    {"x": 1, "y": 2, "revision": "current workspace"},
+                    {"x": 2, "y": 3, "revision": "current workspace"},
+                ]
+            },
+            "mark": {"type": "line"},
+            "encoding": {
+                "x": {"field": "x", "type": "quantitative"},
+                "y": {"field": "y", "type": "quantitative"},
+                "color": {"field": "revision", "type": "nominal"},
+            },
+            "title": "metric.json",
+        },
         indent=4,
         separators=(",", ": "),
     )
 
-    assert expected_script_content in first(page_content.body.script.contents)
+    assert expected_vega_json in first(page_content.body.script.contents)
 
 
 def test_plot_confusion(tmp_dir, dvc):
     confusion_matrix = [{"x": "B", "y": "A"}, {"x": "A", "y": "A"}]
-    tmp_dir.dvc_gen({"metric.json": json.dumps(confusion_matrix)})
-    dvc.metrics.add("metric.json")
+    _run_with_metric(
+        tmp_dir, dvc, confusion_matrix, "metric.json", "first run"
+    )
 
     dvc.plot(
         ["metric.json"],
         "result.html",
-        template=os.path.join(
-            dvc.plot_templates.templates_dir, "default_confusion.json"
-        ),
+        os.path.join(".dvc", "plot", "default_confusion.json"),
     )
 
     page = tmp_dir / "result.html"
-
     assert page.exists()
     page_content = BeautifulSoup(page.read_text())
 
-    with_revision = _add_revision(confusion_matrix)
-    expected_script_content = json.dumps(
-        Template.fill(
-            os.path.join(".dvc", "plot", "default_confusion.json"),
-            with_revision,
-            "metric.json",
-        ),
+    expected_vega_json = json.dumps(
+        {
+            "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+            "data": {
+                "values": [
+                    {"x": "B", "y": "A", "revision": "current workspace"},
+                    {"x": "A", "y": "A", "revision": "current workspace"},
+                ]
+            },
+            "mark": "rect",
+            "encoding": {
+                "x": {
+                    "field": "x",
+                    "type": "nominal",
+                    "sort": "ascending",
+                    "title": "Predicted value",
+                },
+                "y": {
+                    "field": "y",
+                    "type": "nominal",
+                    "sort": "ascending",
+                    "title": "Actual value",
+                },
+                "color": {"aggregate": "count", "type": "quantitative"},
+            },
+            "title": "metric.json",
+        },
         indent=4,
         separators=(",", ": "),
     )
 
-    assert expected_script_content in first(page_content.body.script.contents)
+    assert expected_vega_json in first(page_content.body.script.contents)
