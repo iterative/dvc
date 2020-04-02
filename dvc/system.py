f4bec650eddc8874b3f7ab2f8b34bc5dfe60fd49
charlesbaynham <bayners123@gmail.com>
2020-04-02 15:13:46 +0100

    Only import speedcopy on Win > 7 (#3581)
    * system: Only import speedcopy for Windows > 7
    
    Fixes #3259
    
    * Allow for Win 8
    
    Co-authored-by: Charles Baynham <charles.baynham@npl.co.uk>

--- dvc/system.py
+++ dvc/system.py
@@ -10,7 +10,11 @@ from dvc.exceptions import DvcException
 
 logger = logging.getLogger(__name__)
 
-if platform.system() == "Windows" and sys.version_info < (3, 8):
+if (
+    platform.system() == "Windows"
+    and sys.version_info < (3, 8)
+    and sys.getwindowsversion() >= (6, 2)
+):
     try:
         import speedcopy
 
