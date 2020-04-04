24061c1a5313e96dda5694635197d9c883e699b2
Ruslan Kuprieiev <kupruser@gmail.com>
2020-04-04 21:51:51 +0300

    dvc: don't ignore warnings manually
    We use logger.captureWarnings() instead for CLI and for API this is harmful.

--- dvc/__init__.py
+++ dvc/__init__.py
@@ -3,17 +3,8 @@ DVC
 ----
 Make your data science projects reproducible and shareable.
 """
-import warnings
-
 import dvc.logger
 from dvc.version import __version__  # noqa: F401
 
 
 dvc.logger.setup()
-
-# Ignore numpy's runtime warnings: https://github.com/numpy/numpy/pull/432.
-# We don't directly import numpy, but our dependency networkx does, causing
-# these warnings in some environments. Luckily these warnings are benign and
-# we can simply ignore them so that they don't show up when you are using dvc.
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
