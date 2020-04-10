02aee4ec2f41e995137a371ea6dd61201ceb0657
pawel <pawelredzynski@gmail.com>
2020-04-10 16:42:57 +0200

    fixup

--- dvc/repo/plot.py
+++ dvc/repo/plot.py
@@ -30,10 +30,6 @@ class TooManyDataSourcesError(DvcException):
         )
 
 
-def _all_dict_of_length_one(data):
-    return all([isinstance(e, dict) and len(e) == 1 for e in data])
-
-
 def _load_from_tree(tree, datafile, default_plot=False):
     if datafile.endswith(".json"):
         data = _parse_json(datafile, default_plot, tree)
