972b61f3a57f6878016045dfa8b8922e078287bc
pawel <pawelredzynski@gmail.com>
2020-04-13 14:24:21 +0200

    plot: improve handling non-existing files on revisions

--- tests/unit/repo/test_plot.py
+++ tests/unit/repo/test_plot.py
@@ -1,6 +1,3 @@
-import pytest
-
-from dvc.exceptions import DvcException
 from dvc.repo.plot import _load_from_revisions, WORKSPACE_REVISION_NAME
 
 
@@ -9,8 +6,7 @@ def test_load_no_revisions_clean(mocker):
     repo = mocker.MagicMock()
     repo.scm.is_dirty.return_value = False
 
-    with pytest.raises(DvcException):
-        _load_from_revisions(repo, "datafile", [], False)
+    _load_from_revisions(repo, "datafile", [], False)
 
     assert m.call_count == 1
     assert m.call_args_list[0] == mocker.call(
@@ -23,8 +19,7 @@ def test_load_no_revisions_dirty(mocker):
     repo = mocker.MagicMock()
     repo.scm.is_dirty.return_value = True
 
-    with pytest.raises(DvcException):
-        _load_from_revisions(repo, "datafile", [], False)
+    _load_from_revisions(repo, "datafile", [], False)
 
     assert m.call_count == 2
     assert m.call_args_list[0] == mocker.call(
@@ -40,8 +35,7 @@ def test_load_one(mocker):
     repo = mocker.MagicMock()
     repo.scm.is_dirty.return_value = True
 
-    with pytest.raises(DvcException):
-        _load_from_revisions(repo, "datafile", ["tag1"], False)
+    _load_from_revisions(repo, "datafile", ["tag1"], False)
 
     assert m.call_count == 2
     assert m.call_args_list[0] == mocker.call(
@@ -57,8 +51,7 @@ def test_load_more(mocker):
     repo = mocker.MagicMock()
     repo.scm.is_dirty.return_value = True
 
-    with pytest.raises(DvcException):
-        _load_from_revisions(repo, "datafile", ["tag1", "tag2"], False)
+    _load_from_revisions(repo, "datafile", ["tag1", "tag2"], False)
 
     assert m.call_count == 2
     assert m.call_args_list[0] == mocker.call(
