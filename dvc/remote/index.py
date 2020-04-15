cc6fe9f1008088a8446b09119530d81f488c471f
Peter Rowlands <peter@pmrowla.com>
2020-04-15 14:22:54 +0900

    remote: make index threadsafe

--- dvc/remote/index.py
+++ dvc/remote/index.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import pickle
+import threading
 
 logger = logging.getLogger(__name__)
 
@@ -25,7 +26,9 @@ class RemoteIndex(object):
             )
         else:
             self.path = None
+        self.lock = threading.Lock()
         self._checksums = set()
+        self.modified = False
         self.load()
 
     def __iter__(self):
@@ -38,47 +41,75 @@ class RemoteIndex(object):
     def load(self):
         """(Re)load this index from disk."""
         if self.path and os.path.isfile(self.path):
+            self.lock.acquire()
             try:
                 with open(self.path, "rb") as fobj:
                     self._checksums = pickle.load(fobj)
-            except PermissionError:
+                self.modified = False
+            except IOError as exc:
                 logger.error(
-                    "Insufficient permissions to read index file "
-                    "'{}'".format(self.path)
+                    "Failed to load remote index file '{}'. "
+                    "Remote will be re-indexed: '{}'".format(self.path, exc)
                 )
+            finally:
+                self.lock.release()
 
     def save(self):
         """Save this index to disk."""
-        if self.path:
+        if self.path and self.modified:
+            self.lock.acquire()
             try:
                 with open(self.path, "wb") as fobj:
                     pickle.dump(self._checksums, fobj)
-            except PermissionError:
+                self.modified = False
+            except IOError as exc:
                 logger.error(
-                    "Insufficient permissions to write index file "
-                    "'{}'".format(self.path)
+                    "Failed to save remote index file '{}': {}".format(
+                        self.path, exc
+                    )
                 )
+            finally:
+                self.lock.release()
 
     def invalidate(self):
         """Invalidate this index (to force re-indexing later)."""
+        self.lock.acquire()
         self._checksums.clear()
+        self.modified = True
         if self.path and os.path.isfile(self.path):
-            os.unlink(self.path)
+            try:
+                os.unlink(self.path)
+            except IOError as exc:
+                logger.error(
+                    "Failed to remove remote index file '{}': {}".format(
+                        self.path, exc
+                    )
+                )
+        self.lock.release()
 
     def remove(self, checksum):
         if checksum in self._checksums:
+            self.lock.acquire()
             self._checksums.remove(checksum)
+            self.modified = True
+            self.lock.release()
 
     def replace(self, checksums):
         """Replace the full contents of this index with ``checksums``.
 
         Changes to the index will not be written to disk.
         """
+        self.lock.acquire()
         self._checksums = set(checksums)
+        self.modified = True
+        self.lock.release()
 
     def update(self, *checksums):
         """Update this index, adding elements from ``checksums``.
 
         Changes to the index will not be written to disk.
         """
+        self.lock.acquire()
         self._checksums.update(*checksums)
+        self.modified = True
+        self.lock.release()
