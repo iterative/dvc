74e8f53f79dddb7db24448bfab889603e6409ae0
Alexey Matveev <>
2020-04-20 20:49:02 +0300

    remote: WebDav Fix deepseource error (webdavs)

--- dvc/remote/webdavs.py
+++ dvc/remote/webdavs.py
@@ -4,3 +4,12 @@ from dvc.scheme import Schemes
 
 class RemoteWEBDAVS(RemoteWEBDAV):
     scheme = Schemes.WEBDAVS
+
+    def gc(self):
+        raise NotImplementedError
+
+    def list_cache_paths(self, prefix=None, progress_callback=None):
+        raise NotImplementedError
+
+    def walk_files(self, path_info):
+        raise NotImplementedError
