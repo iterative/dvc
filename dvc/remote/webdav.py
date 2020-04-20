5dc4dd802cb60f123e46af976b7569bf2ee8183f
Alexey Matveev <>
2020-04-20 11:29:35 +0300

    remote: WebDav Fix: base to the original state. Add WebdavURLInfo. Creating intermediate collections.

--- dvc/remote/webdav.py
+++ dvc/remote/webdav.py
@@ -1,34 +1,49 @@
-from .http import RemoteHTTP
-from dvc.scheme import Schemes
-
+import copy
 import os.path
 
+from funcy import cached_property
+
+from .http import RemoteHTTP
+from dvc.scheme import Schemes
+from dvc.path_info import HTTPURLInfo
 from dvc.progress import Tqdm
 from dvc.exceptions import HTTPError
 
 
+class WebdavURLInfo(HTTPURLInfo):
+    def __init__(self, url):
+        super().__init__(url)
+
+    @cached_property
+    def url(self):
+        return "{}://{}{}{}{}{}".format(
+            self.scheme.replace("webdav", "http"),
+            self.netloc,
+            self._spath,
+            (";" + self.params) if self.params else "",
+            ("?" + self.query) if self.query else "",
+            ("#" + self.fragment) if self.fragment else "",
+        )
+
+    def get_collections(self) -> list:
+        def pcol(path):
+            return "{}://{}{}".format(
+                self.scheme.replace("webdav", "http"),
+                self.netloc,
+                path,
+            )
+        p = self.path.split("/")[1:-1]
+        if not p:
+            return []
+        r = []
+        for i in range(len(p)):
+            r.append(pcol("/{}/".format("/".join(p[:i + 1]))))
+        return r
+
+
 class RemoteWEBDAV(RemoteHTTP):
     scheme = Schemes.WEBDAV
-
-    def __init__(self, repo, config):
-        super().__init__(repo, config)
-
-        url = config.get("url")
-        if url:
-            self.path_info = self.path_cls(url)
-            self.path_info.scheme = self.path_info.scheme.replace(
-                "webdav", "http")
-            user = config.get("user", None)
-            if user:
-                self.path_info.user = user
-        else:
-            self.path_info = None
-
-        self.auth = config.get("auth", None)
-        self.custom_auth_header = config.get("custom_auth_header", None)
-        self.password = config.get("password", None)
-        self.ask_password = config.get("ask_password", False)
-        self.headers = {}
+    path_cls = WebdavURLInfo
 
     def _upload(self, from_file, to_info, name=None, no_progress_bar=False):
         def chunks():
@@ -49,6 +64,19 @@ class RemoteWEBDAV(RemoteHTTP):
                             break
                         yield chunk
 
+        self._create_collections(to_info)
         response = self._request("PUT", to_info.url, data=chunks())
         if response.status_code not in (200, 201):
             raise HTTPError(response.status_code, response.reason)
+
+    def _create_collections(self, to_info):
+        url_cols = to_info.get_collections()
+        i = len(url_cols)
+        while i > 0:
+            if bool(self._request("HEAD", url_cols[i - 1])):
+                break
+            i -= 1
+        for i in range(i, len(url_cols)):
+            response = self._request("MKCOL", url_cols[i])
+            if response.status_code not in (200, 201):
+                raise HTTPError(response.status_code, response.reason)
