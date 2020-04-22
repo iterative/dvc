c66ade371310fb87ed40d16e10fb2e45709e804c
Charles Baynham <charles.baynham@npl.co.uk>
2020-04-22 13:39:31 +0100

    Exclude Windows test on Linux

--- tests/unit/command/test_cache.py
+++ tests/unit/command/test_cache.py
@@ -1,3 +1,5 @@
+import os
+import pytest
 from mock import MagicMock
 
 from dvc.config import Config
@@ -20,3 +22,17 @@ def test_path_replacement():
     assert rel_func("c:\\path\\to\\cache") == "c:/path/to/cache"
 
     assert rel_func("ssh://something") == "ssh://something"
+
+
+@pytest.mark.skipif(os.name != "nt", reason="Windows only")
+def test_path_replacement_windows():
+
+    config_filename = "./config/file"
+
+    # Extract the "rel()" function from Config._save_paths
+    Config._map_dirs = MagicMock()
+    Config._save_paths(None, config_filename)
+    _, rel_func = Config._map_dirs.call_args[0]
+
+    assert rel_func("..\\cache") == "../../cache"
+    assert rel_func("c:\\path\\to\\cache") == "c:/path/to/cache"
