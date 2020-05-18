238946a217fe9cb45697d6b967bf9ef4cbb80f29
Ruslan Kuprieiev <ruslan@iterative.ai>
2020-05-19 02:49:13 +0300

    stage: get rid of OutputParams

--- dvc/stage/params.py
+++ dvc/stage/params.py
@@ -1,6 +1,3 @@
-from enum import Enum
-
-
 class StageParams:
     PARAM_MD5 = "md5"
     PARAM_CMD = "cmd"
@@ -13,14 +10,3 @@ class StageParams:
     PARAM_PARAMS = "params"
     PARAM_METRICS = "metrics"
     PARAM_PLOTS = "plots"
-
-
-class OutputParams(Enum):
-    PERSIST = "outs_persist"
-    PERSIST_NO_CACHE = "outs_persist_no_cache"
-    METRICS_NO_CACHE = "metrics_no_cache"
-    METRICS = "metrics"
-    PLOTS_NO_CACHE = "plots_no_cache"
-    PLOTS = "plots"
-    NO_CACHE = "outs_no_cache"
-    OUTS = "outs"
