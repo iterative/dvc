fd333261d2e9fe1364cc278138c79890a1f5fe7a
Casper da Costa-Luis <casper.dcl@physics.org>
2020-05-31 00:14:57 +0100

    dependency: add gdrive

--- dvc/dependency/__init__.py
+++ dvc/dependency/__init__.py
@@ -3,6 +3,7 @@ from urllib.parse import urlparse
 
 import dvc.output as output
 from dvc.dependency.azure import AzureDependency
+from dvc.dependency.gdrive import GDriveDependency
 from dvc.dependency.gs import GSDependency
 from dvc.dependency.hdfs import HDFSDependency
 from dvc.dependency.http import HTTPDependency
@@ -19,6 +20,7 @@ from .repo import RepoDependency
 
 DEPS = [
     AzureDependency,
+    GDriveDependency,
     GSDependency,
     HDFSDependency,
     HTTPDependency,
@@ -33,6 +35,7 @@ DEP_MAP = {
     Schemes.SSH: SSHDependency,
     Schemes.S3: S3Dependency,
     Schemes.AZURE: AzureDependency,
+    Schemes.GDRIVE: GDriveDependency,
     Schemes.GS: GSDependency,
     Schemes.HDFS: HDFSDependency,
     Schemes.HTTP: HTTPDependency,
