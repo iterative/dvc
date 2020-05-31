e0ee3593f57d99108e9ce37784875ab940142eb0
Casper da Costa-Luis <casper.dcl@physics.org>
2020-05-31 23:59:52 +0100

    tests: bash completion

--- scripts/ci/check_patch.sh
+++ scripts/ci/check_patch.sh
@@ -10,3 +10,12 @@ python setup.py checkdocs
 
 # stop the build if there are any formatting errors
 pre-commit run --all-files
+
+# check bash completion
+python scripts/completion/bash.py
+completion_diff="$(git diff -U0 -- scripts/completion/dvc.bash)"
+if [[ -n "$completion_diff" ]]; then
+  echo "ERROR: bash completion changed:" >&2
+  echo "$completion_diff" >&2
+  exit 1
+fi
