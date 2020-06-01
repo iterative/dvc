6f728d3e6ba75c873da3cf6ff5fc7f73dbb974fa
Ruslan Kuprieiev <ruslan@iterative.ai>
2020-06-01 22:31:07 +0300

    dvc: remove `dvc metrics add/remove`
    We will be re-introducing `metrics modify` in the future and
    will likely revisit `add/remove` too, so it is better to get
    rid of these for now and introduce later with a clear scneario
    in mind.
    
    Fixes #3849
    Fixes #3907

--- dvc/repo/metrics/__init__.py
+++ dvc/repo/metrics/__init__.py
@@ -2,20 +2,10 @@ class Metrics:
     def __init__(self, repo):
         self.repo = repo
 
-    def add(self, *args, **kwargs):
-        from dvc.repo.metrics.add import add
-
-        return add(self.repo, *args, **kwargs)
-
     def show(self, *args, **kwargs):
         from dvc.repo.metrics.show import show
 
         return show(self.repo, *args, **kwargs)
-
-    def remove(self, *args, **kwargs):
-        from dvc.repo.metrics.remove import remove
-
-        return remove(self.repo, *args, **kwargs)
 
     def diff(self, *args, **kwargs):
         from .diff import diff
--- dvc/repo/metrics/add.py
+++
@@ -1,5 +0,0 @@
-from dvc.repo.metrics.modify import modify
-
-
-def add(repo, path):
-    modify(repo, path)
