bdf44f70c1e3f2ebc1688209db9a09909f7d6304
Ruslan Kuprieiev <ruslan@iterative.ai>
2020-06-04 04:21:50 +0300

    tests: move mock-ssh-server git dependency
    This one doesn't allow us to release new pre-releases, because pypi
    forbids that:
    
    https://travis-ci.com/github/iterative/dvc/jobs/343343967#L2447
    
    Let's install it separately for now, until there is a new official
    mock-ssh-server release.

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ tests_requirements = [
     "flake8-docstrings",
     "pydocstyle<4.0",
     "jaraco.windows==3.9.2",
-    "mock-ssh-server@git+https://github.com/iterative/mock-ssh-server.git",
+    "mock-ssh-server",
     "moto==1.3.14.dev464",
     "rangehttpserver==1.2.0",
     "beautifulsoup4==4.4.0",
