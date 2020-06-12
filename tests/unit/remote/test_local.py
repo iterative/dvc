d40878d465028b6dc14cc732b14a95520f4dd296
Peter Rowlands <peter@pmrowla.com>
2020-06-12 13:31:57 +0900

    tests: update unit tests for remote/cache separation

--- tests/unit/remote/test_local.py
+++ tests/unit/remote/test_local.py
@@ -5,7 +5,7 @@ import pytest
 
 from dvc.cache import NamedCache
 from dvc.path_info import PathInfo
-from dvc.remote.local import LocalRemote
+from dvc.remote.local import LocalCache
 
 
 def test_status_download_optimization(mocker, dvc):
@@ -13,28 +13,28 @@ def test_status_download_optimization(mocker, dvc):
         And the desired files to fetch are already on the local cache,
         Don't check the existence of the desired files on the remote cache
     """
-    remote = LocalRemote(dvc, {})
+    cache = LocalCache(dvc, {})
 
     infos = NamedCache()
     infos.add("local", "acbd18db4cc2f85cedef654fccc4a4d8", "foo")
     infos.add("local", "37b51d194a7513e45b56f6524f2d51f2", "bar")
 
     local_exists = list(infos["local"])
-    mocker.patch.object(remote, "cache_exists", return_value=local_exists)
+    mocker.patch.object(cache, "checksums_exist", return_value=local_exists)
 
     other_remote = mocker.Mock()
     other_remote.url = "other_remote"
-    other_remote.cache_exists.return_value = []
+    other_remote.checksums_exist.return_value = []
 
-    remote.status(infos, other_remote, download=True)
+    cache.status(infos, other_remote, download=True)
 
-    assert other_remote.cache_exists.call_count == 0
+    assert other_remote.checksums_exist.call_count == 0
 
 
 @pytest.mark.parametrize("link_name", ["hardlink", "symlink"])
 def test_is_protected(tmp_dir, dvc, link_name):
-    remote = LocalRemote(dvc, {})
-    link_method = getattr(remote.tree, link_name)
+    cache = LocalCache(dvc, {})
+    link_method = getattr(cache.tree, link_name)
 
     (tmp_dir / "foo").write_text("foo")
 
@@ -43,47 +43,47 @@ def test_is_protected(tmp_dir, dvc, link_name):
 
     link_method(foo, link)
 
-    assert not remote.is_protected(foo)
-    assert not remote.is_protected(link)
+    assert not cache.is_protected(foo)
+    assert not cache.is_protected(link)
 
-    remote.protect(foo)
+    cache.protect(foo)
 
-    assert remote.is_protected(foo)
-    assert remote.is_protected(link)
+    assert cache.is_protected(foo)
+    assert cache.is_protected(link)
 
-    remote.unprotect(link)
+    cache.unprotect(link)
 
-    assert not remote.is_protected(link)
+    assert not cache.is_protected(link)
     if os.name == "nt" and link_name == "hardlink":
         # NOTE: NTFS doesn't allow deleting read-only files, which forces us to
         # set write perms on the link, which propagates to the source.
-        assert not remote.is_protected(foo)
+        assert not cache.is_protected(foo)
     else:
-        assert remote.is_protected(foo)
+        assert cache.is_protected(foo)
 
 
 @pytest.mark.parametrize("err", [errno.EPERM, errno.EACCES])
 def test_protect_ignore_errors(tmp_dir, mocker, err):
     tmp_dir.gen("foo", "foo")
     foo = PathInfo("foo")
-    remote = LocalRemote(None, {})
+    cache = LocalCache(None, {})
 
-    remote.protect(foo)
+    cache.protect(foo)
 
     mock_chmod = mocker.patch(
         "os.chmod", side_effect=OSError(err, "something")
     )
-    remote.protect(foo)
+    cache.protect(foo)
     assert mock_chmod.called
 
 
 def test_protect_ignore_erofs(tmp_dir, mocker):
     tmp_dir.gen("foo", "foo")
     foo = PathInfo("foo")
-    remote = LocalRemote(None, {})
+    cache = LocalCache(None, {})
 
     mock_chmod = mocker.patch(
         "os.chmod", side_effect=OSError(errno.EROFS, "read-only fs")
     )
-    remote.protect(foo)
+    cache.protect(foo)
     assert mock_chmod.called
