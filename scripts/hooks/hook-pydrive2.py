4e256582d1dcde793eff7b7bb492a649aaa8a237
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2020-06-23 22:54:00 +0545

    fix import errors

--- scripts/hooks/hook-pydrive2.py
+++ scripts/hooks/hook-pydrive2.py
@@ -1,4 +1,6 @@
-from PyInstaller.utils.hooks import copy_metadata
+from PyInstaller.utils.hooks import (  # pylint:disable=import-error
+    copy_metadata,
+)
 
 datas = copy_metadata("pydrive2")
 datas += copy_metadata("google-api-python-client")
