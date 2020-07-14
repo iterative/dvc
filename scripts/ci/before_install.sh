4c9245a3b3ec6aaecbf8911b5978386079564445
Ruslan Kuprieiev <kupruser@gmail.com>
2020-07-14 13:55:14 +0300

    tests: remove real SSH (#4200)
    * tests: remove real SSH
    
    No longer needed, all tests are using mocked ssh already.
    
    * travis: don't setup ssh

--- scripts/ci/before_install.sh
+++ scripts/ci/before_install.sh
@@ -16,21 +16,6 @@ fi
 echo >env.sh
 
 if [[ "$TRAVIS_BUILD_STAGE_NAME" == "test" ]]; then
-  if [[ "$TRAVIS_OS_NAME" != "windows" ]]; then
-    # NOTE: ssh keys for ssh test to be able to ssh to the localhost
-    ssh-keygen -t rsa -N "" -f mykey
-    mkdir -p ~/.ssh
-    cp mykey ~/.ssh/id_rsa
-    cp mykey.pub ~/.ssh/id_rsa.pub
-    cat mykey.pub >>~/.ssh/authorized_keys
-    ssh-keyscan localhost >>~/.ssh/known_hosts
-    ssh localhost ls &>/dev/null
-    ssh-keyscan 127.0.0.1 >>~/.ssh/known_hosts
-    ssh 127.0.0.1 ls &>/dev/null
-    ssh-keyscan 0.0.0.0 >>~/.ssh/known_hosts
-    ssh 0.0.0.0 ls &>/dev/null
-  fi
-
   if [[ "$TRAVIS_OS_NAME" == "osx" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     brew install openssl
     $scriptdir/retry.sh brew cask install google-cloud-sdk
