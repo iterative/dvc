7c795f58f0e50d937511d8cbd7496fe274ae1e24
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2020-07-21 16:44:13 +0545

    tests on mac

--- dvc/repo/tree.py
+++ dvc/repo/tree.py
@@ -2,7 +2,7 @@ import logging
 import os
 from typing import Optional
 
-from funcy import cached_property, post_processing
+from funcy import cached_property, log_calls, post_processing
 
 from dvc.dvcfile import is_valid_filename
 from dvc.exceptions import OutputNotFoundError
@@ -260,6 +260,7 @@ class RepoTree(BaseTree):  # pylint:disable=abstract-method
         self._dvctrees = dict(
             sorted(dvctrees, key=lambda v: len(v[0]), reverse=True)
         )
+        print(self._dvctrees)
         self.tree = tree
         self._kwargs = kwargs
 
@@ -274,9 +275,11 @@ class RepoTree(BaseTree):  # pylint:disable=abstract-method
     def _find_subtree(self, path_prefix) -> Optional[DvcTree]:
         return self._find_subtree_with_prefix(path_prefix)[1]
 
+    @log_calls(logger.info)
     def _find_subtree_with_prefix(self, path):
         # dvctrees is already ordered from low to high
         path_prefix = os.path.abspath(path)
+        print(path_prefix)
         for pref, tree in self._dvctrees.items():
             if path_prefix.startswith(pref):
                 return pref, tree
--- scripts/ci/script.sh
+++ scripts/ci/script.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-python -mtests
+python -m tests tests/func/test_{get,import,ls,update,api}.py
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_SECURE_ENV_VARS" == "true" ]]; then
   pip install codecov
