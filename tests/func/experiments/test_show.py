c1e13840f4485edac18650e1cc73247ee30f1dd0
Peter Rowlands <peter@pmrowla.com>
2020-08-14 15:39:10 +0900

    update tests

--- tests/func/experiments/test_show.py
+++ tests/func/experiments/test_show.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from dvc.dvcfile import PIPELINE_FILE
 from tests.func.test_repro_multistage import COPY_SCRIPT
 
 
@@ -16,5 +19,45 @@ def test_show_simple(tmp_dir, scm, dvc):
             "metrics": {"metrics.yaml": {"foo": 1}},
             "params": {"params.yaml": {"foo": 1}},
             "queued": False,
+            "timestamp": None,
         }
     }
+
+
+def test_show_experiment(tmp_dir, scm, dvc):
+    tmp_dir.gen("copy.py", COPY_SCRIPT)
+    tmp_dir.gen("params.yaml", "foo: 1")
+    dvc.run(
+        cmd="python copy.py params.yaml metrics.yaml",
+        metrics_no_cache=["metrics.yaml"],
+        params=["foo"],
+        name="foo",
+    )
+    scm.add(["copy.py", "params.yaml", "metrics.yaml", "dvc.yaml", "dvc.lock"])
+    scm.commit("baseline")
+    baseline_rev = scm.get_rev()
+    timestamp = datetime.fromtimestamp(
+        scm.repo.rev_parse(baseline_rev).committed_date
+    )
+
+    dvc.reproduce(PIPELINE_FILE, experiment=True, params=["foo=2"])
+    results = dvc.experiments.show()
+
+    expected_baseline = {
+        "metrics": {"metrics.yaml": {"foo": 1}},
+        "params": {"params.yaml": {"foo": 1}},
+        "queued": False,
+        "timestamp": timestamp,
+    }
+    expected_params = {"foo": 2}
+
+    assert set(results.keys()) == {"workspace", baseline_rev}
+    experiments = results[baseline_rev]
+    assert len(experiments) == 2
+    for rev, exp in experiments.items():
+        if rev == "baseline":
+            assert exp == expected_baseline
+        else:
+            assert exp["metrics"]["metrics.yaml"] == expected_params
+            assert exp["params"]["params.yaml"] == expected_params
+            assert exp["timestamp"] > timestamp
