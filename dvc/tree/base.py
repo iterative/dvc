60a874603da9c25f3f9e0df6695540503e9c8838
Peter Rowlands <peter@pmrowla.com>
2020-08-21 15:04:00 +0900

    tree: add overwrite flag for tree.upload

--- dvc/tree/base.py
+++ dvc/tree/base.py
@@ -374,7 +374,14 @@ class BaseTree:
         typ, hash_ = tree.get_file_hash(to_info)
         return typ, hash_ + self.CHECKSUM_DIR_SUFFIX, to_info
 
-    def upload(self, from_info, to_info, name=None, no_progress_bar=False):
+    def upload(
+        self,
+        from_info,
+        to_info,
+        name=None,
+        no_progress_bar=False,
+        overwrite=False,
+    ):
         if not hasattr(self, "_upload"):
             raise RemoteActionNotImplemented("upload", self.scheme)
 
@@ -383,6 +390,9 @@ class BaseTree:
 
         if from_info.scheme != "local":
             raise NotImplementedError
+
+        if not overwrite and self.exists(to_info):
+            raise FileExistsError(to_info)
 
         logger.debug("Uploading '%s' to '%s'", from_info, to_info)
 
