334556f07dc511927543218d4a2a1a1c1c83ed65
Saugat Pachhai <suagatchhetri@outlook.com>
2020-08-27 02:31:25 +0000

    tests: speedup lock tests (#4471)
    * tests: speedup lock tests
    
    Patched to reduce timeout from default 5s to 0.1s
    
    * Reduce timeout further to 0.01s

--- tests/func/test_lock.py
+++ tests/func/test_lock.py
@@ -1,23 +1,23 @@
-import os
+import pytest
 
-from dvc.lock import Lock, LockError
+from dvc.lock import FAILED_TO_LOCK_MESSAGE, Lock, LockError
 from dvc.main import main
-from tests.basic_env import TestDvc
 
 
-class TestLock(TestDvc):
-    def test_with(self):
-        lockfile = os.path.join(self.dvc.dvc_dir, "tmp", "lock")
-        lock = Lock(lockfile)
-        with lock:
-            with self.assertRaises(LockError):
-                lock2 = Lock(lockfile)
-                with lock2:
-                    pass
+def test_with(tmp_dir, dvc, mocker):
+    # patching to speedup tests
+    mocker.patch("dvc.lock.DEFAULT_TIMEOUT", 0.01)
 
-    def test_cli(self):
-        lockfile = os.path.join(self.dvc.dvc_dir, "tmp", "lock")
-        lock = Lock(lockfile)
-        with lock:
-            ret = main(["add", self.FOO])
-            self.assertEqual(ret, 1)
+    lockfile = tmp_dir / dvc.tmp_dir / "lock"
+    with Lock(lockfile):
+        with pytest.raises(LockError), Lock(lockfile):
+            pass
+
+
+def test_cli(tmp_dir, dvc, mocker, caplog):
+    # patching to speedup tests
+    mocker.patch("dvc.lock.DEFAULT_TIMEOUT", 0.01)
+
+    with Lock(tmp_dir / dvc.tmp_dir / "lock"):
+        assert main(["add", "foo"]) == 1
+    assert FAILED_TO_LOCK_MESSAGE in caplog.text
