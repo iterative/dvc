60e076539e4e335eb99dfc094ae19d0042e31748
Peter Rowlands <peter@pmrowla.com>
2020-09-04 17:50:59 +0900

    use is_dvc

--- dvc/repo/diff.py
+++ dvc/repo/diff.py
@@ -120,7 +120,7 @@ def _filter_missing(repo, paths):
     repo_tree = RepoTree(repo, stream=True)
     for path in paths:
         metadata = repo_tree.metadata(path)
-        if metadata.is_output or metadata.part_of_output:
+        if metadata.is_dvc:
             out = metadata.outs[0]
             if out.status()[str(out)] == "not in cache":
                 yield path
