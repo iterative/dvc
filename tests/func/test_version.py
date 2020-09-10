65152a42a39bec5ba026263bdb3f79317ab74f00
karajan1001 <mishanyo1001@gmail.com>
2020-09-10 09:01:53 +0800

    Update tests/func/test_version.py
    Co-authored-by: Ruslan Kuprieiev <kupruser@gmail.com>

--- tests/func/test_version.py
+++ tests/func/test_version.py
@@ -3,3 +3,9 @@ from dvc.main import main
 
 def test_(tmp_dir, dvc, scm):
     assert main(["version"]) == 0
+
+    assert re.search(r"DVC version: \d+\.\d+\.\d+.*", caplog.text)
+    assert re.search(r"Platform: Python \d\.\d+\.\d+ on .*", caplog.text)
+    assert re.search(r"Supports: .*", caplog.text)
+    assert re.search(r"Cache types: .*", caplog.text)
+    assert "Repo: dvc, git" in caplog.text
