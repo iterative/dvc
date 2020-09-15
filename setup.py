4ebacf1e0ac28bd791a30e81494d155213ed6367
Ruslan Kuprieiev <kupruser@gmail.com>
2020-09-15 11:06:36 +0300

    pydrive2: require >=1.6.1
    https://github.com/iterative/PyDrive2/pull/56

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ install_requires = [
 # Extra dependencies for remote integrations
 
 gs = ["google-cloud-storage==1.19.0"]
-gdrive = ["pydrive2>=1.4.14"]
+gdrive = ["pydrive2>=1.6.1"]
 s3 = ["boto3>=1.9.201"]
 azure = ["azure-storage-blob>=12.0", "knack"]
 oss = ["oss2==2.6.1"]
