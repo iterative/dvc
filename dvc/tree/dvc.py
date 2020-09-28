2052c804cd2ce5fb8a7538bdb0cf2355b911bb49
Peter Rowlands <peter@pmrowla.com>
2020-09-28 18:53:02 +0900

    dvctree: use string paths for performance reasons in _get_granular_checksum()

--- dvc/tree/dvc.py
+++ dvc/tree/dvc.py
@@ -56,12 +56,16 @@ class DvcTree(BaseTree):  # pylint:disable=abstract-method
         assert isinstance(path_info, PathInfo)
         if not self.fetch and not self.stream:
             raise FileNotFoundError
+
+        # NOTE: use string paths here for performance reasons
+        path_str = path_info.as_posix()
+        out_path_str = out.path_info.as_posix()
         dir_cache = out.get_dir_cache(remote=remote)
         for entry in dir_cache:
             entry_relpath = entry[out.tree.PARAM_RELPATH]
             if os.name == "nt":
                 entry_relpath = entry_relpath.replace("/", os.sep)
-            if path_info == out.path_info / entry_relpath:
+            if path_str == "/".join([out_path_str, entry_relpath]):
                 return entry[out.tree.PARAM_CHECKSUM]
         raise FileNotFoundError
 
