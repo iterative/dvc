b18e3be27b14196d69de40dda3344b4b0d292366
Tomasz Łakota <tlakota@kplabs.pl>
2020-10-22 09:45:57 +0200

    generator should exit on GeneratorExit exception

--- dvc/tree/ssh/__init__.py
+++ dvc/tree/ssh/__init__.py
@@ -3,7 +3,7 @@ import io
 import logging
 import os
 import threading
-from contextlib import closing, contextmanager, suppress
+from contextlib import closing, contextmanager
 from urllib.parse import urlparse
 
 from funcy import first, memoize, silent, wrap_with
@@ -173,10 +173,11 @@ class SSHTree(BaseTree):
     def walk_files(self, path_info, **kwargs):
         with self.ssh(path_info) as ssh:
             for fname in ssh.walk_files(path_info.path):
-                with suppress(
-                    GeneratorExit
-                ):  # dropping further walk in the middle doesn't hurt
+                try:
                     yield path_info.replace(path=fname)
+                except GeneratorExit:
+                    # raised when generator is dropped in the middle of walk
+                    break
 
     def remove(self, path_info):
         if path_info.scheme != self.scheme:
