50ebc05c4741c32fce8510ed1f83fd29a398173a
karajan1001 <mishanyo1001@gmail.com>
2020-10-23 21:03:56 +0800

    Solve issue on python3.6 and 3.7

--- tests/unit/test_external_repo.py
+++ tests/unit/test_external_repo.py
@@ -100,5 +100,5 @@ def test_fetch_external_repo_jobs(
     with external_repo(str(tmp_dir)) as repo:
         spy = mocker.spy(repo.cloud, "pull")
         repo.fetch_external(["dir1"], jobs=jobs)
-        run_jobs = spy.call_args.kwargs.get("jobs")
+        run_jobs = tuple(spy.call_args_list[0])[1].get("jobs")
         assert run_jobs == jobs
