7c461b389a1ad806f01c625268fbc9d242d2221e
Tomás Farías Santana <tomas@tomasfarias.dev>
2020-10-24 22:30:43 +0200

    WebHDFSTree supports http URL and proper walking

--- dvc/tree/webhdfs.py
+++ dvc/tree/webhdfs.py
@@ -1,6 +1,9 @@
 import logging
+import os
 import threading
+from collections import deque
 from contextlib import contextmanager
+from urllib.parse import urlparse
 
 from funcy import cached_property, wrap_prop
 
@@ -13,20 +16,44 @@ from .base import BaseTree
 logger = logging.getLogger(__name__)
 
 
+def update_pbar(pbar):
+    """Update pbar to accept the two arguments passed by hdfs"""
+
+    def update(_, bytes_transfered):  # No use for first argument (path)
+        if bytes_transfered == -1:
+            return
+        pbar.update(bytes_transfered)
+
+    return update
+
+
 class WebHDFSTree(BaseTree):
     scheme = Schemes.WEBHDFS
     PATH_CLS = CloudURLInfo
     REQUIRES = {"hdfs": "hdfs"}
+    REGEX = r"^webhdfs://((?P<user>.*)@)?.*$"
 
     def __init__(self, repo, config):
         super().__init__(repo, config)
 
+        self.path_info = None
         url = config.get("url")
-        self.path_info = self.PATH_CLS(url) if url else None
+        if not url:
+            return
+
+        parsed = urlparse(url)
+        user = parsed.username or config.get("user")
+
+        self.path_info = self.PATH_CLS.from_parts(
+            scheme="webhdfs",
+            host=parsed.hostname,
+            user=user,
+            port=parsed.port,
+            path=parsed.path,
+        )
 
         self.hdfscli_config = config.get("hdfscli_config")
         self.token = config.get("webhdfs_token")
-        self.user = config.get("user")
         self.alias = config.get("webhdfs_alias")
 
     @wrap_prop(threading.Lock())
@@ -40,15 +67,17 @@ class WebHDFSTree(BaseTree):
         try:
             client = hdfs.config.Config(  # pylint: disable=no-member
                 self.hdfscli_config
-            ).get(self.alias)
+            ).get_client(self.alias)
         except hdfs.util.HdfsError:  # pylint: disable=no-member
+            http_url = f"http://{self.path_info.host}:{self.path_info.port}"
+
             if self.token is not None:
                 client = hdfs.TokenClient(  # pylint: disable=no-member
-                    self.path_info.url, self.token
+                    http_url, self.token
                 )
             else:
                 client = hdfs.InsecureClient(  # pylint: disable=no-member
-                    self.path_info.url, self.user
+                    http_url, self.path_info.user
                 )
 
         return client
@@ -63,9 +92,22 @@ class WebHDFSTree(BaseTree):
             yield reader.read()
 
     def walk_files(self, path_info, **kwargs):
-        yield self.hdfs_client.walk(
-            path_info.path, depth=kwargs.get("depth", 0)
-        )
+        if not self.exists(path_info):
+            return
+
+        root = path_info.path
+        dir_queue = deque([root])
+
+        while dir_queue:
+            for path, dirs, files in self.hdfs_client.walk(
+                dir_queue.pop(), depth=kwargs.get("depth", 0)
+            ):
+                new_dirs = [os.path.join(path, dir_) for dir_ in dirs]
+                dir_queue.extend(new_dirs)
+
+                for file_ in files:
+                    path = os.path.join(path, file_)
+                    yield path_info.replace(path=path)
 
     def remove(self, path_info):
         if path_info.scheme != self.scheme:
@@ -84,10 +126,14 @@ class WebHDFSTree(BaseTree):
         self, from_file, to_info, name=None, no_progress_bar=False, **_kwargs
     ):
         with Tqdm(desc=name, disable=no_progress_bar, bytes=True) as pbar:
-            self.hdfs_client.upload(to_info.path, from_file, progress=pbar)
+            self.hdfs_client.upload(
+                to_info.path, from_file, progress=update_pbar(pbar)
+            )
 
     def _download(
         self, from_info, to_file, name=None, no_progress_bar=False, **_kwargs
     ):
         with Tqdm(desc=name, disable=no_progress_bar, bytes=True) as pbar:
-            self.hdfs_client.download(from_info.path, to_file, progress=pbar)
+            self.hdfs_client.download(
+                from_info.path, to_file, progress=update_pbar(pbar)
+            )
