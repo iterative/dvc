0bca3cfb0d6234b535a9a574c1e4d9c515dc3ce4
damakuno <damakuno@gmail.com>
2020-10-25 21:22:27 +0800

    http: added check for HTTP status code 404
    Fixes #4288

--- dvc/tree/http.py
+++ dvc/tree/http.py
@@ -141,7 +141,13 @@ class HTTPTree(BaseTree):  # pylint:disable=abstract-method
         return response
 
     def exists(self, path_info, use_dvcignore=True):
-        return bool(self._head(path_info.url))
+        res = self._head(path_info.url)
+        if res.status_code == 404:
+            return False
+        if bool(res):
+            return True
+        res.raise_for_status()
+        return False
 
     def get_file_hash(self, path_info):
         url = path_info.url
