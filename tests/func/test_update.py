4d75e75631d64d2b9b16f354c0b8d36183159ec6
Paweł Redzyński <pawelredzynski@gmail.com>
2020-10-26 14:28:30 +0100

    test_update_import: roll back skip (#4781)

--- tests/func/test_update.py
+++ tests/func/test_update.py
@@ -7,16 +7,7 @@ from tests.unit.tree.test_repo import make_subrepo
 
 
 @pytest.mark.parametrize(
-    "cached",
-    [
-        True,
-        pytest.param(
-            False,
-            marks=pytest.mark.skip(
-                reason="https://github.com/iterative/dvc/issues/4418"
-            ),
-        ),
-    ],
+    "cached", [True, False],
 )
 def test_update_import(tmp_dir, dvc, erepo_dir, cached):
     gen = erepo_dir.dvc_gen if cached else erepo_dir.scm_gen
