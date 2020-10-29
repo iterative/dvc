070a7704ca0bf9c3f6f4fdf602c72e8b5582a323
Ruslan Kuprieiev <ruslan@iterative.ai>
2020-10-30 00:25:01 +0200

    git: use dulwich for .gitignore
    In my tests `Git._ignore` is taking around ~65seconds for the whole test
    suit(~50K invocations). The reason is that `gitpython` is launching a real
    `git check-ignore` process, that takes time and resources.
    
    Switching to `pygit2` or `dulwich` makes it drop to ~5seconds, saving us
    almost a minute of the test run time and a lot of resources by not
    launching any processes. I've chosen `dulwich` over `pygit2` here
    because it is much more pythonic and as long as the performance is
    on-par with `pygit2` it is better for us to use `dulwich`.

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ install_requires = [
     "colorama>=0.3.9",
     "configobj>=5.0.6",
     "gitpython>3",
+    "dulwich>=0.20.6",
     "setuptools>=34.0.0",
     "nanotime>=0.5.2",
     "pyasn1>=0.4.1",
