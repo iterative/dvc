79bddc6d1b1e88e84462623d43d576f125e248cf
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2020-11-01 00:57:52 +0800

    import: add --no-exec param
    To be consistent with the same --no-exec param in import-url.
    
    Fixes #4567.

--- dvc/repo/imp.py
+++ dvc/repo/imp.py
@@ -1,6 +1,8 @@
-def imp(self, url, path, out=None, fname=None, rev=None):
+def imp(self, url, path, out=None, fname=None, rev=None, no_exec=False):
     erepo = {"url": url}
     if rev is not None:
         erepo["rev"] = rev
 
-    return self.imp_url(path, out=out, fname=fname, erepo=erepo, frozen=True)
+    return self.imp_url(
+        path, out=out, fname=fname, erepo=erepo, frozen=True, no_exec=no_exec
+    )
