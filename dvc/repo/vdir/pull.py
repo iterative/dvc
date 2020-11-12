827165c9bf795295d4decba665cf0cb84e8bd2d6
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2020-11-12 23:42:30 +0800

    vdir pull: basic implementation
    Implemented these behaviors:
    - pull the .dvc/cache/*.dir
    - created the directory structure
    
    Fixes #(4657)

--- dvc/repo/vdir/pull.py
+++ dvc/repo/vdir/pull.py
@@ -1,2 +1,42 @@
-def pull(repo, *args, **kwargs):
-    return ""
+from pathlib import Path
+
+from .. import locked
+
+
+@locked
+def pull(
+    repo,
+    targets=None,
+    jobs=None,
+    remote=None,
+    all_branches=False,
+    with_deps=False,
+    all_tags=False,
+    recursive=False,
+    all_commits=False,
+):
+    used = repo.used_cache(
+        targets,
+        all_branches=all_branches,
+        all_tags=all_tags,
+        all_commits=all_commits,
+        with_deps=with_deps,
+        force=True,
+        remote=remote,
+        jobs=jobs,
+        recursive=recursive,
+    )
+
+    dirs = _create_dirs(used)
+
+    return dirs
+
+
+def _create_dirs(used):
+    dirs = set()
+    for _, files in used.scheme_names("local"):
+        for f in files:
+            d = Path(f).parent
+            dirs.add(d)
+            d.mkdir(parents=True, exist_ok=True)
+    return dirs
