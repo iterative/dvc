c3d5aeb8042539ca083b2042eabfa4d4f967bdda
Ruslan Kuprieiev <kupruser@gmail.com>
2020-11-13 21:29:46 +0200

    Update dvc/tree/base.py

--- dvc/tree/base.py
+++ dvc/tree/base.py
@@ -79,7 +79,6 @@ class BaseTree:
 
     @cached_property
     def jobs(self):
-        """Number of jobs (workers) set in a configuration"""
         return (
             self.config.get("jobs")
             or (self.repo and self.repo.config["core"].get("jobs"))
