6c0b20cfe06a86cad09b17cf99d6fc4f359cbcfd
Peter Rowlands <peter@pmrowla.com>
2020-12-04 15:19:21 +0900

    stage: track changed files when calling checkpoint callback

--- dvc/stage/run.py
+++ dvc/stage/run.py
@@ -186,5 +186,6 @@ def _kill_nt(proc):
 def _run_callback(stage, callback_func):
     stage.save(allow_missing=True)
     stage.commit(allow_missing=True)
+    stage.repo.scm.track_changed_files()
     logger.debug("Running checkpoint callback for stage '%s'", stage)
     callback_func()
