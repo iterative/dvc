06a37c88cbafec8596698b5de193e4c761d0681a
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2020-12-07 14:53:49 +0545

    run/repro: show command on dry-run, add ">" to script output
    This rollbacks the earlier commit to not show command during
    the dry-run, and also prepends ">" in the command script
    instead of "$" sign.

--- tests/unit/stage/test_run.py
+++ tests/unit/stage/test_run.py
@@ -1,13 +1,24 @@
 import logging
 
+import pytest
+
 from dvc.stage import Stage
 from dvc.stage.run import run_stage
 
 
-def test_run_stage_dry(caplog):
+@pytest.mark.parametrize(
+    "cmd, expected",
+    [
+        ("mycmd arg1 arg2", ["> mycmd arg1 arg2"]),
+        (["mycmd1 arg1", "mycmd2 arg2"], ["> mycmd1 arg1", "> mycmd2 arg2"]),
+    ],
+)
+def test_run_stage_dry(caplog, cmd, expected):
     with caplog.at_level(level=logging.INFO, logger="dvc"):
-        stage = Stage(None, "stage.dvc", cmd="mycmd arg1 arg2")
+        stage = Stage(None, "stage.dvc", cmd=cmd)
         run_stage(stage, dry=True)
-        assert caplog.messages == [
-            "Running callback stage 'stage.dvc':",
-        ]
+
+    expected.insert(
+        0, "Running callback stage 'stage.dvc':",
+    )
+    assert caplog.messages == expected
