6b9d59e50f6833bfd7712e90183fcf3bac59b3da
Ruslan Kuprieiev <ruslan@iterative.ai>
2020-12-08 15:23:19 +0200

    [WIP] git: use dulwich for add/commit

--- dvc/scm/git/backend/dulwich.py
+++ dvc/scm/git/backend/dulwich.py
@@ -57,10 +57,28 @@ class DulwichBackend(BaseGitBackend):  # pylint:disable=abstract-method
         return self.repo.commondir()
 
     def add(self, paths: Iterable[str]):
-        raise NotImplementedError
+        from dvc.utils.fs import walk_files
+
+        if isinstance(paths, str):
+            paths = [paths]
+
+        files = []
+        for path in paths:
+            if not os.path.isabs(path):
+                path = os.path.join(self.root_dir, path)
+            if os.path.isdir(path):
+                files.extend(walk_files(path))
+            else:
+                files.append(path)
+
+        for fpath in files:
+            # NOTE: this doesn't check gitignore, same as GitPythonBackend.add
+            self.repo.stage(relpath(fpath, self.root_dir))
 
     def commit(self, msg: str):
-        raise NotImplementedError
+        from dulwich.porcelain import commit
+
+        commit(self.root_dir, message=msg)
 
     def checkout(
         self, branch: str, create_new: Optional[bool] = False, **kwargs,
