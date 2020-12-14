8272cecc946b329db889518e0a8a25855b8b9af7
Paweł Redzyński <pawelredzynski@gmail.com>
2020-12-14 14:36:06 +0100

    dvc integration: add temporary test for configuration export

--- tests/func/metrics/plots/test_dvclive.py
+++ tests/func/metrics/plots/test_dvclive.py
@@ -17,6 +17,29 @@ DVCLIVE_SCRITP = dedent(
 
 
 @pytest.mark.parametrize("summary", (True, False))
+def test_export_config_tmp(tmp_dir, dvc, mocker, summary):
+    proc_spy = mocker.spy(subprocess, "Popen")
+    tmp_dir.gen("src", "dependency")
+    dvc.run(
+        cmd="mkdir logs && touch logs.json",
+        deps=["src"],
+        name="run_logger",
+        dvclive=["logs"],
+        dvclive_summary=summary,
+    )
+
+    assert proc_spy.call_count == 1
+    _, kwargs = proc_spy.call_args
+
+    assert "DVCLIVE_PATH" in kwargs["env"]
+    assert kwargs["env"]["DVCLIVE_PATH"] == "logs"
+
+    assert "DVCLIVE_SUMMARY" in kwargs["env"]
+    assert kwargs["env"]["DVCLIVE_SUMMARY"] == str(summary).lower()
+
+
+@pytest.mark.skip(reason="dvclive does not exist yet")
+@pytest.mark.parametrize("summary", (True, False))
 def test_export_config(tmp_dir, dvc, mocker, summary):
     proc_spy = mocker.spy(subprocess, "Popen")
     tmp_dir.gen("log.py", DVCLIVE_SCRITP.format(log_path="logs"))
@@ -37,6 +60,7 @@ def test_export_config(tmp_dir, dvc, mocker, summary):
     assert kwargs["env"]["DVCLIVE_SUMMARY"] == str(summary).lower()
 
 
+@pytest.mark.skip(reason="dvclive does not exist yet")
 def test_dvclive_provides_metrics(tmp_dir, dvc):
     tmp_dir.gen("log.py", DVCLIVE_SCRITP.format(log_path="logs"))
     dvc.run(
@@ -58,6 +82,7 @@ def test_dvclive_provides_metrics(tmp_dir, dvc):
     assert "logs/loss.tsv" in plots
 
 
+@pytest.mark.skip(reason="dvclive does not exist yet")
 def test_dvclive_provides_no_metrics(tmp_dir, dvc):
     tmp_dir.gen("log.py", DVCLIVE_SCRITP.format(log_path="logs"))
     dvc.run(
