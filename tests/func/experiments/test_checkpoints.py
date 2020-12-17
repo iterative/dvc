b3f3fe950458daa263828ae18d813e201494d82f
Peter Rowlands <peter@pmrowla.com>
2020-12-17 21:11:29 +0900

    update checkpoints tests

--- tests/func/experiments/test_checkpoints.py
+++ tests/func/experiments/test_checkpoints.py
@@ -1,14 +1,20 @@
+import logging
+
 import pytest
 from funcy import first
 
 from dvc.exceptions import DvcException
 from dvc.repo.experiments import Experiments, MultipleBranchError
+from dvc.repo.experiments.base import EXEC_APPLY, EXEC_CHECKPOINT
 
 
-def test_new_checkpoint(tmp_dir, scm, dvc, checkpoint_stage, mocker):
+@pytest.mark.parametrize("workspace", [True, False])
+def test_new_checkpoint(
+    tmp_dir, scm, dvc, checkpoint_stage, mocker, workspace
+):
     new_mock = mocker.spy(dvc.experiments, "new")
     results = dvc.experiments.run(
-        checkpoint_stage.addressing, params=["foo=2"]
+        checkpoint_stage.addressing, params=["foo=2"], tmp_dir=not workspace
     )
     exp = first(results)
 
@@ -22,33 +28,53 @@ def test_new_checkpoint(tmp_dir, scm, dvc, checkpoint_stage, mocker):
         with tree.open(tmp_dir / "metrics.yaml") as fobj:
             assert fobj.read().strip() == "foo: 2"
 
+    if workspace:
+        assert scm.get_ref(EXEC_APPLY) == exp
+    assert scm.get_ref(EXEC_CHECKPOINT) == exp
+    if workspace:
+        assert (tmp_dir / "foo").read_text().strip() == "5"
+        assert (tmp_dir / "metrics.yaml").read_text().strip() == "foo: 2"
+
 
 @pytest.mark.parametrize(
-    "checkpoint_resume", [Experiments.LAST_CHECKPOINT, "foo"]
+    "checkpoint_resume, workspace",
+    [
+        (Experiments.LAST_CHECKPOINT, True),
+        (Experiments.LAST_CHECKPOINT, False),
+        ("foo", True),
+        ("foo", False),
+    ],
 )
 def test_resume_checkpoint(
-    tmp_dir, scm, dvc, checkpoint_stage, checkpoint_resume
+    tmp_dir, scm, dvc, checkpoint_stage, checkpoint_resume, workspace
 ):
     with pytest.raises(DvcException):
         dvc.experiments.run(
             checkpoint_stage=checkpoint_stage.addressing,
             checkpoint_resume=checkpoint_resume,
+            tmp_dir=not workspace,
         )
 
     results = dvc.experiments.run(
-        checkpoint_stage.addressing, params=["foo=2"]
+        checkpoint_stage.addressing, params=["foo=2"], tmp_dir=not workspace
     )
 
     with pytest.raises(DvcException):
         dvc.experiments.run(
-            checkpoint_stage.addressing, checkpoint_resume="abc1234",
+            checkpoint_stage.addressing,
+            checkpoint_resume="abc1234",
+            tmp_dir=not workspace,
         )
 
     if checkpoint_resume != Experiments.LAST_CHECKPOINT:
         checkpoint_resume = first(results)
 
+    if not workspace:
+        dvc.experiments.apply(first(results))
     results = dvc.experiments.run(
-        checkpoint_stage.addressing, checkpoint_resume=checkpoint_resume
+        checkpoint_stage.addressing,
+        checkpoint_resume=checkpoint_resume,
+        tmp_dir=not workspace,
     )
     exp = first(results)
 
@@ -61,21 +87,52 @@ def test_resume_checkpoint(
         with tree.open(tmp_dir / "metrics.yaml") as fobj:
             assert fobj.read().strip() == "foo: 2"
 
+    if workspace:
+        assert scm.get_ref(EXEC_APPLY) == exp
+    assert scm.get_ref(EXEC_CHECKPOINT) == exp
 
-def test_reset_checkpoint(tmp_dir, scm, dvc, checkpoint_stage, caplog):
+
+@pytest.mark.parametrize("workspace", [True, False])
+def test_reset_checkpoint(
+    tmp_dir, scm, dvc, checkpoint_stage, caplog, workspace
+):
     from dvc.repo.experiments.base import CheckpointExistsError
 
-    dvc.experiments.run(checkpoint_stage.addressing, name="foo")
-    scm.gitpython.repo.git.reset(hard=True)
-    scm.gitpython.repo.git.clean(force=True)
+    dvc.experiments.run(
+        checkpoint_stage.addressing, name="foo", tmp_dir=not workspace,
+    )
 
-    with pytest.raises(CheckpointExistsError):
-        dvc.experiments.run(
-            checkpoint_stage.addressing, name="foo", params=["foo=2"]
-        )
+    if workspace:
+        scm.reset(hard=True)
+        scm.gitpython.repo.git.clean(force=True)
+
+    if workspace:
+        with caplog.at_level(logging.ERROR):
+            dvc.experiments.run(
+                checkpoint_stage.addressing,
+                name="foo",
+                params=["foo=2"],
+                tmp_dir=not workspace,
+            )
+        assert "checkpoint experiment conflicts with existing" in caplog.text
+
+        scm.reset(hard=True)
+        scm.gitpython.repo.git.clean(force=True)
+    else:
+        with pytest.raises(CheckpointExistsError):
+            dvc.experiments.run(
+                checkpoint_stage.addressing,
+                name="foo",
+                params=["foo=2"],
+                tmp_dir=not workspace,
+            )
 
     results = dvc.experiments.run(
-        checkpoint_stage.addressing, params=["foo=2"], name="foo", force=True
+        checkpoint_stage.addressing,
+        params=["foo=2"],
+        name="foo",
+        force=True,
+        tmp_dir=not workspace,
     )
     exp = first(results)
 
@@ -88,22 +145,43 @@ def test_reset_checkpoint(tmp_dir, scm, dvc, checkpoint_stage, caplog):
         with tree.open(tmp_dir / "metrics.yaml") as fobj:
             assert fobj.read().strip() == "foo: 2"
 
+    if workspace:
+        assert scm.get_ref(EXEC_APPLY) == exp
+    assert scm.get_ref(EXEC_CHECKPOINT) == exp
 
-def test_resume_branch(tmp_dir, scm, dvc, checkpoint_stage):
+
+@pytest.mark.parametrize("workspace", [True, False])
+def test_resume_branch(tmp_dir, scm, dvc, checkpoint_stage, workspace):
     results = dvc.experiments.run(
-        checkpoint_stage.addressing, params=["foo=2"]
+        checkpoint_stage.addressing, params=["foo=2"], tmp_dir=not workspace
     )
     branch_rev = first(results)
-
-    results = dvc.experiments.run(
-        checkpoint_stage.addressing, checkpoint_resume=branch_rev
-    )
-    checkpoint_a = first(results)
+    if not workspace:
+        dvc.experiments.apply(branch_rev)
 
     results = dvc.experiments.run(
         checkpoint_stage.addressing,
         checkpoint_resume=branch_rev,
+        tmp_dir=not workspace,
+    )
+    checkpoint_a = first(results)
+    if not workspace:
+        dvc.experiments.apply(checkpoint_a)
+
+    with pytest.raises(DvcException):
+        results = dvc.experiments.run(
+            checkpoint_stage.addressing,
+            checkpoint_resume=branch_rev,
+            params=["foo=100"],
+            tmp_dir=not workspace,
+        )
+
+    dvc.experiments.apply(branch_rev)
+    results = dvc.experiments.run(
+        checkpoint_stage.addressing,
+        checkpoint_resume=branch_rev,
         params=["foo=100"],
+        tmp_dir=not workspace,
     )
     checkpoint_b = first(results)
 
