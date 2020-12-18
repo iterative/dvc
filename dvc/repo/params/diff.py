723477ed5ab06b8935aad227a81dd88160d7824f
Saugat Pachhai <suagatchhetri@outlook.com>
2020-12-18 14:18:10 +0545

    Load from only two revisions in params/metrics (#5121)

--- dvc/repo/params/diff.py
+++ dvc/repo/params/diff.py
@@ -4,10 +4,10 @@ from dvc.utils.diff import format_dict
 from .show import NoParamsError
 
 
-def _get_params(repo, *args, rev=None, **kwargs):
+def _get_params(repo, *args, revs=None, **kwargs):
     try:
-        params = repo.params.show(*args, **kwargs, revs=[rev] if rev else None)
-        return params.get(rev or "", {})
+        params = repo.params.show(*args, **kwargs, revs=revs)
+        return params
     except NoParamsError:
         return {}
 
@@ -18,8 +18,12 @@ def diff(repo, *args, a_rev=None, b_rev=None, **kwargs):
 
     with_unchanged = kwargs.pop("all", False)
 
-    old = _get_params(repo, *args, **kwargs, rev=(a_rev or "HEAD"))
-    new = _get_params(repo, *args, **kwargs, rev=b_rev)
+    a_rev = a_rev or "HEAD"
+    b_rev = b_rev or "workspace"
+
+    params = _get_params(repo, *args, **kwargs, revs=[a_rev, b_rev])
+    old = params.get(a_rev, {})
+    new = params.get(b_rev, {})
 
     return _diff(
         format_dict(old), format_dict(new), with_unchanged=with_unchanged
