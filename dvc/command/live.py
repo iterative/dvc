6b966d28280b7a3941b45ebae69ceafd54630afa
Paweł Redzyński <pawelredzynski@gmail.com>
2020-12-21 17:34:37 +0100

    live: add show/diff

--- dvc/command/live.py
+++ dvc/command/live.py
@@ -3,7 +3,7 @@ import logging
 import os
 
 from dvc.command import completion
-from dvc.command.base import CmdBase
+from dvc.command.base import CmdBase, fix_subparsers
 from dvc.utils.html import write
 
 logger = logging.getLogger(__name__)
@@ -12,13 +12,25 @@ logger = logging.getLogger(__name__)
 class CmdLive(CmdBase):
     UNINITIALIZED = True
 
-    def run(self):
-        metrics, plots = self.repo.live.show(self.args.target, self.args.rev)
+    def _run(self, target, revs=None):
+        metrics, plots = self.repo.live.show(target, revs)
+
         html_path = self.args.target + ".html"
         write(html_path, plots, metrics)
+
         logger.info(f"\nfile://{os.path.abspath(html_path)}")
 
         return 0
+
+
+class CmdLiveShow(CmdLive):
+    def run(self):
+        self._run(self.args.target)
+
+
+class CmdLiveDiff(CmdLive):
+    def run(self):
+        self._run(self.args.target, self.args.revs)
 
 
 def add_parser(subparsers, parent_parser):
@@ -26,23 +38,54 @@ def add_parser(subparsers, parent_parser):
         "Commands to visualize and compare dvclive-produced logs."
     )
 
-    logs_parser = subparsers.add_parser(
+    live_parser = subparsers.add_parser(
         "live",
         parents=[parent_parser],
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description=LIVE_DESCRIPTION,
     )
-    logs_parser.add_argument(
-        "target", help="Logs dir to produce summary from",
-    ).complete = completion.DIR
-    logs_parser.add_argument(
+    live_subparsers = live_parser.add_subparsers(
+        dest="cmd",
+        help="Use `dvc live CMD --help` to display command-specific help.",
+    )
+
+    fix_subparsers(live_subparsers)
+
+    SHOW_HELP = "Visualize dvclive directory content."
+    live_show_parser = live_subparsers.add_parser(
+        "show",
+        parents=[parent_parser],
+        help=SHOW_HELP,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    _add_common_arguments(live_show_parser)
+    live_show_parser.set_defaults(func=CmdLiveShow)
+
+    DIFF_HELP = (
+        "Show multiple versions of dvclive data, "
+        "by plotting it in single view."
+    )
+    live_diff_parser = live_subparsers.add_parser(
+        "diff",
+        parents=[parent_parser],
+        help=DIFF_HELP,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    live_diff_parser.add_argument(
         "--rev",
         nargs="*",
         default=None,
         help="Git revision (e.g. SHA, branch, tag)",
         metavar="<commit>",
     )
-    logs_parser.add_argument(
+    _add_common_arguments(live_diff_parser)
+    live_show_parser.set_defaults(func=CmdLiveDiff)
+
+
+def _add_common_arguments(parser):
+    parser.add_argument(
+        "target", help="Logs dir to produce summary from",
+    ).complete = completion.DIR
+    parser.add_argument(
         "-f", "--file", default=None, help="Name of the generated file."
     )
-    logs_parser.set_defaults(func=CmdLive)
