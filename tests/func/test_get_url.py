eb8e03a465156cb3f2624fed8367b9e33b9594c5
Dmitry Kropachev <40304587+dkropachev@users.noreply.github.com>
2020-12-25 10:56:04 +0700

    dvc/tree/local: fix it for no repo/cache case (#5118)
    * dvc/tree/local: fix it for no repo/cache case
    
    fixes #5069
    
    * Update dvc/output/base.py
    
    * Update dvc/output/base.py
    
    Co-authored-by: Ruslan Kuprieiev <kupruser@gmail.com>

--- tests/func/test_get_url.py
+++ tests/func/test_get_url.py
@@ -13,6 +13,16 @@ def test_get_file(tmp_dir):
     assert (tmp_dir / "foo_imported").read_text() == "foo contents"
 
 
+def test_get_dir(tmp_dir):
+    tmp_dir.gen({"foo": {"foo": "foo contents"}})
+
+    Repo.get_url("foo", "foo_imported")
+
+    assert (tmp_dir / "foo_imported").is_dir()
+    assert (tmp_dir / "foo_imported" / "foo").is_file()
+    assert (tmp_dir / "foo_imported" / "foo").read_text() == "foo contents"
+
+
 @pytest.mark.parametrize("dname", [".", "dir", "dir/subdir"])
 def test_get_url_to_dir(tmp_dir, dname):
     tmp_dir.gen({"foo": "foo contents", "dir": {"subdir": {}}})
