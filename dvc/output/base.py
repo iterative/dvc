718705654a347ae685d7f5958af908d35bee9989
Ruslan Kuprieiev <kupruser@gmail.com>
2020-12-25 05:26:11 +0200

    Update dvc/output/base.py

--- dvc/output/base.py
+++ dvc/output/base.py
@@ -277,8 +277,6 @@ class BaseOutput:
             self.verify_metric()
 
         if not self.use_cache:
-            if self.repo:
-                self.hash_info = self.get_hash()
             if not self.IS_DEPENDENCY:
                 logger.debug(
                     "Output '%s' doesn't use cache. Skipping saving.", self
