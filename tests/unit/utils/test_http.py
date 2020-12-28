c64b4ce78f553c40a7916e1857bbdd63ff7891ab
Dmitry Kropachev <40304587+dkropachev@users.noreply.github.com>
2020-12-29 04:34:24 +0700

    dvc/utils/stream: make IterStream seekable and peekable (#5084)
    fixes #5021

--- tests/unit/utils/test_http.py
+++ tests/unit/utils/test_http.py
@@ -5,6 +5,16 @@ import requests
 from dvc.utils.http import open_url
 
 
+def read_nbytes(fd, num, mode="r"):
+    data = b"" if mode == "rb" else ""
+    while len(data) < num:
+        chunk = fd.read(num - len(data))
+        if not chunk:
+            break
+        data += chunk
+    return data
+
+
 def test_open_url(tmp_path, monkeypatch, http):
     # Simulate bad connection
     original_iter_content = requests.Response.iter_content
@@ -27,5 +37,20 @@ def test_open_url(tmp_path, monkeypatch, http):
 
     with open_url((http / "sample.txt").url) as fd:
         # Test various .read() variants
-        assert fd.read(len(text)) == text
+        assert read_nbytes(fd, len(text), mode="r") == text
         assert fd.read() == text
+        assert fd.read() == ""
+
+
+def test_open_url_peek_rb(tmp_path, monkeypatch, http):
+    # Goes over seek feature in 'rb' mode
+    text = "0123456789" * (io.DEFAULT_BUFFER_SIZE // 10 + 1)
+    http.gen("sample.txt", text * 2)
+
+    with open_url((http / "sample.txt").url, mode="rb") as fd:
+        text = text.encode("utf8")
+        assert fd.peek(len(text)) == text
+        assert read_nbytes(fd, len(text), mode="rb") == text
+        assert fd.peek(len(text)) == text
+        assert fd.read() == text
+        assert fd.read() == b""
