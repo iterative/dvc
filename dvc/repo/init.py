1f82bc2eccda808d06d532471c0e93a7baaa8be4
yihaojiang <njuhobby@gmail.com>
2021-01-02 22:18:38 +0800

    black reformat

--- dvc/repo/init.py
+++ dvc/repo/init.py
@@ -85,7 +85,9 @@ def init(root_dir=os.curdir, no_scm=False, force=False, subdir=False):
     if scm.ignore_file:
         scm.add([os.path.join(dvc_dir, scm.ignore_file)])
 
-    logger.info("\nInitialized DVC repository. "
-                "You can now commit the changes to git.\n")
+    logger.info(
+        "\nInitialized DVC repository. "
+        "You can now commit the changes to git.\n"
+    )
 
     return proj
