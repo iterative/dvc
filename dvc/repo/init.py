fa5487ebfe7dec9d035a3e140b326042bad345b6
Yihao Jiang <Njuhobby@users.noreply.github.com>
2021-01-07 18:20:35 +0800

    show more info when successfully init dvc repo (#5190)
    * show more info when successfully init dvc repo
    
    * Ignore 'line too long' linter error
    
    * simpler solution
    
    * black reformat
    
    * don't break dvc init --no-scm case
    
    * Update dvc/repo/init.py
    
    Co-authored-by: Saugat Pachhai <suagatchhetri@outlook.com>
    
    * Update dvc/repo/init.py
    
    Co-authored-by: Saugat Pachhai <suagatchhetri@outlook.com>

--- dvc/repo/init.py
+++ dvc/repo/init.py
@@ -82,8 +82,10 @@ def init(root_dir=os.curdir, no_scm=False, force=False, subdir=False):
         [config.files["repo"], dvcignore, proj.plots.templates.templates_dir]
     )
 
+    logger.info("Initialized DVC repository.\n")
+
     if scm.ignore_file:
         scm.add([os.path.join(dvc_dir, scm.ignore_file)])
-        logger.info("\nYou can now commit the changes to git.\n")
+        logger.info("You can now commit the changes to git.\n")
 
     return proj
