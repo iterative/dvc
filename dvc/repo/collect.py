2277a1a6557522471f62f458e5ea8f10de501082
Paweł Redzyński <pawelredzynski@gmail.com>
2021-01-22 19:57:43 +0100

    metrics/plots/params: return ordered (#5297)
    * metrics/plots/params: return ordered
    
    * metrics/params/plots: add tests for return order

--- dvc/repo/collect.py
+++ dvc/repo/collect.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from typing import TYPE_CHECKING, Callable, Iterable, Set, Tuple
+from typing import TYPE_CHECKING, Callable, Iterable, List, Tuple
 
 from dvc.path_info import PathInfo
 from dvc.types import DvcPath
@@ -13,19 +13,19 @@ logger = logging.getLogger(__name__)
 
 
 FilterFn = Callable[["BaseOutput"], bool]
-Outputs = Set["BaseOutput"]
-DvcPaths = Set[DvcPath]
+Outputs = List["BaseOutput"]
+DvcPaths = List[DvcPath]
 
 
 def _collect_outs(
     repo: "Repo", output_filter: FilterFn = None, deps: bool = False,
 ) -> Outputs:
-    outs = {
+    outs = [
         out
         for stage in repo.graph  # using `graph` to ensure graph checks run
         for out in (stage.deps if deps else stage.outs)
-    }
-    return set(filter(output_filter, outs)) if output_filter else outs
+    ]
+    return list(filter(output_filter, outs)) if output_filter else outs
 
 
 def _collect_paths(
@@ -36,14 +36,14 @@ def _collect_paths(
 ):
     from dvc.tree.repo import RepoTree
 
-    path_infos = {PathInfo(os.path.abspath(target)) for target in targets}
+    path_infos = [PathInfo(os.path.abspath(target)) for target in targets]
     tree = RepoTree(repo)
 
-    target_infos = set()
+    target_infos = []
     for path_info in path_infos:
 
         if recursive and tree.isdir(path_info):
-            target_infos.update(set(tree.walk_files(path_info)))
+            target_infos.extend(tree.walk_files(path_info))
 
         if not tree.exists(path_info):
             if not recursive:
@@ -51,19 +51,19 @@ def _collect_paths(
                     "'%s' was not found at: '%s'.", path_info, rev,
                 )
             continue
-        target_infos.add(path_info)
+        target_infos.append(path_info)
     return target_infos
 
 
 def _filter_duplicates(
     outs: Outputs, path_infos: DvcPaths
 ) -> Tuple[Outputs, DvcPaths]:
-    res_outs: Outputs = set()
-    res_infos = set(path_infos)
+    res_outs: Outputs = []
+    res_infos = path_infos
 
     for out in outs:
         if out.path_info in path_infos:
-            res_outs.add(out)
+            res_outs.append(out)
             res_infos.remove(out.path_info)
 
     return res_outs, res_infos
@@ -82,7 +82,7 @@ def collect(
     outs: Outputs = _collect_outs(repo, output_filter=output_filter, deps=deps)
 
     if not targets:
-        path_infos: DvcPaths = set()
+        path_infos: DvcPaths = []
         return outs, path_infos
 
     target_infos = _collect_paths(repo, targets, recursive=recursive, rev=rev)
