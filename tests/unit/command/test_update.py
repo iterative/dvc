a6e33452505191e4fa6ae5a731e4846e6bb50c30
Batuhan Taskaya <batuhanosmantaskaya@gmail.com>
2021-01-27 10:02:03 +0300

    unit tests

--- tests/unit/command/test_update.py
+++ tests/unit/command/test_update.py
@@ -13,5 +13,40 @@ def test_update(dvc, mocker):
     assert cmd.run() == 0
 
     m.assert_called_once_with(
-        targets=["target1", "target2"], rev="REV", recursive=True,
+        targets=["target1", "target2"],
+        rev="REV",
+        recursive=True,
+        to_remote=False,
+        remote=None,
+        jobs=None,
+    )
+
+
+def test_update_to_remote(dvc, mocker):
+    cli_args = parse_args(
+        [
+            "update",
+            "target1",
+            "target2",
+            "--to-remote",
+            "-j",
+            "5",
+            "-r",
+            "remote",
+            "--recursive",
+        ]
+    )
+    assert cli_args.func == CmdUpdate
+    cmd = cli_args.func(cli_args)
+    m = mocker.patch("dvc.repo.Repo.update")
+
+    assert cmd.run() == 0
+
+    m.assert_called_once_with(
+        targets=["target1", "target2"],
+        rev=None,
+        recursive=True,
+        to_remote=True,
+        remote="remote",
+        jobs=5,
     )
