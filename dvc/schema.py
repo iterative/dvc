7d9b6aa8e5ed6c4120b6dac6820b3bb173e427e5
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2021-02-01 10:44:38 +0545

    don't allow loading stages without `cmd`
    This was loosened when introducing parametrization.
    But, `foreach`..`do` should be considered first
    before stage's regular structure. And, cmd is made
    required (as it should have been)
    
    Related: #5371, #5370, #5312

--- dvc/schema.py
+++ dvc/schema.py
@@ -1,4 +1,4 @@
-from voluptuous import Any, Optional, Required, Schema
+from voluptuous import Any, Exclusive, Optional, Required, Schema
 
 from dvc import dependency, output
 from dvc.hash_info import HashInfo
@@ -79,7 +79,7 @@ PARAM_PSTAGE_NON_DEFAULT_SCHEMA = {str: [str]}
 VARS_SCHEMA = [str, dict]
 
 STAGE_DEFINITION = {
-    StageParams.PARAM_CMD: Any(str, list),
+    Required(StageParams.PARAM_CMD): Any(str, list),
     Optional(StageParams.PARAM_WDIR): str,
     Optional(StageParams.PARAM_DEPS): [str],
     Optional(StageParams.PARAM_PARAMS): [Any(str, dict)],
@@ -100,7 +100,7 @@ FOREACH_IN = {
     Required(FOREACH_KWD): Any(dict, list, str),
     Required(DO_KWD): STAGE_DEFINITION,
 }
-SINGLE_PIPELINE_STAGE_SCHEMA = {str: Any(STAGE_DEFINITION, FOREACH_IN)}
+SINGLE_PIPELINE_STAGE_SCHEMA = {str: Exclusive(FOREACH_IN, STAGE_DEFINITION)}
 MULTI_STAGE_SCHEMA = {
     STAGES: SINGLE_PIPELINE_STAGE_SCHEMA,
     VARS_KWD: VARS_SCHEMA,
