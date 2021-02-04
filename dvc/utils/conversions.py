e7cc7765ac5e09f5e6269a40e3f1af05c8b6956d
Batuhan Taskaya <batuhanosmantaskaya@gmail.com>
2021-02-04 15:58:42 +0300

    config: Support ~/.aws/config parsing (#5378)
    * config: Support ~/.aws/config parsing
    
    * skip when the ~/.aws/config doesn't exist
    
    * Expand windows paths properly

---
+++ dvc/utils/conversions.py
@@ -0,0 +1,24 @@
+# https://github.com/aws/aws-cli/blob/5aa599949f60b6af554fd5714d7161aa272716f7/awscli/customizations/s3/utils.py
+
+MULTIPLIERS = {
+    "kb": 1024,
+    "mb": 1024 ** 2,
+    "gb": 1024 ** 3,
+    "tb": 1024 ** 4,
+    "kib": 1024,
+    "mib": 1024 ** 2,
+    "gib": 1024 ** 3,
+    "tib": 1024 ** 4,
+}
+
+
+def human_readable_to_bytes(value):
+    value = value.lower()
+    suffix = None
+    if value.endswith(tuple(MULTIPLIERS.keys())):
+        size = 2
+        size += value[-2] == "i"  # KiB, MiB etc
+        value, suffix = value[:-size], value[-size:]
+
+    multiplier = MULTIPLIERS.get(suffix, 1)
+    return int(value) * multiplier
