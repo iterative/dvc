550cbb5e1154dff930b1cff33ee159bb16b03693
Peter Rowlands (변기호) <peter@pmrowla.com>
2021-02-05 20:43:44 +0900

    git: update dulwich dependency to 0.20.18 (#5420)

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ install_requires = [
     "colorama>=0.3.9",
     "configobj>=5.0.6",
     "gitpython>3",
-    "dulwich>=0.20.15",
+    "dulwich>=0.20.18",
     "pygit2>=1.4.0",
     "setuptools>=34.0.0",
     "nanotime>=0.5.2",
