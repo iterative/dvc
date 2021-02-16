98ad86889cd935d6934b8b8730685dee3dc0395f
Batuhan Taskaya <batuhanosmantaskaya@gmail.com>
2021-02-16 16:02:29 +0300

    update: forbid updating repo-imports with --to-remote

--- dvc/stage/imports.py
+++ dvc/stage/imports.py
@@ -1,9 +1,16 @@
 import logging
 
+from dvc.exceptions import InvalidArgumentError
+
 logger = logging.getLogger(__name__)
 
 
 def _update_import_on_remote(stage, remote, jobs):
+    if stage.is_repo_import:
+        raise InvalidArgumentError(
+            "Can't update a repo import with --to-remote"
+        )
+
     url = stage.deps[0].path_info.url
     stage.outs[0].hash_info = stage.repo.cloud.transfer(
         url, jobs=jobs, remote=remote, command="update"
