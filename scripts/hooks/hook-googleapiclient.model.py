50a6301fd0ba155415548ad86594b1e9bfb7c655
Helio Machado <0x2b3bfa0+git@googlemail.com>
2021-03-14 10:44:14 +0100

    Suppress linter error for unrecognized imports
    Co-authored-by: Peter Rowlands (변기호) <peter@pmrowla.com>

--- scripts/hooks/hook-googleapiclient.model.py
+++ scripts/hooks/hook-googleapiclient.model.py
@@ -1,5 +1,7 @@
 """Temporary fix for https://github.com/iterative/dvc/issues/5618."""
-from PyInstaller.utils.hooks import collect_data_files, copy_metadata
+from PyInstaller.utils.hooks import (    # pylint:disable=import-error
+    collect_data_files, copy_metadata
+)
 
 datas = collect_data_files("googleapiclient.discovery")
 datas += copy_metadata("google_api_python_client")
