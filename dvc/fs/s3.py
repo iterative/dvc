130bdd790b7bbe8c4e5decf9aa44fd6734cc231f
Batuhan Taskaya <batuhanosmantaskaya@gmail.com>
2021-03-23 12:33:42 +0300

    Use _download/_upload with old implementation

--- dvc/fs/s3.py
+++ dvc/fs/s3.py
@@ -1,18 +1,22 @@
 import os
 import threading
 from collections import defaultdict
+from contextlib import contextmanager
 
 from funcy import cached_property, wrap_prop
 
+from dvc.exceptions import DvcException
 from dvc.path_info import CloudURLInfo
+from dvc.progress import Tqdm
 from dvc.scheme import Schemes
+from dvc.utils import error_link
 
 from .fsspec_wrapper import FSSpecWrapper
 
 _AWS_CONFIG_PATH = os.path.join(os.path.expanduser("~"), ".aws", "config")
 
 
-class S3FileSystem(FSSpecWrapper):
+class BaseS3FileSystem(FSSpecWrapper):
     scheme = Schemes.S3
     PATH_CLS = CloudURLInfo
     REQUIRES = {"s3fs": "s3fs"}
@@ -134,3 +138,100 @@ class S3FileSystem(FSSpecWrapper):
         return self.fs.open(
             self._with_bucket(path_info), mode=mode, **self._open_args
         )
+
+
+class S3FileSystem(BaseS3FileSystem):
+    @wrap_prop(threading.Lock())
+    @cached_property
+    def s3(self):
+        import boto3
+
+        login_info = self.login_info
+        client_kwargs = login_info.get("client_kwargs", {})
+        session_opts = {
+            "profile_name": login_info.get("profile"),
+            "region_name": client_kwargs.get("region_name"),
+        }
+
+        if "key" in login_info:
+            session_opts["aws_access_key_id"] = login_info["key"]
+        if "secret" in login_info:
+            session_opts["aws_secret_access_key"] = login_info["secret"]
+        if "token" in login_info:
+            session_opts["aws_session_token"] = login_info["token"]
+
+        session = boto3.session.Session(**session_opts)
+        # pylint: disable=attribute-defined-outside-init
+        self.endpoint_url = client_kwargs.get("endpoint_url")
+        return session.resource(
+            "s3",
+            endpoint_url=self.endpoint_url,
+            use_ssl=login_info["use_ssl"],
+        )
+
+    @contextmanager
+    def _get_s3(self):
+        from botocore.exceptions import (
+            EndpointConnectionError,
+            NoCredentialsError,
+        )
+
+        try:
+            yield self.s3
+        except NoCredentialsError as exc:
+            link = error_link("no-credentials")
+            raise DvcException(
+                f"Unable to find AWS credentials. {link}"
+            ) from exc
+        except EndpointConnectionError as exc:
+            link = error_link("connection-error")
+            name = self.endpoint_url or "AWS S3"
+            raise DvcException(
+                f"Unable to connect to '{name}'. {link}"
+            ) from exc
+
+    @contextmanager
+    def _get_bucket(self, bucket):
+        with self._get_s3() as s3:
+            try:
+                yield s3.Bucket(bucket)
+            except s3.meta.client.exceptions.NoSuchBucket as exc:
+                link = error_link("no-bucket")
+                raise DvcException(
+                    f"Bucket '{bucket}' does not exist. {link}"
+                ) from exc
+
+    @contextmanager
+    def _get_obj(self, path_info):
+        with self._get_bucket(path_info.bucket) as bucket:
+            try:
+                yield bucket.Object(path_info.path)
+            except bucket.meta.client.exceptions.NoSuchKey as exc:
+                raise DvcException(f"{path_info.url} does not exist") from exc
+
+    def _upload(
+        self, from_file, to_info, name=None, no_progress_bar=False, **_kwargs
+    ):
+        with self._get_obj(to_info) as obj:
+            total = os.path.getsize(from_file)
+            with Tqdm(
+                disable=no_progress_bar, total=total, bytes=True, desc=name
+            ) as pbar:
+                obj.upload_file(
+                    from_file,
+                    Callback=pbar.update,
+                    ExtraArgs=self.login_info.get("s3_additional_kwargs"),
+                )
+
+    def _download(
+        self, from_info, to_file, name=None, no_progress_bar=False, **pbar_args
+    ):
+        with self._get_obj(from_info) as obj:
+            with Tqdm(
+                disable=no_progress_bar,
+                total=obj.content_length,
+                bytes=True,
+                desc=name,
+                **pbar_args,
+            ) as pbar:
+                obj.download_file(to_file, Callback=pbar.update)
