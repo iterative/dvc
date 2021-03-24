f2ba67aece1d5ff3bc5aaf2d40948606c3f7e5c8
Ruslan Kuprieiev <kupruser@gmail.com>
2021-03-24 09:43:05 +0200

    build: upgrade pyinstaller to 4.2 (#5682)
    * build: upgrade pyinstaller to 4.2
    
    * pyinstaller: hooks: remove hook-googleapiclient.model.py
    
    https://github.com/pyinstaller/pyinstaller-hooks-contrib/blob/master/src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-googleapiclient.model.py
    
    * gha: use python 3.8 for pyinstaller

--- scripts/pyinstaller/hooks/hook-googleapiclient.model.py
+++
@@ -1,8 +0,0 @@
-"""Temporary fix for https://github.com/iterative/dvc/issues/5618."""
-from PyInstaller.utils.hooks import (  # pylint:disable=import-error
-    collect_data_files,
-    copy_metadata,
-)
-
-datas = collect_data_files("googleapiclient.discovery")
-datas += copy_metadata("google_api_python_client")
