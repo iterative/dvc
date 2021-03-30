3c12ab96faa7cc60763b49224070bf2ce69c4c34
Nigel <nigel.van.hattum@ordina.nl>
2021-03-30 17:25:29 +0200

    Changed param name to avoid conflicts

--- dvc/fs/s3.py
+++ dvc/fs/s3.py
@@ -38,7 +38,7 @@ class S3FileSystem(BaseFileSystem):
         self.endpoint_url = config.get("endpointurl")
 
         self.use_ssl = config.get("use_ssl", True)
-        self.verify = config.get("verify", True)
+        self.verify_ssl = config.get("verify_ssl", True)
 
         self.extra_args = {}
 
@@ -137,7 +137,7 @@ class S3FileSystem(BaseFileSystem):
         return session.resource(
             "s3",
             endpoint_url=self.endpoint_url,
-            verify=self.verify,
+            verify=self.verify_ssl,
             use_ssl=self.use_ssl,
             config=boto3.session.Config(
                 signature_version="s3v4", s3=s3_config
