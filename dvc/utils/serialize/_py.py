ff850ca5866a84443914dc736e809bd1f9283975
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2021-04-07 16:54:47 +0545

    fix segment

--- dvc/utils/serialize/_py.py
+++ dvc/utils/serialize/_py.py
@@ -168,7 +168,7 @@ def _ast_assign_to_dict(
         v = assign.value
         offsets = slice(v.col_offset, v.end_col_offset)
         lno = assign.lineno - 1
-        segment = src_lines.get(lno, []).get(offsets)
+        segment = src_lines[lno][offsets] if len(src_lines) > lno else None
         result[name] = {"lineno": lno, "value": value, "segment": segment}
     else:
         result[name] = value
