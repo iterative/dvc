8790eb3dc0c444117c1ec2c6557689c7b1ec03fc
Batuhan Taskaya <batuhanosmantaskaya@gmail.com>
2021-04-13 12:10:43 +0300

    tests: use motoserver docker image

--- tests/remotes/s3.py
+++ tests/remotes/s3.py
@@ -1,6 +1,5 @@
 import locale
 import os
-import time
 import uuid
 
 import pytest
@@ -12,18 +11,17 @@ from dvc.utils import env2bool
 from .base import Base
 
 TEST_AWS_REPO_BUCKET = os.environ.get("DVC_TEST_AWS_REPO_BUCKET", "dvc-temp")
-
-TEST_AWS_S3_PORT = 5555
-TEST_AWS_ENDPOINT_URL = f"http://127.0.0.1:{TEST_AWS_S3_PORT}/"
+TEST_AWS_ENDPOINT_URL = "http://127.0.0.1:{port}/"
 
 
 class S3(Base, CloudURLInfo):
 
     IS_OBJECT_STORAGE = True
+    TEST_AWS_ENDPOINT_URL = None
 
     @cached_property
     def config(self):
-        return {"url": self.url, "endpointurl": TEST_AWS_ENDPOINT_URL}
+        return {"url": self.url, "endpointurl": self.TEST_AWS_ENDPOINT_URL}
 
     @staticmethod
     def should_test():
@@ -56,7 +54,7 @@ class S3(Base, CloudURLInfo):
     def _s3(self):
         import boto3
 
-        return boto3.client("s3", endpoint_url=TEST_AWS_ENDPOINT_URL)
+        return boto3.client("s3", endpoint_url=self.config["endpointurl"])
 
     def is_file(self):
         from botocore.exceptions import ClientError
@@ -126,41 +124,27 @@ def s3_fake_creds_file(monkeypatch):
         aws_creds.unlink()
 
 
-# Due to moto being uncompatible with aioboto on wrapper
-# mode, we start the moto server as a subprocess (imitates
-# a real S3 service) and then create a client that uses
-# this server.
-#
-# Originally adopted from:
-# https://github.com/dask/s3fs/blob/main/s3fs/tests/test_s3fs.py#L66-L86
 @pytest.fixture(scope="session")
-def s3_server(test_config):
-    test_config.requires("s3")
-
-    import shlex
-    import subprocess
-
+def s3_server(test_config, docker_compose, docker_services):
     import requests
 
-    proc = subprocess.Popen(
-        shlex.split(f"moto_server s3 -p {TEST_AWS_S3_PORT}")
-    )
+    test_config.requires("s3")
 
-    timeout = 5
-    while timeout > 0:
+    port = docker_services.port_for("motoserver", 5000)
+    endpoint_url = TEST_AWS_ENDPOINT_URL.format(port=port)
+
+    def _check():
         try:
-            r = requests.get(TEST_AWS_ENDPOINT_URL)
-            if r.ok:
-                break
+            r = requests.get(endpoint_url)
+            return r.ok
         except requests.RequestException:
-            pass
-        timeout -= 0.1
-        time.sleep(0.1)
+            return False
 
-    yield
-
-    proc.terminate()
-    proc.wait()
+    docker_services.wait_until_responsive(
+        timeout=60.0, pause=0.1, check=_check
+    )
+    S3.TEST_AWS_ENDPOINT_URL = endpoint_url
+    return endpoint_url
 
 
 @pytest.fixture
