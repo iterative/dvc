9fca48418de53607e40b05fcff5d48cf9111ee7b
maximerischard <rischardmaxime@gmail.com>
2021-04-14 14:19:48 +0100

    Remove rwlock decorator for the run() method of the Stage class.
    Instead of pre-emptively grabbing read locks on the dependencies
    and write locks on the outputs when calling the run() method,
    delegate the locking mechanism to other methods.
    This results in more selective locking that is less likely
    to cause a LockError when executing steps in parallel.

--- dvc/stage/__init__.py
+++ dvc/stage/__init__.py
@@ -489,7 +489,6 @@ class Stage(params.StageParams):
         if link_failures:
             raise CacheLinkError(link_failures)
 
-    @rwlocked(read=["deps"], write=["outs"])
     def run(
         self,
         dry=False,
@@ -503,16 +502,16 @@ class Stage(params.StageParams):
 
         if not self.frozen and self.is_import:
             jobs = kwargs.get("jobs", None)
-            sync_import(self, dry, force, jobs)
+            self.sync_import(self, dry, force, jobs)
         elif not self.frozen and self.cmd:
-            run_stage(self, dry, force, **kwargs)
+            self.run_stage(self, dry, force, **kwargs)
         else:
             args = (
                 ("outputs", "frozen ") if self.frozen else ("data sources", "")
             )
             logger.info("Verifying %s in %s%s", *args, self)
             if not dry:
-                check_missing_outputs(self)
+                self.check_missing_outputs(self)
 
         if not dry:
             if kwargs.get("checkpoint_func", None):
@@ -520,6 +519,18 @@ class Stage(params.StageParams):
             self.save(allow_missing=allow_missing)
             if not no_commit:
                 self.commit(allow_missing=allow_missing)
+
+    @rwlocked(read=["deps"], write=["outs"])
+    def _run_stage(self, dry, force, **kwargs):
+        return run_stage(self, dry, force, **kwargs)
+
+    @rwlocked(read=["deps"], write=["outs"])
+    def _sync_import(self, dry, force, jobs)
+        sync_import(self, dry, force, jobs)
+
+    @rwlocked(read=["outs"])
+    def _check_missing_outputs(self):
+        check_missing_outputs(self)
 
     def filter_outs(self, path_info):
         def _func(o):
