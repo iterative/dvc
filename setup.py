e7571ab3251ef4417fa196e7ad45b7f6f8a42ee3
Batuhan Taskaya <batuhanosmantaskaya@gmail.com>
2021-05-10 18:28:11 +0300

    setup: fixate aiobotocore/botocore/boto3 versions (#5987)

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ install_requires = [
 
 gs = ["gcsfs==2021.4.0"]
 gdrive = ["pydrive2>=1.8.1", "six >= 1.13.0"]
-s3 = ["s3fs==2021.4.0", "boto3==1.16.52"]
+s3 = ["s3fs==2021.4.0", "aiobotocore[boto3]==1.3.0"]
 azure = ["adlfs==0.7.1", "azure-identity>=1.4.0", "knack"]
 # https://github.com/Legrandin/pycryptodome/issues/465
 oss = ["oss2==2.6.1", "pycryptodome>=3.10"]
