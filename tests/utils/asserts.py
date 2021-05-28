c1387a594c01c0a83ca511c3f4fb3c51ac2c61de
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2021-05-28 13:51:51 +0545

    use kwargs when passing command line args to run

---
+++ tests/utils/asserts.py
@@ -0,0 +1,13 @@
+from typing import Any
+from unittest.mock import ANY, Mock
+
+
+def called_once_with_subset(m: Mock, *args: Any, **kwargs: Any) -> bool:
+    m.assert_called_once()
+    m_args, m_kwargs = m.call_args
+
+    expected_args = m_args + (ANY,) * (len(m_args) - len(args))
+    expected_kwargs = {k: kwargs.get(k, ANY) for k in m_kwargs}
+    m.assert_called_with(*expected_args, **expected_kwargs)
+
+    return True
