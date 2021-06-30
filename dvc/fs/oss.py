52f32069fdcfde5d88556ce81333ca78968c1c46
karajan1001 <mishanyo1001@gmail.com>
2021-06-30 11:04:14 +0800

    Performance optimaztion and ossfs version update

--- dvc/fs/oss.py
+++ dvc/fs/oss.py
@@ -1,8 +1,10 @@
+import os
 import threading
 
 from funcy import cached_property, wrap_prop
 
 from dvc.path_info import CloudURLInfo
+from dvc.progress import Tqdm
 from dvc.scheme import Schemes
 
 from .fsspec_wrapper import ObjectFSWrapper
@@ -29,3 +31,42 @@ class OSSFileSystem(ObjectFSWrapper):
         from ossfs import OSSFileSystem as _OSSFileSystem
 
         return _OSSFileSystem(**self.fs_args)
+
+    def _upload(
+        self, from_file, to_info, name=None, no_progress_bar=False, **pbar_args
+    ):
+        total = os.path.getsize(from_file)
+        resumable = total > self.fs.SIMPLE_TRANSFER_SIZE
+        with Tqdm(
+            disable=no_progress_bar,
+            total=total,
+            bytes=True,
+            desc=name,
+            **pbar_args,
+        ) as pbar:
+            self.fs.put_file(
+                from_file,
+                to_info.url,
+                resumable=resumable,
+                progress_callback=pbar.update_to,
+            )
+        self.fs.invalidate_cache(self._with_bucket(to_info.parent))
+
+    def _download(
+        self, from_info, to_file, name=None, no_progress_bar=False, **pbar_args
+    ):
+        total = self.fs.size(from_info.url)
+        resumable = total > self.fs.SIMPLE_TRANSFER_SIZE
+        with Tqdm(
+            disable=no_progress_bar,
+            total=total,
+            bytes=True,
+            desc=name,
+            **pbar_args,
+        ) as pbar:
+            self.fs.get_file(
+                from_info.url,
+                to_file,
+                resumable=resumable,
+                progress_callback=pbar.update_to,
+            )
