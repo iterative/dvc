a4dd936f3d8f803f009646ee7d7ab0d9d8f5ad57
Ruslan Kuprieiev <ruslan@iterative.ai>
2021-07-03 00:59:30 +0300

    output: don't use os.path.join
    Takes around 2sec for 700K objects.

--- dvc/output.py
+++ dvc/output.py
@@ -887,7 +887,7 @@ class Output:
         obj.name = str(self)
         if isinstance(obj, Tree):
             for key, entry_obj in obj:
-                entry_obj.name = os.path.join(obj.name, *key)
+                entry_obj.name = os.path.sep.join([obj.name, *key])
 
     def get_used_external(
         self, **kwargs
