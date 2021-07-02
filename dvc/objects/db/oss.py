da67a8f3325480ff794d0386fa436c87454b262e
karajan1001 <mishanyo1001@gmail.com>
2021-07-02 15:26:37 +0800

    Bump and add comments
    1. Bump ossfs from 2021.6.2 to 2021.7.0
    2. add some comments to the verification trigger

--- dvc/objects/db/oss.py
+++ dvc/objects/db/oss.py
@@ -2,4 +2,9 @@ from .base import ObjectDB
 
 
 class OSSObjectDB(ObjectDB):
+    """
+    The oss remote is migrated to ossfs recently
+    add some additional verification for the data
+    """
+
     DEFAULT_VERIFY = True
