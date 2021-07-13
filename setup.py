58f0911234540959552ac5a61ae6e7e0280e51fe
Batuhan Taskaya <batuhanosmantaskaya@gmail.com>
2021-07-13 13:39:30 +0300

    azure: support directory imports with azure (#6245)
    * azure: support directory imports with azure
    
    * upgrade azurite to 3.13
    
    * tmp: use azurite 3.12?
    
    * upgrade to azurite 3.13.1
    
    * bump adlfs to 2021.7.0

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ install_requires = [
 gs = ["gcsfs==2021.6.1"]
 gdrive = ["pydrive2>=1.8.1", "six >= 1.13.0"]
 s3 = ["s3fs==2021.6.1", "aiobotocore[boto3]==1.3.0"]
-azure = ["adlfs==0.7.1", "azure-identity>=1.4.0", "knack"]
+azure = ["adlfs==2021.7.0", "azure-identity>=1.4.0", "knack"]
 # https://github.com/Legrandin/pycryptodome/issues/465
 oss = ["oss2==2.6.1", "pycryptodome>=3.10"]
 ssh = ["paramiko[invoke]>=2.7.0"]
