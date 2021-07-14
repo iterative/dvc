cdc8e89d677211f2af7bbca73b3292c4986f5eef
Batuhan Taskaya <batuhan@python.org>
2021-07-14 18:29:37 +0300

    setup: upgrade fsspec/s3fs/gcsfs to 2021.7.0

--- setup.py
+++ setup.py
@@ -89,16 +89,16 @@ install_requires = [
     "python-benedict>=0.21.1",
     "pyparsing==2.4.7",
     "typing_extensions>=3.7.4",
-    "fsspec>=2021.6.1",
+    "fsspec>=2021.7.0",
     "diskcache>=5.2.1",
 ]
 
 
 # Extra dependencies for remote integrations
 
-gs = ["gcsfs==2021.6.1"]
+gs = ["gcsfs==2021.7.0"]
 gdrive = ["pydrive2>=1.8.1", "six >= 1.13.0"]
-s3 = ["s3fs==2021.6.1", "aiobotocore[boto3]==1.3.0"]
+s3 = ["s3fs==2021.7.0", "aiobotocore[boto3]==1.3.0"]
 azure = ["adlfs==2021.7.0", "azure-identity>=1.4.0", "knack"]
 # https://github.com/Legrandin/pycryptodome/issues/465
 oss = ["ossfs==2021.7.3"]
