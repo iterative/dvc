66d0df2d120bd49776079913e0d6868753ceb5e5
Batuhan Taskaya <batuhanosmantaskaya@gmail.com>
2021-07-20 11:59:45 +0300

    setup: upgrade adlfs to 2021.7.1 (#6337)
    The previous release got yanked on the PyPI, due to it containing backwards incompatible changes.

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ install_requires = [
 gs = ["gcsfs==2021.7.0"]
 gdrive = ["pydrive2>=1.8.1", "six >= 1.13.0"]
 s3 = ["s3fs==2021.7.0", "aiobotocore[boto3]==1.3.0"]
-azure = ["adlfs==2021.7.0", "azure-identity>=1.4.0", "knack"]
+azure = ["adlfs==2021.7.1", "azure-identity>=1.4.0", "knack"]
 # https://github.com/Legrandin/pycryptodome/issues/465
 oss = ["ossfs==2021.7.5"]
 ssh = ["paramiko[invoke]>=2.7.0"]
