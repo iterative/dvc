dd1803dcadc6aeaccaaead2ca8af043030e8370a
Nikita Kodenko <kodenkonikita@gmail.com>
2021-07-21 09:44:51 +0700

    tests: test_prompt: unittest.mock -> pytest-mock

--- tests/unit/test_prompt.py
+++ tests/unit/test_prompt.py
@@ -1,12 +1,10 @@
-from unittest import mock
-
 from dvc.prompt import confirm
 
 
-def test_confirm_in_tty_if_stdin_is_closed():
-    with mock.patch("dvc.prompt.input", side_effect=EOFError) as mock_input:
-        with mock.patch("sys.stdout.isatty", return_value=True) as mock_isatty:
-            ret = confirm("message")
-            mock_isatty.assert_called()
-            mock_input.assert_called()
-            assert not ret
+def test_confirm_in_tty_if_stdin_is_closed(mocker):
+    mock_input = mocker.patch("dvc.prompt.input", side_effect=EOFError)
+    mock_isatty = mocker.patch("sys.stdout.isatty", return_value=True)
+    ret = confirm("message")
+    mock_isatty.assert_called()
+    mock_input.assert_called()
+    assert not ret
