7444696dcd114bac4a80942b0eca34ab87bab5ad
Saugat Pachhai <suagatchhetri@outlook.com>
2021-08-03 01:30:04 +0545

    Fix merging of params when empty (#6378)
    * Fix merging of params when empty
    
    Recent release of python-benedict==0.24.1 started ignoring empty dictionaries
    during merge. The way we expected to work was questionable and also how
    benedict itself tries to handle the references to empty dictionary is
    also questionable.
    
    I consider this to be a proper way to handle this even if this gets fixed
    on the benedict side.
    
    * Revert "setup: limit python-benedict version (#6377)"
    
    This reverts commit 15de826266c913a967a2913293f59c65b333fdf1.
    
    * add comment for why passing through benedict

--- dvc/utils/collections.py
+++ dvc/utils/collections.py
@@ -80,6 +80,20 @@ def chunk_dict(d: Dict[_KT, _VT], size: int = 1) -> List[Dict[_KT, _VT]]:
     return [{key: d[key] for key in chunk} for chunk in chunks(size, d)]
 
 
+def merge_params(src: Dict, to_update: Dict) -> Dict:
+    """Recursively merges params with benedict's syntax support in-place."""
+    from benedict import benedict
+
+    if src:
+        benedict(src).merge(to_update, overwrite=True)
+    else:
+        # benedict has issues keeping references to an empty dictionary
+        # see: https://github.com/iterative/dvc/issues/6374.
+        # Also, passing to_update through benedict to expand the syntax.
+        src.update(benedict(to_update))
+    return src
+
+
 class _NamespacedDict(dict):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
