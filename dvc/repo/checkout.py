b26d79089cbc1874cb671f219f4c3a2d4664eccb
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2021-08-04 14:19:46 +0545

    migrate checkout/diff to use index.outs

--- dvc/repo/checkout.py
+++ dvc/repo/checkout.py
@@ -28,13 +28,7 @@ def _fspath_dir(path):
 
 
 def _remove_unused_links(repo):
-    used = [
-        out.fspath
-        for stage in repo.index.stages
-        for out in stage.outs
-        if out.scheme == "local"
-    ]
-
+    used = [out.fspath for out in repo.index.outs if out.scheme == "local"]
     unused = repo.state.get_unused_links(used, repo.fs)
     ret = [_fspath_dir(u) for u in unused]
     repo.state.remove_links(unused, repo.fs)
