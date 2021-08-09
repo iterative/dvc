749bab76096366f8ae11e716831e34d8cbc2a3cc
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2021-08-09 11:41:49 +0200

    Added refresh tag

--- dvc/utils/html.py
+++ dvc/utils/html.py
@@ -6,6 +6,7 @@ from dvc.types import StrPath
 PAGE_HTML = """<!DOCTYPE html>
 <html>
 <head>
+    {refresh_tag}
     <title>DVC Plot</title>
     <script src="https://cdn.jsdelivr.net/npm/vega@5.10.0"></script>
     <script src="https://cdn.jsdelivr.net/npm/vega-lite@4.8.1"></script>
@@ -31,14 +32,19 @@ class MissingPlaceholderError(DvcException):
 class HTML:
     PLACEHOLDER = "plot_divs"
     PLACEHOLDER_FORMAT_STR = f"{{{PLACEHOLDER}}}"
+    REFRESH_PLACEHOLDER = "refresh_tag"
+    REFRESH_TAG = '<meta http-equiv="refresh" content="{}">'
 
-    def __init__(self, template: str = None):
+    def __init__(self, template: Optional[str] = None, refresh_seconds: Optional[int] = None):
         template = template or PAGE_HTML
         if self.PLACEHOLDER_FORMAT_STR not in template:
             raise MissingPlaceholderError(self.PLACEHOLDER_FORMAT_STR)
 
         self.template = template
         self.elements: List[str] = []
+        self.refresh_tag = ""
+        if refresh_seconds is not None:
+            self.refresh_tag = self.REFRESH_TAG.format(refresh_seconds)
 
     def with_metrics(self, metrics: Dict[str, Dict]) -> "HTML":
         import tabulate
@@ -70,7 +76,10 @@ class HTML:
         return self
 
     def embed(self) -> str:
-        kwargs = {self.PLACEHOLDER: "\n".join(self.elements)}
+        kwargs = {
+            self.PLACEHOLDER: "\n".join(self.elements),
+            self.REFRESH_PLACEHOLDER: self.refresh_tag
+        }
         return self.template.format(**kwargs)
 
 
@@ -79,13 +88,14 @@ def write(
     plots: Dict[str, Dict],
     metrics: Optional[Dict[str, Dict]] = None,
     template_path: Optional[StrPath] = None,
+    refresh_seconds: Optional[int] = None,
 ):
     page_html = None
     if template_path:
         with open(template_path) as fobj:
             page_html = fobj.read()
 
-    document = HTML(page_html)
+    document = HTML(page_html, refresh_seconds=refresh_seconds)
     if metrics:
         document.with_metrics(metrics)
         document.with_element("<br>")
