da7849f88d93bc48df99cb831b3cb1a2049b6ff4
karajan1001 <mishanyo1001@gmail.com>
2021-08-12 16:08:15 +0800

    Better test cases, more corner case

--- tests/func/experiments/test_remove.py
+++ tests/func/experiments/test_remove.py
@@ -4,29 +4,40 @@ from dvc.repo.experiments.utils import exp_refs_by_rev
 
 
 def test_remove_experiments_by_ref(tmp_dir, scm, dvc, exp_stage, caplog):
-    results = dvc.experiments.run(exp_stage.addressing, params=["foo=2"])
-    exp = first(results)
-    ref_info = first(exp_refs_by_rev(scm, exp))
+    queue_length = 3
+    ref_list = []
 
-    assert dvc.experiments.remove(["non-exist"]) == 0
+    for i in range(queue_length):
+        results = dvc.experiments.run(
+            exp_stage.addressing, params=[f"foo={i}"]
+        )
+        ref_info = first(exp_refs_by_rev(scm, first(results)))
+        ref_list.append(str(ref_info))
+
+    assert dvc.experiments.remove(ref_list[:2] + ["non-exist"]) == 2
     assert ("'non-exist' is not a valid experiment") in caplog.text
-
-    removed = dvc.experiments.remove([str(ref_info)])
-    assert removed == 1
-    assert scm.get_ref(str(ref_info)) is None
+    assert scm.get_ref(str(ref_list[0])) is None
+    assert scm.get_ref(str(ref_list[1])) is None
+    assert scm.get_ref(str(ref_list[2])) is not None
 
 
 def test_remove_all_queued_experiments(tmp_dir, scm, dvc, exp_stage):
     queue_length = 3
+
     for i in range(queue_length):
         dvc.experiments.run(
             exp_stage.addressing, params=[f"foo={i}"], queue=True
         )
 
+    results = dvc.experiments.run(
+        exp_stage.addressing, params=[f"foo={queue_length}"]
+    )
+    ref_info = first(exp_refs_by_rev(scm, first(results)))
+
     assert len(dvc.experiments.stash) == queue_length
-    removed = dvc.experiments.remove(queue=True)
-    assert removed == queue_length
+    assert dvc.experiments.remove(queue=True) == queue_length
     assert len(dvc.experiments.stash) == 0
+    assert scm.get_ref(str(ref_info)) is not None
 
 
 def test_remove_special_queued_experiments(tmp_dir, scm, dvc, exp_stage):
@@ -38,12 +49,24 @@ def test_remove_special_queued_experiments(tmp_dir, scm, dvc, exp_stage):
         exp_stage.addressing, params=["foo=2"], queue=True, name="queue2"
     )
     rev2 = first(results)
+    results = dvc.experiments.run(
+        exp_stage.addressing, params=["foo=3"], queue=True, name="queue3"
+    )
+    rev3 = first(results)
+    results = dvc.experiments.run(exp_stage.addressing, params=["foo=4"])
+    ref_info1 = first(exp_refs_by_rev(scm, first(results)))
+    results = dvc.experiments.run(exp_stage.addressing, params=["foo=5"])
+    ref_info2 = first(exp_refs_by_rev(scm, first(results)))
+
     assert rev1 in dvc.experiments.stash_revs
     assert rev2 in dvc.experiments.stash_revs
+    assert rev3 in dvc.experiments.stash_revs
+    assert scm.get_ref(str(ref_info1)) is not None
+    assert scm.get_ref(str(ref_info2)) is not None
 
-    assert dvc.experiments.remove(["queue1"]) == 1
+    assert dvc.experiments.remove(["queue1", rev2[:5], str(ref_info1)]) == 3
     assert rev1 not in dvc.experiments.stash_revs
-    assert rev2 in dvc.experiments.stash_revs
-
-    assert dvc.experiments.remove([rev2[:5]]) == 1
-    assert len(dvc.experiments.stash) == 0
+    assert rev2 not in dvc.experiments.stash_revs
+    assert rev3 in dvc.experiments.stash_revs
+    assert scm.get_ref(str(ref_info1)) is None
+    assert scm.get_ref(str(ref_info2)) is not None
