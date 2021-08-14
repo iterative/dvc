f27d31f1e2866a3eae34182e3b8fc315d9930790
Ruslan Kuprieiev <kupruser@gmail.com>
2021-08-15 02:39:00 +0300

    pyinstaller: build: check dvc doctor output

--- scripts/pyinstaller/hooks/hook-dvc.py
+++ scripts/pyinstaller/hooks/hook-dvc.py
@@ -1,2 +1,19 @@
+from PyInstaller.utils.hooks import (  # pylint:disable=import-error
+    copy_metadata,
+)
+
+# needed for `dvc doctor` to show dep versions
+datas = copy_metadata("adlfs", recursive=True)
+datas += copy_metadata("knack")
+datas += copy_metadata("gcsfs")
+datas += copy_metadata("pyarrow")
+datas += copy_metadata("hdfs")
+datas += copy_metadata("pydrive2")
+datas += copy_metadata("s3fs", recursive=True)
+datas += copy_metadata("boto3")
+datas += copy_metadata("ossfs")
+datas += copy_metadata("sshfs")
+datas += copy_metadata("webdav4")
+
 # https://github.com/pypa/setuptools/issues/1963
 hiddenimports = ["pkg_resources.py2_warn"]
