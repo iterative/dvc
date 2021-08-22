dmpetrov/nlx

address feedback, add tests

tests/unit/remote/test_index.py
@@ -8,7 +8,7 @@ from dvc.remote.index import RemoteIndex
 
 @pytest.fixture(scope="function")
 def index(dvc):
-    idx = RemoteIndex(dvc.index_tmp_dir, "foo")
+    idx = RemoteIndex(dvc.index_db_dir, "foo")
     idx.load()
     yield idx
     idx.dump()

iterative/dvc

tests/unit/remote/test_index.py
@@ -8,7 +8,7 @@ from dvc.objects.db.index import ObjectDBIndex
 
 @pytest.fixture
 def index(dvc):
-    index_ = ObjectDBIndex(dvc.index_tmp_dir, "foo")
+    index_ = ObjectDBIndex(dvc.index_db_dir, "foo")
     yield index_
 
 