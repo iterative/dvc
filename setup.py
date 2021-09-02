7a31929423e6005713c3a1fe7ed3cb7f3c607b39
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2021-09-02 17:57:48 +0200

    Moved dvclive to rest_requirements

--- setup.py
+++ setup.py
@@ -115,13 +115,9 @@ ssh_gssapi = ["sshfs[gssapi]>=2021.7.1"]
 all_remotes = gs + s3 + azure + ssh + oss + gdrive + hdfs + webhdfs + webdav
 
 terraform = ssh + ["python-terraform>=0.10.1", "jinja2>=2.0.0"]
-dvclive = ["dvclive>=0.2.1"]
-
 tests_requirements = (
-    (Path("test_requirements.txt").read_text().strip().splitlines())
-    + terraform
-    + dvclive
-)
+    Path("test_requirements.txt").read_text().strip().splitlines()
+) + terraform
 
 setup(
     name="dvc",
