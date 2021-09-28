05eaeef012db112e4c853499556f547b9b50ee26
Saugat Pachhai <suagatchhetri@outlook.com>
2021-09-28 19:19:52 +0545

    utils.http: remove open url (#6700)
    This was used before on opening http urls as a stream,
    but we no longer use those (as we use fsspec's open method).
    
    One thing to note however is that, compared to previous
    implementation, fsspec's http filesystem's open is not very
    robust to failures.

--- dvc/utils/stream.py
+++ dvc/utils/stream.py
@@ -8,61 +8,6 @@ from dvc.istextfile import DEFAULT_CHUNK_SIZE, istextblock
 from dvc.utils import dos2unix
 
 
-class IterStream(io.RawIOBase):
-    """Wraps an iterator yielding bytes as a file object"""
-
-    def __init__(self, iterator):  # pylint: disable=super-init-not-called
-        self.iterator = iterator
-        self.leftover = b""
-
-    def readable(self):
-        return True
-
-    def writable(self) -> bool:
-        return False
-
-    # Python 3 requires only .readinto() method, it still uses other ones
-    # under some circumstances and falls back if those are absent. Since
-    # iterator already constructs byte strings for us, .readinto() is not the
-    # most optimal, so we provide .read1() too.
-
-    def readinto(self, b):
-        try:
-            n = len(b)  # We're supposed to return at most this much
-            chunk = self.leftover or next(self.iterator)
-            output, self.leftover = chunk[:n], chunk[n:]
-
-            n_out = len(output)
-            b[:n_out] = output
-            return n_out
-        except StopIteration:
-            return 0  # indicate EOF
-
-    readinto1 = readinto
-
-    def read1(self, n=-1):
-        try:
-            chunk = self.leftover or next(self.iterator)
-        except StopIteration:
-            return b""
-
-        # Return an arbitrary number or bytes
-        if n <= 0:
-            self.leftover = b""
-            return chunk
-
-        output, self.leftover = chunk[:n], chunk[n:]
-        return output
-
-    def peek(self, n):
-        while len(self.leftover) < n:
-            try:
-                self.leftover += next(self.iterator)
-            except StopIteration:
-                break
-        return self.leftover[:n]
-
-
 class HashedStreamReader(io.IOBase):
 
     PARAM_CHECKSUM = "md5"
