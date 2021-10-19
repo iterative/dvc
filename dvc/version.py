4244e5a4754928315ccc996a005f3ee9d102f962
Saugat Pachhai <suagatchhetri@outlook.com>
2021-10-20 00:44:09 +0545

    fix #6835 (#6837)

--- dvc/version.py
+++ dvc/version.py
@@ -1,6 +1,6 @@
 # pylint: disable=unused-import
 try:
-    from ._version import version as __version__
-    from ._version import version_tuple  # noqa: F401
+    from ._dvc_version import version as __version__
+    from ._dvc_version import version_tuple  # noqa: F401
 except ImportError:
     __version__ = "UNKNOWN"
