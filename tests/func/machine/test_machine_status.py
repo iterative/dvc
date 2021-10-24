f43c04f93537f51b96edb205e7e31c1b7b69086c
karajan1001 <mishanyo1001@gmail.com>
2021-10-24 17:56:24 +0800

    Make machine status name optional
    fix: #6838
    1. make machine status name optional.
    2. show all machine status if no name provide.
    3. UI changes for this cmd.
    4. add a new test for it.
    
    Co-authored-by: Dave Berenbaum <dave.berenbaum@gmail.com>

--- tests/func/machine/test_machine_status.py
+++ tests/func/machine/test_machine_status.py
@@ -8,6 +8,7 @@ def test_status(
     status = machine_instance
     assert main(["machine", "status", "foo"]) == 0
     cap = capsys.readouterr()
-    assert "instance_num_1:" in cap.out
+    assert "machine 'foo':\n" in cap.out
+    assert "\tinstance_num_1:\n" in cap.out
     for key in CmdMachineStatus.SHOWN_FIELD:
-        assert f"\t{key:20}: {status[key]}" in cap.out
+        assert f"\t\t{key:20}: {status[key]}\n" in cap.out
