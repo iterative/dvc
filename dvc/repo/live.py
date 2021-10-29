318b269ad118f198bb97b203e151529398da0f54
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2021-10-29 21:30:47 +0200

    live: Call `webbrowser.open`.
    Use `funcy.once_per_args` decorator to prevent opening the html multiple times if user closes first time.
    
    Closes iterative/dvclive#180

--- dvc/repo/live.py
+++ dvc/repo/live.py
@@ -1,5 +1,10 @@
 import logging
+import webbrowser
+from pathlib import Path
+from platform import uname
 from typing import TYPE_CHECKING, List, Optional
+
+from funcy import once_per_args
 
 from dvc.render.utils import render
 
@@ -9,15 +14,27 @@ if TYPE_CHECKING:
     from dvc.output import Output
     from dvc.repo import Repo
 
+webbrowser_open = once_per_args(webbrowser.open)
+
 
 def create_summary(out):
+
     assert out.live and out.live["html"]
 
     metrics, plots = out.repo.live.show(out.fs_path)
 
     html_path = out.fs_path + "_dvc_plots"
 
-    render(out.repo, plots, metrics=metrics, path=html_path, refresh_seconds=5)
+    index_path = render(
+        out.repo, plots, metrics=metrics, path=html_path, refresh_seconds=5
+    )
+
+    url = index_path.as_uri()
+
+    if "Microsoft" in uname().release:
+        url = Path(index_path) / "index.html"
+
+    webbrowser_open(url)
 
 
 def summary_fs_path(out: "Output") -> Optional[str]:
