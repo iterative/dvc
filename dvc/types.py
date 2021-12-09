9bb6494e55b02e34e9941bea532af5691b683531
karajan1001 <mishanyo1001@gmail.com>
2021-12-09 19:30:25 +0800

    Push: accept multiple as input(#7196)
    fix: #7196
    
    New feature: now `dvc exp push` can accept a list of experiment names as input.
    
    1. refactor of experiment push
    2. add support to list input for the `exp push`
    3. add function tests for the multi exp push
    4. add unit test for the arg parse

--- dvc/types.py
+++ dvc/types.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
+from typing import TYPE_CHECKING, Any, Dict, Iterable, List, Optional, Union
 
 if TYPE_CHECKING:
     from os import PathLike
@@ -10,3 +10,4 @@ OptStr = Optional[str]
 TargetType = Union[List[str], str]
 DictStrAny = Dict[str, Any]
 DictAny = Dict[Any, Any]
+IterableStr = Iterable[str]
