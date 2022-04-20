0782ad81b93e3fb77951c7e6decbefed6730f7c5
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2022-04-20 12:05:27 +0545

    executors: add support for cancel_futures=True in Python<3.9
    Also adds a kwarg `cancel_on_error` on the constructor to support
    cancelling futures on `shutdown`. This is not done by default to
    be backward compatible with `concurrent.futures.ThreadPool` executor.

--- dvc/utils/threadpool.py
+++ dvc/utils/threadpool.py
@@ -1,3 +1,5 @@
+import queue
+import sys
 from concurrent import futures
 from itertools import islice
 from typing import Any, Callable, Iterable, Iterator, Set, TypeVar
@@ -7,6 +9,12 @@ _T = TypeVar("_T")
 
 class ThreadPoolExecutor(futures.ThreadPoolExecutor):
     _max_workers: int
+
+    def __init__(
+        self, max_workers: int = None, cancel_on_error: bool = False, **kwargs
+    ):
+        super().__init__(max_workers=max_workers, **kwargs)
+        self._cancel_on_error = cancel_on_error
 
     @property
     def max_workers(self) -> int:
@@ -32,3 +40,35 @@ class ThreadPoolExecutor(futures.ThreadPoolExecutor):
             for fut in done:
                 yield fut.result()
             tasks.update(create_taskset(len(done)))
+
+    def shutdown(self, wait=True, *, cancel_futures=False):
+        if sys.version_info > (3, 9):
+            # pylint: disable=unexpected-keyword-arg
+            return super().shutdown(wait=wait, cancel_futures=cancel_futures)
+
+        with self._shutdown_lock:
+            self._shutdown = True
+            if cancel_futures:
+                # Drain all work items from the queue, and then cancel their
+                # associated futures.
+                while True:
+                    try:
+                        work_item = self._work_queue.get_nowait()
+                    except queue.Empty:
+                        break
+                    if work_item is not None:
+                        work_item.future.cancel()
+
+            # Send a wake-up to prevent threads calling
+            # _work_queue.get(block=True) from permanently blocking.
+            self._work_queue.put(None)
+        if wait:
+            for t in self._threads:
+                t.join()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._cancel_on_error:
+            self.shutdown(wait=True, cancel_futures=exc_val is not None)
+        else:
+            self.shutdown(wait=True)
+        return False
