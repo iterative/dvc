23e62a9ff7970de697f6a1ebbf55e6fc1538dee7
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2022-04-20 23:32:16 +0545

    fs: make localfs fsspec-compliant
    It is not completely fsspec compliant, but it's upto the level
    required by our FSSpecWrapper.
    
    Next thing to tackle would be to merging `fs.base.FileSystem`
    and `fs.fsspec_wrapper.FSSpecWrapper` into one filesystem
    that is lazily-loaded and is itself fsspec-compliant.

--- tests/func/test_add.py
+++ tests/func/test_add.py
@@ -783,7 +783,7 @@ def test_add_optimization_for_hardlink_on_empty_files(tmp_dir, dvc, mocker):
     m = mocker.spy(LocalFileSystem, "is_hardlink")
     stages = dvc.add(["foo", "bar", "lorem", "ipsum"])
 
-    assert m.call_count == 4
+    assert m.call_count == 8
     assert m.call_args != call(tmp_dir / "foo")
     assert m.call_args != call(tmp_dir / "bar")
 
