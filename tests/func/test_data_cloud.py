394b82ae8c69dcb20b980e2eaf343e42bcad15cc
Karthikeyan Singaravelan <tir.karthi@gmail.com>
2022-05-07 00:39:09 +0530

    Move test to test_data_cloud.

--- tests/func/test_data_cloud.py
+++ tests/func/test_data_cloud.py
@@ -8,6 +8,7 @@ from flaky.flaky_decorator import flaky
 import dvc as dvc_module
 from dvc.cli import main
 from dvc.data.db.local import LocalObjectDB
+from dvc.exceptions import GlobDoesNotMatchError
 from dvc.external_repo import clean_repos
 from dvc.objects.db import ObjectDB
 from dvc.stage.exceptions import StageNotFound
@@ -570,3 +571,18 @@ def test_target_remote(tmp_dir, dvc, make_remote):
         "6b18131dc289fd37006705affe961ef8.dir",
         "b8a9f715dbb64fd5c56e7783c6820a61",
     }
+
+
+def test_push_pull_glob_no_match(
+    tmp_dir, dvc, erepo_dir, run_copy, local_remote
+):
+    tmp_dir.gen("foo", "foo")
+    erepo_dir.add_remote(config=local_remote.config)
+
+    with erepo_dir.chdir():
+        with pytest.raises(GlobDoesNotMatchError):
+            dvc.push(targets="b*", glob=True)
+
+        with pytest.raises(GlobDoesNotMatchError):
+            dvc.pull(targets="b*", glob=True)
+        assert not os.path.exists(erepo_dir.dvc.stage_cache.cache_dir)
