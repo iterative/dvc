dcd3e07e0e5351855bc2965bbf40d9890f9e2a80
karajan1001 <mishanyo1001@gmail.com>
2022-05-09 17:48:34 +0800

    exp: implement internal queue.kill support(#7587)
    fix: #7587
    1. Add implement kill method for local queue class
    2. Add a test to make sure the following job will be success after the
       original job was killed.
    3. Some refactoring work on `exp remove`

--- tests/unit/repo/experiments/queue/test_local.py
+++ tests/unit/repo/experiments/queue/test_local.py
@@ -8,8 +8,8 @@ def test_shutdown_no_tasks(test_queue, mocker):
 
 
 @shared_task
-def _foo():
-    return "foo"
+def _foo(arg="foo"):
+    return arg
 
 
 def test_shutdown_active_tasks(test_queue, mocker):
@@ -35,3 +35,35 @@ def test_shutdown_active_tasks(test_queue, mocker):
     assert "foo" == result.get()
     assert result.id not in test_queue._shutdown_task_ids
     shutdown_spy.assert_called_once()
+
+
+def test_post_run_after_kill(test_queue, mocker):
+    import time
+
+    from celery import chain
+
+    sig_bar = test_queue.proc.run(
+        ["python3", "-c", "import time; time.sleep(5)"], name="bar"
+    )
+    result_bar = sig_bar.freeze()
+    sig_foo = _foo.s()
+    result_foo = sig_foo.freeze()
+    run_chain = chain(sig_bar, sig_foo)
+
+    run_chain.delay()
+    timeout = time.time() + 10
+
+    while True:
+        if result_bar.status == "RUNNING":
+            break
+        if time.time() > timeout:
+            raise AssertionError()
+
+    assert result_foo.status == "PENDING"
+    test_queue.proc.kill("bar")
+
+    while True:
+        if result_foo.status == "SUCCESS":
+            break
+        if time.time() > timeout:
+            raise AssertionError()
