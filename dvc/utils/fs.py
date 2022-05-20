d9c387b4f788e35e631bb576c7e0c14fc758f625
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2022-05-20 17:08:21 +0545

    dvc: remove unused utils and redundant tests
    The tests should be in dvc-objects repo npw, and will be migrated soon.
    The nanotime and diskcache are specified in dvc-objects, so we don't need
    those here.

--- dvc/utils/fs.py
+++ dvc/utils/fs.py
@@ -4,18 +4,14 @@ import os
 import shutil
 import stat
 import sys
-from contextlib import contextmanager, suppress
 from typing import TYPE_CHECKING
 
 from dvc.exceptions import DvcException
-from dvc.fs import system
 
 if TYPE_CHECKING:
     from dvc.types import StrPath
 
 logger = logging.getLogger(__name__)
-
-LOCAL_CHUNK_SIZE = 2**20  # 1 MB
 
 
 class BasePathNotInCheckedPathException(DvcException):
@@ -27,6 +23,8 @@ class BasePathNotInCheckedPathException(DvcException):
 
 
 def contains_symlink_up_to(path: "StrPath", base_path: "StrPath"):
+    from dvc.fs import system
+
     base_path = os.path.normcase(os.fspath(base_path))
     path = os.path.normcase(os.fspath(path))
 
@@ -40,27 +38,6 @@ def contains_symlink_up_to(path: "StrPath", base_path: "StrPath"):
     if os.path.dirname(path) == path:
         return False
     return contains_symlink_up_to(os.path.dirname(path), base_path)
-
-
-def move(src, dst):
-    """Atomically move src to dst and chmod it with mode.
-
-    Moving is performed in two stages to make the whole operation atomic in
-    case src and dst are on different filesystems and actual physical copying
-    of data is happening.
-    """
-    from shortuuid import uuid
-
-    dst = os.path.abspath(dst)
-    tmp = f"{dst}.{uuid()}"
-
-    if os.path.islink(src):
-        shutil.copy(src, tmp)
-        _unlink(src, _chmod)
-    else:
-        shutil.move(src, tmp)
-
-    shutil.move(tmp, dst)
 
 
 def _chmod(func, p, excinfo):  # pylint: disable=unused-argument
@@ -141,72 +118,3 @@ def makedirs(path, exist_ok=False, mode=None):
         os.chmod(path, mode)
     except OSError:
         logger.trace("failed to chmod '%o' '%s'", mode, path, exc_info=True)
-
-
-def copyfile(src, dest, callback=None, no_progress_bar=False, name=None):
-    """Copy file with progress bar"""
-    name = name if name else os.path.basename(dest)
-    total = os.stat(src).st_size
-
-    if os.path.isdir(dest):
-        dest = os.path.join(dest, os.path.basename(src))
-
-    if callback:
-        callback.set_size(total)
-
-    try:
-        system.reflink(src, dest)
-    except OSError:
-        from dvc.fs.callbacks import Callback
-
-        with open(src, "rb") as fsrc, open(dest, "wb+") as fdest:
-            with Callback.as_tqdm_callback(
-                callback,
-                size=total,
-                bytes=True,
-                disable=no_progress_bar,
-                desc=name,
-            ) as cb:
-                wrapped = cb.wrap_attr(fdest, "write")
-                while True:
-                    buf = fsrc.read(LOCAL_CHUNK_SIZE)
-                    if not buf:
-                        break
-                    wrapped.write(buf)
-
-    if callback:
-        callback.absolute_update(total)
-
-
-def copy_fobj_to_file(fsrc, dest):
-    """Copy contents of open file object to destination path."""
-    with open(dest, "wb+") as fdest:
-        shutil.copyfileobj(fsrc, fdest)
-
-
-def walk_files(directory):
-    for root, _, files in os.walk(directory):
-        for f in files:
-            yield os.path.join(root, f)
-
-
-@contextmanager
-def as_atomic(fs, to_info, create_parents=False):
-    from dvc.utils import tmp_fname
-
-    parent = fs.path.parent(to_info)
-    if create_parents:
-        fs.makedirs(parent, exist_ok=True)
-
-    tmp_info = fs.path.join(parent, tmp_fname())
-    try:
-        yield tmp_info
-    except BaseException:
-        # Handle stuff like KeyboardInterrupt
-        # as well as other errors that might
-        # arise during file transfer.
-        with suppress(FileNotFoundError):
-            fs.remove(tmp_info)
-        raise
-    else:
-        fs.move(tmp_info, to_info)
--- tests/func/utils/test_fs.py
+++ tests/func/utils/test_fs.py
@@ -3,7 +3,7 @@ import stat
 
 import pytest
 
-from dvc.utils.fs import copyfile, makedirs
+from dvc.utils.fs import makedirs
 
 
 @pytest.mark.skipif(os.name == "nt", reason="Not supported for Windows.")
@@ -18,19 +18,3 @@ def test_makedirs_permissions(tmp_dir):
 
     assert stat.S_IMODE(os.stat(test_dir).st_mode) == dir_mode
     assert stat.S_IMODE(os.stat(intermediate_dir).st_mode) == dir_mode
-
-
-def test_copyfile(tmp_dir):
-    src = "file1"
-    dest = "file2"
-    dest_dir = "testdir"
-
-    tmp_dir.gen(src, "file1contents")
-
-    os.mkdir(dest_dir)
-
-    copyfile(src, dest)
-    assert (tmp_dir / dest).read_text() == "file1contents"
-
-    copyfile(src, dest_dir)
-    assert (tmp_dir / dest_dir / src).read_text() == "file1contents"
