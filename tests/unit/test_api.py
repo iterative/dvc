ea12ced51d2a929353a58011a81c868e680df009
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2022-05-30 17:12:21 +0200

    api: open: Raise ValueError if rev is used in wrong mode.
    Closes #7405
    
    Co-authored-by: Peter Rowlands (변기호) <peter@pmrowla.com>

---
+++ tests/unit/test_api.py
@@ -0,0 +1,21 @@
+import pytest
+
+from dvc import api
+
+
+def test_open_raises_error_if_no_context(tmp_dir, dvc):
+    tmp_dir.dvc_gen("foo", "foo-text")
+
+    with pytest.raises(
+        AttributeError, match="should be used in a with statement."
+    ):
+        fd = api.open("foo")
+        fd.read()
+
+
+def test_open_rev_raises_error_on_wrong_mode(tmp_dir, dvc):
+    tmp_dir.dvc_gen("foo", "foo-text")
+
+    with pytest.raises(ValueError, match="Only reading `mode` is supported."):
+        with api.open("foo", mode="w"):
+            pass
