52f02d238d6dc361b39856ecfcc496093a172ade
Daniele Trifirò <daniele@iterative.ai>
2022-05-31 21:37:54 +0200

    bump scmrepo to 0.0.23
    `scmrepo==0.0.23` includes proper support for windows file:// URLs
    (https://github.com/iterative/scmrepo/pull/68), so Windows tests
    relying on file URLs had to be fixed in order to be valid
    [RFC 8089](https://datatracker.ietf.org/doc/html/rfc8089) URLs.

--- tests/func/test_get.py
+++ tests/func/test_get.py
@@ -266,7 +266,7 @@ def test_get_pipeline_tracked_outs(
     dvc.scm.commit("add pipeline stage")
 
     with git_dir.chdir():
-        Repo.get(f"file://{os.fspath(tmp_dir)}", "bar", out="baz")
+        Repo.get(f"file://{tmp_dir.as_posix()}", "bar", out="baz")
         assert (git_dir / "baz").read_text() == "foo"
 
 
--- tests/func/test_import.py
+++ tests/func/test_import.py
@@ -408,7 +408,7 @@ def test_import_pipeline_tracked_outs(
     dvc.scm.commit("add pipeline stage")
 
     with erepo_dir.chdir():
-        erepo_dir.dvc.imp(f"file://{os.fspath(tmp_dir)}", "bar", out="baz")
+        erepo_dir.dvc.imp(f"file://{tmp_dir.as_posix()}", "bar", out="baz")
         assert (erepo_dir / "baz").read_text() == "foo"
 
 
