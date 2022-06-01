d7f4050a23a41c047db44bdf9d4a0daef5c793cf
Peter Rowlands <peter@pmrowla.com>
2022-06-01 17:41:13 +0900

    exp: simplify shutdown behavior

--- tests/unit/repo/experiments/queue/test_local.py
+++ tests/unit/repo/experiments/queue/test_local.py
@@ -17,28 +17,9 @@ def _foo(arg=None):  # pylint: disable=unused-argument
     return "foo"
 
 
-def test_shutdown_active_tasks(test_queue, mocker):
-
-    shutdown_spy = mocker.spy(test_queue.celery.control, "shutdown")
-    # use frozen signature to get an assigned task ID and AsyncResult object
-    # before running the task
-    sig = _foo.s()
-    result = sig.freeze()
-    mocker.patch.object(
-        test_queue, "_iter_active_tasks", return_value=[(result.id, None)]
-    )
-
-    # shutdown signal handler should be registered but not executed since task
-    # has not completed
+def test_shutdown(test_queue, mocker):
+    shutdown_spy = mocker.patch("celery.app.control.Control.shutdown")
     test_queue.shutdown()
-    assert result.id in test_queue._shutdown_task_ids
-    assert not result.ready()
-    shutdown_spy.assert_not_called()
-
-    sig.delay()
-    # block until task completes
-    assert "foo" == result.get()
-    assert result.id not in test_queue._shutdown_task_ids
     shutdown_spy.assert_called_once()
 
 
