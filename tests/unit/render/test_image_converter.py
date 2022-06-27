35e9634b151c73adbc01b3c314369f2ee8049a2c
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2022-06-27 09:36:19 +0200

    render: image_converter: Support slash in revision.
    Fixes #7934

--- tests/unit/render/test_image_converter.py
+++ tests/unit/render/test_image_converter.py
@@ -28,3 +28,19 @@ def test_image_converter_with_out(tmp_dir):
     }
 
     assert (tmp_dir / "foo" / "r_image.png").read_bytes() == b"content"
+
+
+def test_image_converter_with_slash_in_revision(tmp_dir):
+    """Regression test for #7934"""
+    data = b"content"
+    converter = ImageConverter({"out": tmp_dir / "foo"})
+
+    datapoints, _ = converter.convert(data, "feature/r", "image.png")
+
+    assert datapoints[0] == {
+        REVISION_FIELD: "feature/r",
+        "filename": "image.png",
+        SRC_FIELD: str(tmp_dir / "foo" / "feature_r_image.png"),
+    }
+
+    assert (tmp_dir / "foo" / "feature_r_image.png").read_bytes() == b"content"
