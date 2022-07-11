df5b82698a07abf10912c4adebd72edd8cb0d9c4
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2022-07-11 11:03:06 +0200

    tests: limit printed durations to 50

--- tests/__main__.py
+++ tests/__main__.py
@@ -14,7 +14,7 @@ os.putenv(
 )
 os.putenv("DVC_HOME", REPO_ROOT)
 
-opts = "-v -n=auto --dist loadscope --cov=dvc --durations=0"
+opts = "-v -n=auto --dist loadscope --cov=dvc --durations=50"
 params = " ".join(sys.argv[1:])
 cmd = f"{sys.executable} -m pytest {opts} {params}"
 check_call(cmd, shell=True)
