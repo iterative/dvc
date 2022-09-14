e3361117c0f82fe374c9e660b8e36d4d8ba39042
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2022-09-14 17:00:04 +0545

    expose dvcfs in dvc.api and add to fsspec's registry

--- dvc/api/__init__.py
+++ dvc/api/__init__.py
@@ -1,9 +1,6 @@
-from .data import (  # noqa, pylint: disable=redefined-builtin
-    get_url,
-    open,
-    read,
-)
+from dvc.fs.dvc import _DvcFileSystem as DvcFileSystem
+
+from .data import open  # pylint: disable=redefined-builtin
+from .data import get_url, read
 from .experiments import make_checkpoint
 from .params import params_show
-
-__all__ = ["get_url", "make_checkpoint", "open", "params_show", "read"]
