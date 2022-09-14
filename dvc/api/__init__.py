5bddb7879dbe9ec885513f8402453e07a374f142
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2022-09-14 18:09:26 +0545

    readd __all__ to avoid exposing modules

--- dvc/api/__init__.py
+++ dvc/api/__init__.py
@@ -4,3 +4,12 @@ from .data import open  # pylint: disable=redefined-builtin
 from .data import get_url, read
 from .experiments import make_checkpoint
 from .params import params_show
+
+__all__ = [
+    "get_url",
+    "make_checkpoint",
+    "open",
+    "params_show",
+    "read",
+    "DvcFileSystem",
+]
