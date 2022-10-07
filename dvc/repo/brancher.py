add28f73f144a679b43ea17feaac69df64bca7b5
Peter Rowlands <peter@pmrowla.com>
2022-10-07 18:38:59 +0900

    brancher: clear repo.index on each branch

--- dvc/repo/brancher.py
+++ dvc/repo/brancher.py
@@ -77,6 +77,7 @@ def brancher(  # noqa: E302
         from dvc.fs import GitFileSystem
 
         for sha, names in found_revs.items():
+            self.__dict__.pop("index", None)
             self.fs = GitFileSystem(scm=scm, rev=sha)
             self.root_dir = self.fs.path.join("/", *repo_root_parts)
 
