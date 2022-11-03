4db2e261043b3fb2d19ffdac0e69721c526c9f74
codamuse <codamuse@mailfence.com>
2022-11-02 22:56:10 -0400

    feat: remove arguments and add docstring

--- dvc/api/metrics.py
+++ dvc/api/metrics.py
@@ -9,14 +9,105 @@ from dvc.repo import Repo
 
 def metrics_show(
     *targets: str,
-    all_branches: Optional[bool] = False,
-    all_tags: Optional[bool] = False,
-    all_commits: Optional[bool] = False,
-    recursive: Optional[bool] = False,
     repo: Optional[str] = None,
     rev: Optional[str] = None,
 ) -> Dict:
-    """Get metrics tracked in `repo`"""
+    """Get metrics tracked in `repo`.
+
+    Without arguments, this function will retrieve all metrics from all tracked
+    metric files, for the current working tree.
+
+    See the options below to restrict the metrics retrieved.
+
+    Args:
+        *targets (str, optional): Names of the metric files to retrieve
+        params from. For example, "classifier_eval.json, clustering_eval.json".
+        If no `targets` are provided, all metric files tracked in `dvc.yaml`
+        will be used.
+        Note that targets don't necessarily have to be defined in `dvc.yaml`.
+        repo (str, optional): Location of the DVC repository.
+            Defaults to the current project (found by walking up from the
+            current working directory tree).
+            It can be a URL or a file system path.
+            Both HTTP and SSH protocols are supported for online Git repos
+            (e.g. [user@]server:project.git).
+        rev (str, optional): Name of the `Git revision`_ to retrieve metrics
+            from.
+            Defaults to `None`.
+            An example of git revision can be a branch or tag name, a commit
+            hash or a dvc experiment name.
+            If `repo` is not a Git repo, this option is ignored.
+            If `None`, the current working tree will be used.
+
+    Returns:
+        Dict: See Examples below.
+
+    Raises:
+        DvcException: If no params are found in `repo`.
+
+    Examples:
+
+        - No arguments.
+
+        Working on https://github.com/iterative/example-get-started
+
+        >>> import dvc.api
+        >>> metrics = dvc.api.metrics_show()
+        >>> print(json.dumps(metrics, indent=4))
+        {
+            "avg_prec": 0.9249974999612706,
+            "roc_auc": 0.9460213440787918
+        }
+
+        ---
+
+        - Using `rev`.
+
+        Working on https://github.com/iterative/example-get-started
+
+        >>> import json
+        >>> import dvc.api
+        >>> metrics = dvc.api.metrics_show(rev="tune-hyperparams")
+        >>> print(json.dumps(metrics, indent=4))
+        {
+            "avg_prec": 0.9268792615819422,
+            "roc_auc": 0.945093365854111
+        }
+
+        ---
+
+        - Using `targets`.
+
+        Working on https://github.com/iterative/example-get-started
+
+        >>> import json
+        >>> import dvc.api
+        >>> metrics = dvc.api.metrics_show("evaluation.json")
+        >>> print(json.dumps(metrics, indent=4))
+        {
+            "avg_prec": 0.9249974999612706,
+            "roc_auc": 0.9460213440787918
+        }
+
+        ---
+
+        - Git URL as `repo`.
+
+        >>> import json
+        >>> import dvc.api
+        >>> metrics = dvc.api.metrics_show(
+        ...     repo="https://github.com/iterative/demo-fashion-mnist")
+        >>> print(json.dumps(metrics, indent=4))
+        {
+            "loss": 0.25284987688064575,
+            "accuracy": 0.9071000218391418
+        }
+
+
+    .. _Git revision:
+        https://git-scm.com/docs/revisions
+
+    """
 
     def _onerror_raise(result: Dict, exception: Exception, *args, **kwargs):
         raise exception
@@ -49,11 +140,7 @@ def metrics_show(
     with Repo.open(repo) as _repo:
         metrics = _repo.metrics.show(
             targets=targets,
-            all_branches=all_branches,
-            all_tags=all_tags,
-            recursive=recursive,
             revs=rev if rev is None else [rev],
-            all_commits=all_commits,
             onerror=_onerror_raise,
         )
 
