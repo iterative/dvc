3406e91aa3ea64c39eb0d75b47e3e7514b468ff1
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2022-11-03 20:53:08 +0545

    migrate test_{add,analytics,init,unprotect,local,process}.py to pytest

--- tests/func/test_unprotect.py
+++ tests/func/test_unprotect.py
@@ -1,37 +1,27 @@
 import os
 
-from dvc.cli import main
-from tests.basic_env import TestDvc
 
+def test_unprotect(tmp_dir, dvc):
+    tmp_dir.gen("foo", "foo")
 
-class TestUnprotect(TestDvc):
-    def test(self):
-        ret = main(["config", "cache.type", "hardlink"])
-        self.assertEqual(ret, 0)
+    dvc.odb.local.cache_types = ["hardlink"]
+    dvc.add("foo")
+    cache = os.path.join(
+        ".dvc", "cache", "ac", "bd18db4cc2f85cedef654fccc4a4d8"
+    )
+    assert not os.access("foo", os.W_OK)
+    assert not os.access(cache, os.W_OK)
 
-        ret = main(["add", self.FOO])
-        self.assertEqual(ret, 0)
+    dvc.unprotect("foo")
+    assert os.access("foo", os.W_OK)
 
-        cache = os.path.join(
-            ".dvc", "cache", "ac", "bd18db4cc2f85cedef654fccc4a4d8"
-        )
+    if os.name == "nt":
+        # NOTE: cache is now unprotected, because NTFS doesn't allow
+        # deleting read-only files, so we have to try to set write perms
+        # on files that we try to delete, which propagates to the cache
+        # file. But it should be restored after the next cache check, hence
+        # why we call `dvc status` here.
+        assert os.access(cache, os.W_OK)
+        dvc.status()
 
-        self.assertFalse(os.access(self.FOO, os.W_OK))
-        self.assertFalse(os.access(cache, os.W_OK))
-
-        ret = main(["unprotect", self.FOO])
-        self.assertEqual(ret, 0)
-
-        self.assertTrue(os.access(self.FOO, os.W_OK))
-
-        if os.name == "nt":
-            # NOTE: cache is now unprotected, because NTFS doesn't allow
-            # deleting read-only files, so we have to try to set write perms
-            # on files that we try to delete, which propagates to the cache
-            # file. But it should be restored after the next cache check, hence
-            # why we call `dvc status` here.
-            self.assertTrue(os.access(cache, os.W_OK))
-            ret = main(["status"])
-            self.assertEqual(ret, 0)
-
-        self.assertFalse(os.access(cache, os.W_OK))
+    assert not os.access(cache, os.W_OK)
