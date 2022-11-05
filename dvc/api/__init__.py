26587ba79220364ef347af52aa27a6a58583089e
codamuse <codamuse@mailfence.com>
2022-11-05 09:41:59 -0400

    refactor: combine metrics_show and params_show and remove args from metrics_show

--- dvc/api/__init__.py
+++ dvc/api/__init__.py
@@ -3,8 +3,10 @@ from dvc.fs.dvc import _DVCFileSystem as DVCFileSystem
 from .data import open  # pylint: disable=redefined-builtin
 from .data import get_url, read
 from .experiments import make_checkpoint
-from .metrics import metrics_show
-from .params import params_show
+
+# from .metrics import metrics_show
+# from .params import params_show
+from .show import metrics_show, params_show
 
 __all__ = [
     "get_url",
