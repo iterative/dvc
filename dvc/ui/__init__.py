80d6344be58eb4a8f693b34df2da0ce0caf03bbb
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2022-11-22 10:29:53 +0100

    Update dvc/ui/__init__.py
    Co-authored-by: Peter Rowlands (변기호) <peter@pmrowla.com>

--- dvc/ui/__init__.py
+++ dvc/ui/__init__.py
@@ -270,7 +270,7 @@ class Console:
 
         path = Path(file).resolve()
         url = (
-            relpath(path) if "microsoft" in uname().release else path.as_uri()
+            relpath(path) if "microsoft" in uname().release.lower() else path.as_uri()
         )
 
         opened = webbrowser.open(url)
