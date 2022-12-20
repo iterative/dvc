542d89dd5499c0bec0af78b4d4669b05df33639e
dberenbaum <dave@iterative.ai>
2022-12-20 10:49:49 -0500

    metrics: fix diff bt active branch and workspace

--- dvc/repo/metrics/diff.py
+++ dvc/repo/metrics/diff.py
@@ -12,6 +12,13 @@ def diff(repo, *args, a_rev=None, b_rev=None, **kwargs):
     b_rev = b_rev or "workspace"
 
     metrics = repo.metrics.show(*args, **kwargs, revs=[a_rev, b_rev])
+
+    # workspace may have been replaced by active branch
+    workspace_rev = (a_rev == "workspace") or (b_rev == "workspace")
+    if workspace_rev and "workspace" not in metrics:
+        active_branch = repo.scm.active_branch()
+        metrics["workspace"] = metrics[active_branch]
+
     old = metrics.get(a_rev, {}).get("data", {})
     new = metrics.get(b_rev, {}).get("data", {})
 
