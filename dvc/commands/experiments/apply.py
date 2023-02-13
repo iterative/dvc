b50115d8f4de5188c9821faf6559d177a55410de
Peter Rowlands <peter@pmrowla.com>
2023-02-13 15:16:52 +0900

    apply: add --no-force deprecation message

--- dvc/commands/experiments/apply.py
+++ dvc/commands/experiments/apply.py
@@ -4,12 +4,20 @@ import logging
 from dvc.cli import completion
 from dvc.cli.command import CmdBase
 from dvc.cli.utils import append_doc_link
+from dvc.ui import ui
 
 logger = logging.getLogger(__name__)
 
 
 class CmdExperimentsApply(CmdBase):
     def run(self):
+        if not self.args.force:
+            ui.write(
+                "The --no-force option is deprecated and will be removed in a future"
+                " DVC release. To revert the result of 'exp apply', run:\n"
+                "\n\tgit reset --hard\n"
+                "\tgit stash apply refs/exps/apply/stash\n"
+            )
         self.repo.experiments.apply(self.args.experiment)
 
         return 0
@@ -23,6 +31,12 @@ def add_parser(experiments_subparsers, parent_parser):
         description=append_doc_link(EXPERIMENTS_APPLY_HELP, "exp/apply"),
         help=EXPERIMENTS_APPLY_HELP,
         formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    experiments_apply_parser.add_argument(
+        "--no-force",
+        action="store_false",
+        dest="force",
+        help="Fail if this command would overwrite conflicting changes.",
     )
     experiments_apply_parser.add_argument(
         "experiment", help="Experiment to be applied."
