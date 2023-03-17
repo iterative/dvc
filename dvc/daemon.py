c516a4c1e995647614fbde40ee00431d7e85e8c2
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2023-03-17 14:42:48 +0545

    close all streams in child

--- dvc/daemon.py
+++ dvc/daemon.py
@@ -82,6 +82,7 @@ def _spawn_posix(cmd, env):
     sys.stdin.close()
     sys.stdout.close()
     sys.stderr.close()
+    os.closerange(0, 3)
 
     if platform.system() == "Darwin":
         # workaround for MacOS bug
