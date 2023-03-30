1545f366cce3dd7ef7c334fe35c9cfef4794533e
Alexander Guschin <1aguschin@gmail.com>
2023-03-30 13:10:06 +0600

    issue warning at incorrect artifact name

--- dvc/repo/artifacts.py
+++ dvc/repo/artifacts.py
@@ -1,4 +1,6 @@
+import logging
 import os
+import re
 from typing import TYPE_CHECKING, Any, Dict
 
 from dvc.annotations import Artifact
@@ -7,6 +9,22 @@ from dvc.utils import relpath
 
 if TYPE_CHECKING:
     from dvc.repo import Repo
+
+logger = logging.getLogger(__name__)
+
+
+NAME_RE = re.compile("^[a-z][a-z0-9-/.]*[a-z0-9]$")
+
+
+def check_name_format(name: str) -> None:
+    # "/" forbidden, only in dvc exp as we didn't support it for now.
+    if not bool(NAME_RE.search(name)):
+        logger.warning(
+            "To be compatible with Git tags and GTO, artifact name ('%s') "
+            "must satisfy the following regex: %s",
+            name,
+            NAME_RE.pattern,
+        )
 
 
 class ArtifactsFile(FileMixin):
@@ -24,7 +42,7 @@ class Artifacts:
         self.repo = repo
 
     def read(self) -> Dict[str, Dict[str, Any]]:
-        artifacts = {}
+        artifacts: Dict[str, Dict[str, Any]] = {}
         for (
             dvcfile,
             dvcfile_artifacts,
@@ -36,9 +54,11 @@ class Artifacts:
                     os.path.join(os.path.dirname(dvcfile), dvcfile_artifacts),
                     verify=False,
                 ).load()
-            if dvcfile_artifacts:
-                artifacts[relpath(dvcfile, self.repo.root_dir)] = {
-                    name: Artifact(**{"path": name, **(value or {})})
-                    for name, value in dvcfile_artifacts.items()
-                }
+            if not dvcfile_artifacts:
+                continue
+            dvcyaml = relpath(dvcfile, self.repo.root_dir)
+            artifacts[dvcyaml] = {}
+            for name, value in dvcfile_artifacts.items():
+                check_name_format(name)
+                artifacts[dvcyaml][name] = Artifact(**{"path": name, **(value or {})})
         return artifacts
