5563a595d528ced23b0f2f8990b20d9a8d99afc2
skshetry <18718008+skshetry@users.noreply.github.com>
2023-04-18 22:22:26 +0545

    utils.studio: allow any base url to be set, not just the hostname (#9341)
    This keeps it in symmetry with
    https://github.com/iterative/dvc-studio-client/pull/28, and allows
    setting the base url with path, not just limited to netloc.
    
    This was always intended to work, urljoin with the added slash was
    causing this bug.

--- dvc/utils/studio.py
+++ dvc/utils/studio.py
@@ -57,7 +57,7 @@ def notify_refs(
     data = {"repo_url": repo_url, "client": "dvc", "refs": refs}
 
     try:
-        r = post("/webhook/dvc", token, data, base_url=base_url)
+        r = post("webhook/dvc", token, data, base_url=base_url)
     except requests.RequestException as e:
         logger.trace("", exc_info=True)  # type: ignore[attr-defined]
 
