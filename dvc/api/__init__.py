2a04263f021db190641ee2ca077b70563c74cdea
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2023-04-27 10:24:07 +0200

    api: Add `scm` module with `all_{X}` methods for listing revs.

--- dvc/api/__init__.py
+++ dvc/api/__init__.py
@@ -3,9 +3,13 @@ from dvc.fs.dvc import _DVCFileSystem as DVCFileSystem
 from .data import open  # pylint: disable=redefined-builtin
 from .data import get_url, read
 from .experiments import exp_save, exp_show, make_checkpoint
+from .scm import all_branches, all_commits, all_tags
 from .show import metrics_show, params_show
 
 __all__ = [
+    "all_branches",
+    "all_commits",
+    "all_tags",
     "exp_save",
     "exp_show",
     "get_url",
