6c79f6bbfc89f5f4b366d8a2a05883a0ed63383e
Peter Rowlands <peter@pmrowla.com>
2023-05-01 16:28:47 +0900

    testing: register pytest plugin

--- dvc/testing/benchmarks/cli/commands/test_plots.py
+++ dvc/testing/benchmarks/cli/commands/test_plots.py
@@ -1,7 +1,9 @@
 import pytest
 
 from dvc.repo import Repo
-from tests.benchmarks.conftest import pull
+from dvc.testing.benchmarks.fixtures import _pull
+
+# pylint: disable=redefined-outer-name,unused-argument
 
 # pylint: disable=unused-argument
 
@@ -9,7 +11,7 @@ from tests.benchmarks.conftest import pull
 @pytest.mark.requires(minversion=(2, 34, 0), reason="top-level plots not supported")
 def test_plots(project, bench_dvc):
     with Repo() as dvc:
-        pull(dvc)
+        _pull(dvc)
 
     kwargs = {"rounds": 5, "iterations": 3, "warmup_rounds": 2}
     bench_dvc("plots", "show", name="show", **kwargs)
--- dvc/testing/conftest.py
+++ dvc/testing/conftest.py
@@ -1,7 +1,1 @@
 from .fixtures import *  # noqa, pylint: disable=wildcard-import
-
-
-def pytest_addoption(parser):
-    from .benchmarks.conftest import pytest_addoption as bench_addoption
-
-    bench_addoption(parser)
