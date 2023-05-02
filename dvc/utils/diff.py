1d5596c40e254dbf06016f388fd5ea7132a784fd
paulourbano <paulourbano@users.noreply.github.com>
2023-05-02 16:35:51 +0000

    utils: removing `diff` key from result dictionary

--- dvc/utils/diff.py
+++ dvc/utils/diff.py
@@ -26,8 +26,7 @@ def _diff_vals(old, new, with_unchanged):
     res = {"old": old, "new": new}
     if isinstance(new, (int, float)) and isinstance(old, (int, float)):
         res["diff"] = new - old
-    elif isinstance(new, str) and isinstance(old, str):
-        res["diff"] = ""
+    
     return res
 
 
