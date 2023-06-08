ed664143b48f5372773075ad5093b93ffdfd755b
skshetry <18718008+skshetry@users.noreply.github.com>
2023-06-08 08:19:59 +0545

    dvc.build: move logic out of __init__ to dvc.build (#9563)

--- dvc/__init__.py
+++ dvc/__init__.py
@@ -3,11 +3,8 @@ DVC
 ----
 Make your data science projects reproducible and shareable.
 """
-from typing import Optional
-
 import dvc.logger
-from dvc import _build
+from dvc.build import PKG  # noqa: F401
 from dvc.version import __version__, version_tuple  # noqa: F401
 
-PKG: "Optional[str]" = _build.PKG
 dvc.logger.setup()
