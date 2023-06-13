24800fda3934ec47f43e751127415cd8a013e682
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2023-06-13 20:24:08 +0545

    remove dvc._build

--- dvc/build.py
+++ dvc/build.py
@@ -1,5 +1,10 @@
 from typing import Optional
 
-from . import _build
+try:
+    # file is created during dvc build
+    # pylint:disable=unused-import
+    from . import _build  # type: ignore[attr-defined, import]
 
-PKG: "Optional[str]" = _build.PKG
+    PKG: Optional[str] = _build.PKG  # type: ignore[assignment]
+except ImportError:
+    PKG = None  # type: ignore[assignment]
