iterative/dvc

commit d736d0a3efcf4f9ff58dc787a620bd330da9b9a9

output: checkout: use f-string (#9715)
Regression from https://github.com/iterative/dvc/pull/9673.

dvc/output.py
@@ -1402,7 +1402,7 @@ class Output:
         otransfer(staging, self.cache, {obj.hash_info}, hardlink=relink, shallow=False)
 
         if relink:
-            with CheckoutCallback(desc="Checking out {path}", unit="files") as callback:
+            with CheckoutCallback(desc=f"Checking out {path}", unit="files") as callback:
                 self._checkout(
                     path,
                     self.fs,

commit 91b19ce6ea9f3bac7dc2a6ead010267173999c0b

output: fix formatting
Regression from https://github.com/iterative/dvc/pull/9715

dvc/output.py
@@ -1402,7 +1402,9 @@ class Output:
         otransfer(staging, self.cache, {obj.hash_info}, hardlink=relink, shallow=False)
 
         if relink:
-            with CheckoutCallback(desc=f"Checking out {path}", unit="files") as callback:
+            with CheckoutCallback(
+                desc=f"Checking out {path}", unit="files"
+            ) as callback:
                 self._checkout(
                     path,
                     self.fs,