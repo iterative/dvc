43477b372bc153ac5ef298c19fee204952c217cd
Dom Miketa <dmiketa@exscientia.co.uk>
2023-08-05 17:49:32 +0100

    address comments

--- tests/func/utils/test_hydra.py
+++ tests/func/utils/test_hydra.py
@@ -1,5 +1,3 @@
-from contextlib import nullcontext as does_not_raise
-
 import pytest
 
 from dvc.exceptions import InvalidArgumentError
@@ -191,34 +189,28 @@ def hydra_setup_dir_basic(tmp_dir, config_subdir, config_name, config_content):
 
 
 @pytest.mark.parametrize(
-    "config_subdir,config_module,config_name,error_context,config_content",
+    "config_subdir,config_module,config_content",
     [
-        ("conf", None, "config", does_not_raise(), {"normal_yaml_config": True}),
+        ("conf", None, {"normal_yaml_config": False}),
         (
             None,
             "hydra.test_utils.configs",
-            "config",
-            does_not_raise(),
             {"normal_yaml_config": True},
         ),
         (
             "conf",
             "hydra.test_utils.configs",
-            "config",
-            pytest.raises(ValueError, match="^Exactly one of"),
-            None,
+            {"normal_yaml_config": False},
         ),
         (
             None,
             None,
-            "config",
-            pytest.raises(ValueError, match="^Exactly one of"),
             None,
         ),
     ],
 )
 def test_compose_and_dump_dir_module(
-    tmp_dir, config_subdir, config_module, config_name, error_context, config_content
+    tmp_dir, config_subdir, config_module, config_name, config_content
 ):
     from dvc.utils.hydra import compose_and_dump
 
@@ -226,10 +218,10 @@ def test_compose_and_dump_dir_module(
     config_dir = hydra_setup_dir_basic(
         tmp_dir, config_subdir, config_name, config_content
     )
+    config_name = "config"
 
-    with error_context:
-        compose_and_dump(output_file, config_dir, config_module, config_name, [])
-        assert output_file.parse() == config_content
+    compose_and_dump(output_file, config_dir, config_module, config_name, [])
+    assert output_file.parse() == config_content
 
 
 def test_compose_and_dump_yaml_handles_string(tmp_dir):
