b0c93e5347c1facd6078c9adda1201eab4c3dfbb
Ruslan Kuprieiev <kupruser@gmail.com>
2023-08-08 00:41:21 +0300

    repo: cache: support some view options

--- dvc/repo/cache.py
+++ dvc/repo/cache.py
@@ -1,11 +1,17 @@
 import os
 
 
-def check_missing(repo, rev=None):
+def check_missing(repo, rev=None, max_size=None, types=None):
     from dvc_data.index import StorageKeyError
 
     with repo.switch(rev or "workspace"):
-        index = repo.index.data["repo"]
+        idx = repo.index.targets_view(
+            None,
+            max_size=max_size,
+            types=types,
+        )
+
+        index = idx.data["repo"]
 
     def onerror(_entry, _exc):
         pass
