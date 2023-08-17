d7feea29ada4c3eaab22f5a7ad53148f65f9e4d8
Ruslan Kuprieiev <kupruser@gmail.com>
2023-08-17 22:31:24 +0300

    import: set core.remote when both --remote and --remote-config are specified
    From https://github.com/iterative/dvc/pull/9853#issuecomment-1682797083

--- dvc/repo/imp.py
+++ dvc/repo/imp.py
@@ -13,16 +13,28 @@ def imp(
     if rev is not None:
         erepo["rev"] = rev
 
+    if remote and remote_config and isinstance(config, str):
+        raise ValueError(
+            "Can't specify config path together with both remote and remote_config"
+        )
+
     if config is not None:
         erepo["config"] = config
 
     if remote is not None and remote_config is not None:
         conf = erepo.get("config") or {}
+
+        core = conf.get("core") or {}
+        core["remote"] = remote
+
         remotes = conf.get("remote") or {}
         remote_conf = remotes.get(remote) or {}
         remote_conf.update(remote_config)
         remotes[remote] = remote_conf
+
+        conf["core"] = core
         conf["remote"] = remotes
+
         erepo["config"] = conf
     elif remote is not None:
         erepo["remote"] = remote
