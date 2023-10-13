72fd265a8f3a73d28a0dc96685aee77bc4bd56fe
Ruslan Kuprieiev <kupruser@gmail.com>
2023-10-13 17:41:08 +0300

    get-url/ls-url: support remote:// with in-repo config

--- tests/func/test_ls_url.py
+++ tests/func/test_ls_url.py
@@ -1,5 +1,23 @@
+from dvc.config import Config
+from dvc.repo import Repo
 from dvc.testing.workspace_tests import TestLsUrl as _TestLsUrl
 
 
 class TestLsUrl(_TestLsUrl):
     pass
+
+
+def test_ls_url_config(dvc, make_remote):
+    remote_path = make_remote("myremote", default=False, typ="local")
+    (remote_path / "foo").write_text("foo")
+    (remote_path / "bar").write_text("bar")
+
+    actual = sorted(
+        Repo.ls_url("remote://myremote", config=Config.from_cwd()),
+        key=lambda entry: entry["path"],
+    )
+    expected = [
+        {"isdir": False, "path": "bar", "size": 3},
+        {"isdir": False, "path": "foo", "size": 3},
+    ]
+    assert actual == expected
--- tests/unit/command/test_ls_url.py
+++ tests/unit/command/test_ls_url.py
@@ -1,8 +1,9 @@
 from dvc.cli import parse_args
 from dvc.commands.ls_url import CmdListUrl
+from dvc.config import Config
 
 
-def test_ls_url(mocker):
+def test_ls_url(mocker, M):
     cli_args = parse_args(["ls-url", "src"])
     assert cli_args.func == CmdListUrl
     cmd = cli_args.func(cli_args)
@@ -10,10 +11,12 @@ def test_ls_url(mocker):
 
     assert cmd.run() == 0
 
-    m.assert_called_once_with("src", recursive=False, fs_config=None)
+    m.assert_called_once_with(
+        "src", recursive=False, fs_config=None, config=M.instance_of(Config)
+    )
 
 
-def test_recursive(mocker):
+def test_recursive(mocker, M):
     cli_args = parse_args(["ls-url", "-R", "src"])
     assert cli_args.func == CmdListUrl
     cmd = cli_args.func(cli_args)
@@ -21,4 +24,6 @@ def test_recursive(mocker):
 
     assert cmd.run() == 0
 
-    m.assert_called_once_with("src", recursive=True, fs_config=None)
+    m.assert_called_once_with(
+        "src", recursive=True, fs_config=None, config=M.instance_of(Config)
+    )
