0db01817035b1dfd9b3a136fffc05bf4b9b9c218
Amrit Ghimire <mail@amritghimire.com>
2023-11-07 20:01:00 +0545

    Update unit tests for auth commands
    Unit tests for DVC Studio authentication commands have been updated. These checks verify successful execution and appropriate responses under various authentication scenarios, including login with invalid scope, standard login, logout, and token commands in order to enhance the robustness and reliability of the auth module.

--- tests/unit/command/test_auth.py
+++ tests/unit/command/test_auth.py
@@ -1,7 +1,15 @@
 from unittest import mock
 
 from dvc.cli import main
+from dvc.commands.auth import DEFAULT_SCOPES
 from dvc.utils.studio import STUDIO_URL
+
+MOCK_RESPONSE = {
+    "verification_uri": STUDIO_URL + "/auth/device-login",
+    "user_code": "MOCKCODE",
+    "device_code": "random-value",
+    "token_uri": STUDIO_URL + "/api/device-login/token",
+}
 
 
 def test_auth_login_invalid_scope():
@@ -10,23 +18,80 @@ def test_auth_login_invalid_scope():
 
 @mock.patch("dvc.utils.studio.check_token_authorization")
 @mock.patch("dvc.utils.studio.start_device_login")
-def test_auth_login(mock_start_device_login, mock_check_token_authorization, dvc):
-    mock_response = {
-        "verification_uri": STUDIO_URL + "/auth/device-login",
-        "user_code": "MOCKCODE",
-        "device_code": "random-value",
-        "token_uri": STUDIO_URL + "/api/device-login/token",
-    }
-    mock_start_device_login.return_value = mock_response
+def test_auth_login_token_check_failed(
+    mock_start_device_login, mock_check_token_authorization, dvc
+):
+    mock_start_device_login.return_value = MOCK_RESPONSE
     mock_check_token_authorization.return_value = None
 
     assert main(["auth", "login"]) == 1
 
+
+@mock.patch("dvc.utils.studio.check_token_authorization")
+@mock.patch("dvc.utils.studio.start_device_login")
+def test_auth_login_success(
+    mock_start_device_login, mock_check_token_authorization, dvc, M
+):
+    mock_start_device_login.return_value = MOCK_RESPONSE
     mock_check_token_authorization.return_value = "isat_access_token"
+
     assert main(["auth", "login"]) == 0
+    assert mock_start_device_login.call_args.kwargs == {
+        "base_url": STUDIO_URL,
+        "data": {"client_name": "dvc", "scopes": DEFAULT_SCOPES, "token_name": M.any},
+    }
+    mock_check_token_authorization.assert_called_with(
+        uri=MOCK_RESPONSE["token_uri"], device_code=MOCK_RESPONSE["device_code"]
+    )
+
     config = dvc.config.load_one("global")
     assert config["studio"]["token"] == "isat_access_token"
     assert config["studio"]["url"] == STUDIO_URL
+
+
+@mock.patch("dvc.utils.studio.check_token_authorization")
+@mock.patch("dvc.utils.studio.start_device_login")
+def test_auth_login_arguments(
+    mock_start_device_login, mock_check_token_authorization, dvc, M
+):
+    mock_start_device_login.return_value = MOCK_RESPONSE
+    mock_check_token_authorization.return_value = "isat_access_token"
+
+    assert (
+        main(
+            [
+                "auth",
+                "login",
+                "--name",
+                "token_name",
+                "--hostname",
+                "https://example.com",
+                "--scopes",
+                "live",
+            ]
+        )
+        == 0
+    )
+
+    mock_start_device_login.assert_called_with(
+        data={"client_name": "dvc", "token_name": "token_name", "scopes": "live"},
+        base_url="https://example.com",
+    )
+    mock_check_token_authorization.assert_called()
+
+
+@mock.patch("dvc.utils.studio.check_token_authorization")
+@mock.patch("dvc.utils.studio.start_device_login")
+@mock.patch("webbrowser.open")
+def test_auth_device_code(
+    mock_webbrowser_open, mock_start_device_login, mock_check_token_authorization
+):
+    mock_start_device_login.return_value = MOCK_RESPONSE
+    mock_check_token_authorization.return_value = "isat_access_token"
+
+    assert main(["auth", "login", "--use-device-code"]) == 0
+
+    mock_webbrowser_open.assert_not_called()
 
 
 def test_auth_logout(dvc):
