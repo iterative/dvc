8776227b80a29890b59d877e8fdb886925c5d511
Amrit Ghimire <mail@amritghimire.com>
2023-11-29 14:59:02 +0545

    Refactor tests in `test_studio.py` for better readability

--- tests/unit/command/test_studio.py
+++ tests/unit/command/test_studio.py
@@ -1,25 +1,23 @@
-from unittest import mock
-
 from dvc_studio_client.auth import AuthorizationExpired
 
 from dvc.cli import main
 from dvc.utils.studio import STUDIO_URL
 
 
-def test_studio_login_invalid_scope():
-    assert main(["studio", "login", "--scopes", "invalid!"]) == 1
-
-
-@mock.patch("dvc_studio_client.auth.get_access_token")
-def test_studio_login_token_check_failed(mock_get_access_token):
-    mock_get_access_token.side_effect = AuthorizationExpired
+def test_studio_login_token_check_failed(mocker):
+    mocker.patch(
+        "dvc_studio_client.auth.get_access_token", side_effect=AuthorizationExpired
+    )
 
     assert main(["studio", "login"]) == 1
 
 
-@mock.patch("dvc_studio_client.auth.get_access_token")
-def test_studio_login_success(mock_get_access_token, dvc):
-    mock_get_access_token.return_value = ("token_name", "isat_access_token")
+def test_studio_login_success(mocker, dvc):
+    mocker.patch(
+        "dvc_studio_client.auth.get_access_token",
+        return_value=("token_name", "isat_access_token"),
+    )
+
     assert main(["studio", "login"]) == 0
 
     config = dvc.config.load_one("global")
@@ -27,9 +25,11 @@ def test_studio_login_success(mock_get_access_token, dvc):
     assert config["studio"]["url"] == STUDIO_URL
 
 
-@mock.patch("dvc_studio_client.auth.get_access_token")
-def test_studio_login_arguments(mock_get_access_token):
-    mock_get_access_token.return_value = ("token_name", "isat+access_token")
+def test_studio_login_arguments(mocker):
+    mock = mocker.patch(
+        "dvc_studio_client.auth.get_access_token",
+        return_value=("token_name", "isat_access_token"),
+    )
 
     assert (
         main(
@@ -42,28 +42,19 @@ def test_studio_login_arguments(mock_get_access_token):
                 "https://example.com",
                 "--scopes",
                 "experiments",
+                "--use-device-code",
             ]
         )
         == 0
     )
 
-    mock_get_access_token.assert_called_with(
+    mock.assert_called_with(
         token_name="token_name",
         hostname="https://example.com",
         scopes="experiments",
-        use_device_code=False,
+        use_device_code=True,
         client_name="dvc",
     )
-
-
-@mock.patch("dvc_studio_client.auth.get_access_token")
-@mock.patch("webbrowser.open")
-def test_studio_device_code(mock_webbrowser_open, mock_get_access_token):
-    mock_get_access_token.return_value = ("token_name", "isat+access_token")
-
-    assert main(["studio", "login", "--use-device-code"]) == 0
-
-    mock_webbrowser_open.assert_not_called()
 
 
 def test_studio_logout(dvc):
@@ -77,13 +68,12 @@ def test_studio_logout(dvc):
     assert main(["studio", "logout"]) == 1
 
 
-@mock.patch("dvc.ui.ui.write")
-def test_studio_token(mock_write, dvc):
+def test_studio_token(dvc, capsys):
     with dvc.config.edit("global") as conf:
         conf["studio"]["token"] = "isat_access_token"
 
     assert main(["studio", "token"]) == 0
-    mock_write.assert_called_with("isat_access_token")
+    assert capsys.readouterr().out == "isat_access_token\n"
 
     with dvc.config.edit("global") as conf:
         del conf["studio"]["token"]
