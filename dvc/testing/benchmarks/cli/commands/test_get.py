1c77d4e1106cbb782cf649b838ac4ecf2bfc5788
Ruslan Kuprieiev <kupruser@gmail.com>
2023-12-08 17:04:04 +0200

    bench: mark test_get/import as flaky
    Sometimes seeing those error out in 2.x and early 3.x with:
    
    ```
    ERROR: unexpected error - [Errno 95] no more link types left to try out: [Errno 95] Operation not supported
    ```
    
    It seems like we've fixed it at some point, but I was not able to quickly find a link.

--- dvc/testing/benchmarks/cli/commands/test_get.py
+++ dvc/testing/benchmarks/cli/commands/test_get.py
@@ -1,3 +1,7 @@
+import pytest
+
+
+@pytest.mark.flaky(max_runs=3, min_passes=1)
 def test_get(bench_dvc, tmp_dir, scm, dvc, make_dataset, remote):
     dataset = make_dataset(
         cache=False, files=False, dvcfile=True, commit=True, remote=True
