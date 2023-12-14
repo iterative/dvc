d9abbe744ef07b16ced1728705d0d57cea9eec98
dberenbaum <dave@iterative.ai>
2023-12-14 15:23:46 -0500

    bench: refactor test_repro

--- dvc/testing/benchmarks/cli/commands/test_repro.py
+++ dvc/testing/benchmarks/cli/commands/test_repro.py
@@ -1,25 +1,8 @@
-def test_repro(bench_dvc, tmp_dir, dvc, dataset):
-    dvc.add(dataset)
+def test_repro(bench_dvc, tmp_dir, dvc, make_dataset, run_copy):
+    dataset = make_dataset(dvcfile=True, files=True, cache=True)
     source_path = dataset.relative_to(tmp_dir).as_posix()
-    dvc.stage.add(
-        name="copy-1",
-        deps=[source_path],
-        outs=["copy-1"],
-        cmd=f"cp -R {source_path} copy-1",
-    )
-    dvc.stage.add(
-        name="copy-2",
-        deps=[source_path],
-        outs=["copy-2"],
-        cmd=f"cp -R {source_path} copy-2",
-    )
-    combine_cmds = [
-        "mkdir combine",
-        "cp -R copy-1 combine/1",
-        "cp -R copy-2 combine/2",
-    ]
-    dvc.stage.add(
-        name="combine", deps=["copy-1", "copy-2"], outs=["combine"], cmd=combine_cmds
-    )
+    run_copy(source_path, "copy-1", name="copy-1")
+    run_copy(source_path, "copy-2", name="copy-2")
+    run_copy("copy-1", "copy-3", name="copy-3")
     bench_dvc("repro")
     bench_dvc("repro", name="noop")
