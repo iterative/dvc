4cac5922dc788d498037b0c54e8b31c3473520c8
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2024-01-30 16:39:20 +0545

    drop dvc.datasets

--- dvc/datasets.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-
-def get(name: str) -> dict[str, dict[str, Any]]:
-    from dvc.repo import Repo
-
-    with Repo() as repo:
-        datasets = repo.index.datasets
-        return datasets[name]
