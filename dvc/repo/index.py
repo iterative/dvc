3a32d90f50be8aec51808de498272c4fd97550bb
Ruslan Kuprieiev <kupruser@gmail.com>
2024-02-01 15:00:31 +0200

    repo: index: don't set cache storage if there is no out.cache

--- dvc/repo/index.py
+++ dvc/repo/index.py
@@ -215,7 +215,9 @@ def _load_storage_from_out(storage_map, key, out):
     from dvc.config import NoRemoteError
     from dvc_data.index import FileStorage, ObjectStorage
 
-    storage_map.add_cache(ObjectStorage(key, out.cache))
+    if out.cache:
+        storage_map.add_cache(ObjectStorage(key, out.cache))
+
     try:
         remote = out.repo.cloud.get_remote(out.remote)
         if remote.fs.version_aware:
