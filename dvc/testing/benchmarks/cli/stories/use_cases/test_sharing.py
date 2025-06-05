5fb8986dbde6c1e797f6e69df0498670fb02a250
skshetry <18718008+skshetry@users.noreply.github.com>
2025-06-05 11:41:04 +0545

    bench: check for dvc_bin.version instead of dvc_rev (#10764)
    * bench: check for dvc_bin.version instead of dvc_rev
    
    * Update dvc/testing/benchmarks/cli/stories/use_cases/test_sharing.py
    
    * Update dvc/testing/benchmarks/cli/stories/use_cases/test_sharing.py

--- dvc/testing/benchmarks/cli/stories/use_cases/test_sharing.py
+++ dvc/testing/benchmarks/cli/stories/use_cases/test_sharing.py
@@ -3,10 +3,12 @@ import shutil
 from packaging import version
 
 
-def test_sharing(bench_dvc, tmp_dir, dvc, make_dataset, remote, dvc_rev):
+def test_sharing(bench_dvc, tmp_dir, dvc, make_dataset, remote, dvc_bin):
     is_gs = dvc.config["remote"]["upstream"]["url"].startswith("gs://")
-    if dvc_rev and is_gs and version.Version(dvc_rev) < version.Version("3.59.3"):
-        # support for allow_anonymous_login in gs was introduced in dvc==3.59.3
+    if is_gs and version.Version(dvc_bin.version) < version.Version("3.59.2"):
+        # support for allow_anonymous_login in gs was introduced in dvc==3.60
+        # This should not impact the test, just that it will make it slower if used
+        # with fake-gcs-server.
         with dvc.config.edit() as d:
             d["remote"]["upstream"].pop("allow_anonymous_login", None)
 
