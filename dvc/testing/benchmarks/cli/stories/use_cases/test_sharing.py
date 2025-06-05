60a0ac4973c658e69baa30a449f14644b5beb5c4
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2025-06-05 09:02:24 +0545

    tests(bench): remove allow_anonymous_login config use from dvc<3.59.3

--- dvc/testing/benchmarks/cli/stories/use_cases/test_sharing.py
+++ dvc/testing/benchmarks/cli/stories/use_cases/test_sharing.py
@@ -1,7 +1,15 @@
 import shutil
 
+from packaging import version
 
-def test_sharing(bench_dvc, tmp_dir, dvc, make_dataset, remote):
+
+def test_sharing(bench_dvc, tmp_dir, dvc, make_dataset, remote, dvc_rev):
+    is_gs = dvc.config["remote"]["upstream"]["url"].startswith("gs://")
+    if is_gs and version.Version(dvc_rev) < version.Version("3.59.3"):
+        # support for allow_anonymous_login in gs was introduced in dvc==3.59.3
+        with dvc.config.edit() as d:
+            d["remote"]["upstream"].pop("allow_anonymous_login", None)
+
     dataset = make_dataset(cache=True, dvcfile=True)
 
     bench_dvc("push")
