07e88f86e6227502c6f95dc81d12cf86d5e52fdd
rgoya <rodrigo.goya@abcellera.com>
2025-06-11 23:55:08 -0700

    Test should check default behaviour when no env var is set

--- tests/unit/test_dirs.py
+++ tests/unit/test_dirs.py
@@ -3,7 +3,7 @@ import sys
 import pytest
 
 from dvc.dirs import global_config_dir, site_cache_dir
-from dvc.env import DVC_GLOBAL_CONFIG_DIR
+from dvc.env import DVC_GLOBAL_CONFIG_DIR, DVC_SITE_CACHE_DIR
 
 
 def test_global_config_dir_respects_env_var(monkeypatch):
@@ -13,5 +13,6 @@ def test_global_config_dir_respects_env_var(monkeypatch):
 
 
 @pytest.mark.skipif(sys.platform != "linux", reason="Only for Unix platforms")
-def test_site_cache_dir_on_unix():
+def test_site_cache_dir_on_unix(monkeypatch):
+    monkeypatch.delenv(DVC_SITE_CACHE_DIR, raising=False)
     assert site_cache_dir() == "/var/tmp/dvc"
